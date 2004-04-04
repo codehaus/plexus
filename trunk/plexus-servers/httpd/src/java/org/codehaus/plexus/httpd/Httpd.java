6e52325686faff7a806252fcccc8210042715e64
trygvis <trygvis@30dfa635-7bf8-0310-9608-e41f2d776689>
2004-04-04 23:57:16 +0000

    o Adding required imports.

--- trunk/plexus-servers/httpd/src/java/org/codehaus/plexus/httpd/Httpd.java
+++ trunk/plexus-servers/httpd/src/java/org/codehaus/plexus/httpd/Httpd.java
@@ -7,9 +7,6 @@
  */
 package org.codehaus.plexus.httpd;
 
-import org.apache.plexus.logging.AbstractLogEnabled;
-import org.apache.velocity.context.Context;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InterruptedIOException;
@@ -21,6 +18,10 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.StringTokenizer;
+
+import org.apache.velocity.context.Context;
+
+import org.codehaus.plexus.logging.AbstractLogEnabled;
 
 /**
  * HttpAdaptor sets the basic adaptor listening for HTTP requests
