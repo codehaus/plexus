ae1f647df03c830d042453bf891c3a82c666e7d6
trygvis <trygvis@30dfa635-7bf8-0310-9608-e41f2d776689>
2004-06-16 23:37:22 +0000

    o Rewrite, now it's using the native plexus personality.

---
+++ plexus-components/plexus-xmlrpc/src/test/java/org/codehaus/plexus/xmlrpc/DefaultXmlRpcComponentTest.java
@@ -0,0 +1,38 @@
+package org.codehaus.plexus.xmlrpc;
+
+/*
+ * LICENSE
+ */
+
+import java.net.URL;
+import java.util.Vector;
+
+import org.codehaus.plexus.PlexusTestCase;
+
+/**
+ * @author <a href="mailto:trygvis@inamo.no">Trygve Laugst&oslash;l</a>
+ * @version $Id$
+ */
+public class DefaultXmlRpcComponentTest
+    extends PlexusTestCase
+{
+    public void testBasic()
+        throws Exception
+    {
+        URL url = new URL( "http://localhost:8080/rpc2" );
+
+        XmlRpcComponent xmlRpc = (XmlRpcComponent) lookup( XmlRpcComponent.ROLE );
+
+        Vector params = new Vector();
+
+        params.add( "Travis" );
+
+        Object obj = xmlRpc.executeRpc( url, "poker.poke", params);
+
+        assertEquals( obj.getClass().getName(), String.class.getName() );
+
+        String str = obj.toString();
+
+        assertEquals( "Hello World Travis!", str );
+    }
+}
