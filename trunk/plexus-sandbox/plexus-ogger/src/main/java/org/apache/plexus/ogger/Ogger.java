94362b055fc7c4999f27a7b0a39d69f08f7226ca
trygvis <trygvis@30dfa635-7bf8-0310-9608-e41f2d776689>
2004-06-21 01:08:34 +0000

    o Updating builds.

--- trunk/plexus-sandbox/plexus-ogger/src/main/java/org/apache/plexus/ogger/Ogger.java
+++ trunk/plexus-sandbox/plexus-ogger/src/main/java/org/apache/plexus/ogger/Ogger.java
@@ -1,14 +1,9 @@
 package org.apache.plexus.ogger;
 
-/** The Workflow Management System entry-point.
- *
- *  @author <a href="mailto:bob@eng.werken.com">bob mcwhirter</a>
- *
+/**
  *  @version $Id$
  */
 public interface Ogger
 {
-    /** Component role.
-     */
     public static final String ROLE = Ogger.class.getName();
 }
