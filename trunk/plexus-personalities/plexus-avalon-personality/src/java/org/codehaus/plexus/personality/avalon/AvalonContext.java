e9b0f17a3f450d43c466caf54a49f18ded746d0c
dandiep <dandiep@30dfa635-7bf8-0310-9608-e41f2d776689>
2004-06-21 16:36:29 +0000

    Add in the app.home context variable.

--- trunk/plexus-personalities/plexus-avalon-personality/src/java/org/codehaus/plexus/personality/avalon/AvalonContext.java
+++ trunk/plexus-personalities/plexus-avalon-personality/src/java/org/codehaus/plexus/personality/avalon/AvalonContext.java
@@ -4,18 +4,28 @@ import org.apache.avalon.framework.context.Context;
 import org.apache.avalon.framework.context.ContextException;
 
 /**
- * TODO document AvalonContext
+ * A wrapper for the plexus context.
  * 
  * @author <a href="mailto:dan@envoisolutions.com">Dan Diephouse</a>
  * @since Jan 6, 2004
  */
-public class AvalonContext implements Context
+public class AvalonContext
+    implements Context
 {
     org.codehaus.plexus.context.Context context;
     
     public AvalonContext( org.codehaus.plexus.context.Context context )
     {
         this.context = context;
+        
+        try
+		{
+			context.put( "app.home", context.get("plexus.home") );
+		}
+		catch (Exception e)
+		{
+		    throw new RuntimeException( "Couldn't find a home directory.", e );
+		}
     }
     
     /**
