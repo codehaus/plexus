170889a3b0093df964e6b9e2f7dff537915c70aa
trygvis <trygvis@30dfa635-7bf8-0310-9608-e41f2d776689>
2004-06-22 06:15:54 +0000

    o Update reflecting the latest changes in the builder.

--- trunk/plexus-maven-plugin/src/main/java/org/codehaus/plexus/maven/plugin/PlexusContainerGenerator.java
+++ trunk/plexus-maven-plugin/src/main/java/org/codehaus/plexus/maven/plugin/PlexusContainerGenerator.java
@@ -1,58 +1,36 @@
-/**
- *
- * Copyright 2004 The Apache Software Foundation
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
-package org.apache.maven.plugin.rar;
+package org.apache.maven.plugin.plexus;
 
-import java.io.File;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
+import org.apache.maven.plugin.AbstractPlugin;
 import org.apache.maven.plugin.PluginExecutionRequest;
 import org.apache.maven.plugin.PluginExecutionResponse;
-import org.apache.maven.plugin.AbstractPlugin;
 import org.apache.maven.project.MavenProject;
-import org.codehaus.plexus.builder.DefaultPlexusBuilder;
+
+import org.codehaus.plexus.builder.PlexusBuilder;
 
 /**
- * A mojo that build a J2EE RAR archive.
+ * @goal runtime
  *
- * @version $Revision$ $Date$
- * @maven.plugin.id plexus
- * @maven.plugin.description Maven pojo to build plexus containers
+ * @requiresDependencyResolution
  *
- * @parameter mavenRepoLocal String true validator description
- * @parameter outputDirectory String true validator description
- * @parameter pom String true validator description
- * @parameter plexusVersion String true validator description
- * @parameter plexusApplication String true validator description
- * @parameter plexusConfiguration String true validator description
- * @parameter plexusConfigurationsDirectory String true validator description
- * @parameter plexusConfigurationPropertiesFile String true validator description
+ * @description Builds plexus containers.
  *
- * @goal.name plexus:container
- * @goal.plexus:container.description assemble a container
- * @goal.plexus:container.parameter pom #basedir/project.xml
- * @goal.plexus:container.parameter outputDirectory #maven.build.dir/plexus
- * @goal.plexus:container.parameter mavenRepoLocal #maven.repo.local
- * @goal.plexus:container.parameter plexusVersion #plexus.version
- * @goal.plexus:container.parameter plexusApplication #plexus.application
- * @goal.plexus:container.parameter plexusConfiguration #plexus.configuration
- * @goal.plexus:container.parameter plexusComponentManifest #plexus.componentManifest
- * @goal.plexus:container.parameter plexusConfigurationsDirectory #plexus.configurationsDirectory
- * @goal.plexus:container.parameter plexusConfigurationPropertiesFile #plexus.configurationPropertiesFile
+ * @parameter
+ *  name="project"
+ *  type="String"
+ *  required="true"
+ *  validator=""
+ *  expression="#project"
+ *  description=""
+ * @parameter
+ *  name="runtimeBuilder"
+ *  type="org.codehaus.plexus.builder.PlexusBuilder"
+ *  required="true"
+ *  validator=""
+ *  expression="#component.org.codehaus.plexus.builder.PlexusBuilder"
+ *  description=""
+ * 
+ * @author <a href="mailto:trygvis@inamo.no">Trygve Laugst&oslash;l</a>
+ * @version $Id$
  */
 public class PlexusContainerGenerator
     extends AbstractPlugin
@@ -66,41 +44,41 @@ public class PlexusContainerGenerator
 
         String outputDirectory = (String) request.getParameter( "outputDirectory" );
 
-        String pom = (String) request.getParameter( "pom" );
+        MavenProject project = (MavenProject) request.getParameter( "project" );
 
-        String mavenRepoLocal = (String) request.getParameter( "mavenRepoLocal" );
+//        String mavenRepoLocal = (String) request.getParameter( "mavenRepoLocal" );
 
-        String plexusVersion = (String) request.getParameter( "plexusVersion" );
+//        String plexusVersion = (String) request.getParameter( "plexusVersion" );
 
-        String plexusApplication = (String) request.getParameter( "plexusApplication" );
+//        String plexusApplication = (String) request.getParameter( "plexusApplication" );
 
         String plexusConfiguration = (String) request.getParameter( "plexusConfiguration" );
 
-        String componentManifest = (String) request.getParameter( "plexusComponentManifest" );
+//        String componentManifest = (String) request.getParameter( "plexusComponentManifest" );
 
         String configurationsDirectory = (String) request.getParameter( "plexusConfigurationsDirectory" );
 
         String configurationPropertiesFile = (String) request.getParameter( "plexusConfigurationPropertiesFile" );
 
+        PlexusBuilder builder = (PlexusBuilder) request.getParameter( "runtimeBuilder" );
+
         // ----------------------------------------------------------------------
         //
         // ----------------------------------------------------------------------
 
-        DefaultPlexusBuilder builder = new DefaultPlexusBuilder();
-
         builder.setBaseDirectory( outputDirectory );
 
-        builder.setProjectPom( pom );
+        builder.setProject( project );
 
-        builder.setMavenRepoLocal( mavenRepoLocal );
+        builder.setMavenRepoLocal( project.getLocalRepository() );
 
-        builder.setApplication( plexusApplication );
+//        builder.setApplication( plexusApplication );
 
-        builder.setPlexusVersion( plexusVersion );
+//        builder.setPlexusVersion( plexusVersion );
 
         builder.setPlexusConfiguration( plexusConfiguration );
 
-        builder.setComponentManifest( componentManifest );
+//        builder.setComponentManifest( componentManifest );
 
         builder.setConfigurationsDirectory( configurationsDirectory );
 
