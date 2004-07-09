e189cd52f48688c47fe083a78a5beaf1788a69b3
trygvis <trygvis@30dfa635-7bf8-0310-9608-e41f2d776689>
2004-07-09 17:09:49 +0000

    o Updating/improving the tests.

--- trunk/plexus-tools/plexus-runtime-builder/src/test/java/org/codehaus/plexus/builder/PlexusBuilderTest.java
+++ trunk/plexus-tools/plexus-runtime-builder/src/test/java/org/codehaus/plexus/builder/PlexusBuilderTest.java
@@ -1,11 +1,14 @@
 package org.codehaus.plexus.builder;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.project.MavenProjectBuilder;
 
 import org.codehaus.plexus.PlexusTestCase;
+import org.codehaus.plexus.util.FileUtils;
 
 public class PlexusBuilderTest
     extends PlexusTestCase
@@ -25,15 +28,15 @@ public class PlexusBuilderTest
     {
         super.setUp();
 
-        buildDirectory = new File( basedir, "target/generated-runtime" ).getPath();
+        buildDirectory = new File( getBasedir(), "target/generated-runtime" ).getPath();
 
-        plexusConfiguration = new File( basedir, "src/test/resources/conf/plexus.conf" ).getPath();
+        plexusConfiguration = new File( getBasedir(), "src/test/resources/conf/plexus.conf" ).getPath();
 
-        componentManifest = new File( basedir, "src/test/resources/plexus-component.manifest" ).getPath();
+        componentManifest = new File( getBasedir(), "src/test/resources/plexus-component.manifest" ).getPath();
 
-        configurationsDirectory = new File( basedir, "src/test/resources/conf" ).getPath();
+        configurationsDirectory = new File( getBasedir(), "src/test/resources/conf" ).getPath();
 
-        configurationPropertiesFile = new File( basedir, "src/test/resources/configuration.properties" ).getPath();
+        configurationPropertiesFile = new File( getBasedir(), "src/test/resources/configuration.properties" ).getPath();
     }
 
     public void testPlexusBuilder()
@@ -45,13 +48,13 @@ public class PlexusBuilderTest
 
         builder.setBaseDirectory( buildDirectory );
 
-        MavenProject project = projectBuilder.build( new File( getTestFile( "src/test/project" ) + "/project.xml" ) );
+        MavenProject project = projectBuilder.build( new File( getTestFile( "src/test/project/project.xml" ) ), getTestFile( "src/test/repository" ) );
 
         builder.setProject( project );
 
-        //builder.setMavenRepoLocal( System.getProperty( "user.home" ) + "/maven-repo-local" );
-
         builder.setMavenRepoLocal( getTestFile( "src/test/repository" ) );
+
+        builder.setApplicationName( "testapp" );
 
         builder.setPlexusConfiguration( plexusConfiguration );
 
@@ -60,5 +63,84 @@ public class PlexusBuilderTest
         builder.setConfigurationPropertiesFile( configurationPropertiesFile );
 
         builder.build();
+
+        assertFileEquals( "classworlds-classworlds-1.1-SNAPSHOT", "core/boot/classworlds-1.1-SNAPSHOT.jar" );
+
+        assertFileEquals( "plexus-plexus-0.14-SNAPSHOT", "core/plexus-0.14-SNAPSHOT.jar" );
+
+        assertFileEquals( "plexus-plexus-dependency-1.0", "core/plexus-dependency-1.0.jar" );
+
+        assertFileEquals( "group1-artifact1-2.0", "apps/testapp/lib/artifact1-2.0.jar" );
+
+        assertFileEquals( "group2-artifact2-1.0", "apps/testapp/lib/artifact2-1.0.foo" );
+
+        assertFileEquals( "group2-artifact2-1.0", "apps/testapp/lib/artifact2-1.0.foo" );
+
+        assertFileEquals( "plexus-plexus-runtime-builder-test-project-2.0-SNAPSHOT", "apps/testapp/lib/plexus-runtime-builder-test-project-2.0-SNAPSHOT.jar" );
+
+        assertFileEquals( "plexus-plexus-runtime-builder-test-project-2.0-SNAPSHOT", "apps/testapp/lib/plexus-runtime-builder-test-project-2.0-SNAPSHOT.jar" );
+
+        assertFileEquals( "plexus-plexus-runtime-builder-test-project-2.0-SNAPSHOT", "apps/testapp/lib/plexus-runtime-builder-test-project-2.0-SNAPSHOT.jar" );
+
+        assertFile( "bin/plexus.bat" );
+
+        assertFile( "bin/plexus.sh" );
+
+        assertFile( "conf/classworlds.conf" );
+
+        assertFile( "conf/plexus.conf" );
+
+        assertFile( "conf/wrapper.conf" );
+    }
+
+    private void assertFileEquals( String expected, String fileName )
+        throws Exception
+    {
+        File file = new File( buildDirectory, fileName );
+
+        assertTrue( file.getAbsolutePath() + " doesn't exist.", file.exists() );
+
+        assertTrue( file.getAbsolutePath() + " isn't a file.", file.isFile() );
+
+        String actual = FileUtils.fileRead( file ).trim();
+
+        assertEquals( expected, actual );
+    }
+
+    private void assertFile( String fileName )
+        throws Exception
+    {
+        File expectedFile = new File( getTestFile( "src/test/expected-runtime/" ), fileName );
+
+        File actualFile = new File( buildDirectory, fileName );
+
+        BufferedReader expectedInput = new BufferedReader( new FileReader( expectedFile ) );
+
+        BufferedReader actualInput = new BufferedReader( new FileReader( actualFile ) );
+
+        String expected, actual;
+
+        int line = 0;
+
+        while ( ( expected = expectedInput.readLine() ) != null )
+        {
+            actual = actualInput.readLine();
+
+            if ( actual == null )
+            {
+                fail( "Line #" + line + ": unexpected end of file." );
+            }
+
+            assertEquals( "Line: #" + line, expected, actual );
+
+            line++;
+        }
+
+        actual = actualInput.readLine();
+
+        if ( actual != null )
+        {
+            fail( "The actual file contains more files than the expected file." );
+        }
     }
 }
