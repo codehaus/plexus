b57367354e06e9e8b99affb162635edb1ab7df36
jdcasey <jdcasey@30dfa635-7bf8-0310-9608-e41f2d776689>
2004-09-15 22:17:16 +0000

    o Changed cling package structure to indicate it's membership to the plexus project. o Added marmalade taglib def files o Added plexus component descriptor (might not be included in this commit...will check)

---
+++ trunk/plexus-tools/plexus-cling/src/main/java/org/codehaus/plexus/cling/model/Classpath.java
@@ -0,0 +1,17 @@
+/* Created on Sep 13, 2004 */
+package org.codehaus.plexus.cling.model;
+
+import java.net.URL;
+import java.util.List;
+
+/**
+ * @author jdcasey
+ */
+public interface Classpath
+{
+
+    public void addEntry( ClasspathEntry entry );
+
+    public List getEntries();
+
+}
---
+++ trunk/plexus-tools/plexus-cling/src/main/java/org/codehaus/plexus/cling/model/LegalUsage.java
@@ -0,0 +1,16 @@
+/* Created on Sep 14, 2004 */
+package org.codehaus.plexus.cling.model;
+
+import java.util.List;
+
+import org.codehaus.plexus.cling.cli.InvocationTemplate;
+
+/**
+ * @author jdcasey
+ */
+public interface LegalUsage
+{
+    
+    public List getInvocationTemplates();
+
+}
