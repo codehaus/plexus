84089ebd734a25b6c8d6ed183cb10a3cdab4892c
jdcasey <jdcasey@30dfa635-7bf8-0310-9608-e41f2d776689>
2004-09-16 05:08:28 +0000

    o Added tests for configuration package.

--- trunk/plexus-tools/plexus-cling/src/main/java/org/codehaus/plexus/cling/configuration/SupplementaryCLIngConfiguration.java
+++ trunk/plexus-tools/plexus-cling/src/main/java/org/codehaus/plexus/cling/configuration/SupplementaryCLIngConfiguration.java
@@ -8,44 +8,18 @@ import org.apache.maven.artifact.repository.ArtifactRepository;
 /**
  * @author jdcasey
  */
-public class SupplementaryCLIngConfiguration
+public class SupplementaryCLIngConfiguration extends AbstractCLIngConfiguration
     implements CLIngConfiguration
 {
     
-    private ArtifactRepository localRepository;
-    private Set remoteRepositories;
-    
     public void setLocalRepository(ArtifactRepository localRepository)
     {
-        this.localRepository = localRepository;
+        super.setLocalRepository(localRepository);
     }
 
-    public ArtifactRepository getLocalRepository()
-    {
-        return localRepository;
-    }
-    
     public void setRemoteRepositories(Set remoteRepositories)
     {
-        this.remoteRepositories = remoteRepositories;
-    }
-
-    public Set getRemoteRepositories()
-    {
-        return remoteRepositories;
-    }
-
-    public void overrideWith( CLIngConfiguration otherConfig )
-    {
-        ArtifactRepository otherLocal = otherConfig.getLocalRepository();
-        if(otherLocal != null) {
-            this.localRepository = otherLocal;
-        }
-        
-        Set otherRemoteRepos = otherConfig.getRemoteRepositories();
-        if(otherRemoteRepos != null) {
-            this.remoteRepositories = otherRemoteRepos;
-        }
+        super.setRemoteRepositories(remoteRepositories);
     }
 
 }
