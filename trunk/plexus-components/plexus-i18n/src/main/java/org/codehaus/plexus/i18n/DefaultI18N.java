3d3297f889a549cb26df39aad76d5faebd475744
jvanzyl <jvanzyl@30dfa635-7bf8-0310-9608-e41f2d776689>
2004-09-23 18:42:42 +0000

    o changes which allow for the dynamic changing of i18n resources.

--- trunk/plexus-components/plexus-i18n/src/main/java/org/codehaus/plexus/i18n/DefaultI18N.java
+++ trunk/plexus-components/plexus-i18n/src/main/java/org/codehaus/plexus/i18n/DefaultI18N.java
@@ -1,59 +1,5 @@
 package org.codehaus.plexus.i18n;
 
-/* ====================================================================
- * The Apache Software License, Version 1.1
- *
- * Copyright (c) 2001 The Apache Software Foundation.  All rights
- * reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:
- *       "This product includes software developed by the
- *        Apache Software Foundation (http://www.codehaus.org/)."
- *    Alternately, this acknowledgment may appear in the software itself,
- *    if and wherever such third-party acknowledgments normally appear.
- *
- * 4. The names "Apache" and "Apache Software Foundation" and
- *    "Apache Turbine" must not be used to endorse or promote products
- *    derived from this software without prior written permission. For
- *    written permission, please contact codehaus@codehaus.org.
- *
- * 5. Products derived from this software may not be called "Apache",
- *    "Apache Turbine", nor may "Apache" appear in their name, without
- *    prior written permission of the Apache Software Foundation.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
- * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
- * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- * ====================================================================
- *
- * This software consists of voluntary contributions made by many
- * individuals on behalf of the Apache Software Foundation.  For more
- * information on the Apache Software Foundation, please see
- * <http://www.codehaus.org/>.
- */
-
 import org.codehaus.plexus.logging.AbstractLogEnabled;
 import org.codehaus.plexus.personality.plexus.lifecycle.phase.Initializable;
 import org.codehaus.plexus.util.StringUtils;
@@ -64,126 +10,55 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
-
-
-/**
- * <p>This class is the single point of access to all i18n
- * resources.  It caches different ResourceBundles for different
- * Locales.</p>
- *
- * <p>Usage example:</p>
- *
- * <blockquote><code><pre>
- * LocalizationService ls = (LocalizationService) TurbineServices
- *     .getInstance().getService(LocalizationService.SERVICE_NAME);
- * </pre></code></blockquote>
- *
- * <p>Then call {@link #getString(String, Locale, String)}, or one of
- * four methods to retrieve a ResourceBundle:
- *
- * <ul>
- * <li>getBundle("MyBundleName")</li>
- * <li>getBundle("MyBundleName", httpAcceptLanguageHeader)</li>
- * <li>etBundle("MyBundleName", HttpServletRequest)</li>
- * <li>getBundle("MyBundleName", Locale)</li>
- * <li>etc.</li>
- * </ul></p>
- *
- * @author <a href="mailto:jm@mediaphil.de">Jonas Maurus</a>
- * @author <a href="mailto:jon@latchkey.com">Jon S. Stevens</a>
- * @author <a href="mailto:novalidemail@foo.com">Frank Y. Kim</a>
- * @author <a href="mailto:dlr@finemaltcoding.com">Daniel Rall</a>
- * @author <a href="mailto:leonardr@collab.net">Leonard Richardson</a>
- * @version $Id$
- */
+import java.lang.reflect.Field;
 
 public class DefaultI18N
     extends AbstractLogEnabled
     implements I18N, Initializable
 {
-    /**
-     * The value to pass to <code>MessageFormat</code> if a
-     * <code>null</code> reference is passed to <code>format()</code>.
-     */
     private static final Object[] NO_ARGS = new Object[0];
 
-    /**
-     * Bundle name keys a HashMap of the ResourceBundles in this
-     * service (which is in turn keyed by Locale).
-     */
     private HashMap bundles;
 
-    /**
-     * The list of default bundles to search.
-     */
     private String[] bundleNames;
 
-    /** The default bundle name to use if not specified. */
     private String defaultBundleName;
 
-    /** The name of the default locale to use (includes language and country). */
     private Locale defaultLocale = Locale.getDefault();
 
-    /** The name of the default language to use. */
     private String defaultLanguage = Locale.getDefault().getLanguage();
 
-    /** The name of the default country to use. */
     private String defaultCountry = Locale.getDefault().getCountry();
-
-    /**
-     * Creates a new instance.
-     */
-    public DefaultI18N()
-    {
-    }
 
     // ----------------------------------------------------------------------
     // Accessors
     // ----------------------------------------------------------------------
 
-    /**
-     * Retrieves the default language (specified in the config file).
-     */
     public String getDefaultLanguage()
     {
         return defaultLanguage;
     }
 
-    /**
-     * Retrieves the default country (specified in the config file).
-     */
     public String getDefaultCountry()
     {
         return defaultCountry;
     }
 
-    /**
-     * @see I18N#getDefaultBundleName()
-     */
     public String getDefaultBundleName()
     {
         return defaultBundleName;
     }
 
-    /**
-     * @see I18N#getBundleNames()
-     */
     public String[] getBundleNames()
     {
         return (String[]) bundleNames.clone();
     }
 
-    /**
-     * @see I18N#getBundle()
-     */
     public ResourceBundle getBundle()
     {
         return getBundle( getDefaultBundleName(), (Locale) null );
     }
 
-    /**
-     * @see I18N#getBundle(String)
-     */
     public ResourceBundle getBundle( String bundleName )
     {
         return getBundle( bundleName, (Locale) null );
@@ -194,7 +69,7 @@ public class DefaultI18N
      * the Locale information supplied in the HTTP "Accept-Language"
      * header.
      *
-     * @param bundleName Name of bundle.
+     * @param bundleName     Name of bundle.
      * @param languageHeader A String with the language header.
      * @return A localized ResourceBundle.
      */
@@ -208,15 +83,33 @@ public class DefaultI18N
      * and the given Locale.
      *
      * @param bundleName Name of bundle (or <code>null</code> for the
-     * default bundle).
-     * @param locale The locale (or <code>null</code> for the locale
-     * indicated by the default language and country).
+     *                   default bundle).
+     * @param locale     The locale (or <code>null</code> for the locale
+     *                   indicated by the default language and country).
      * @return A localized ResourceBundle.
      */
     public ResourceBundle getBundle( String bundleName, Locale locale )
     {
         // Assure usable inputs.
         bundleName = ( bundleName == null ? getDefaultBundleName() : bundleName.trim() );
+
+        try
+        {
+            Class klass = ResourceBundle.getBundle( bundleName ).getClass().getSuperclass();
+
+            Field field = klass.getDeclaredField( "cacheList" );
+
+            field.setAccessible( true );
+
+            sun.misc.SoftCache cache = (sun.misc.SoftCache) field.get( null );
+
+            cache.clear();
+
+            field.setAccessible( false );
+        }
+        catch ( Exception e )
+        {
+        }
 
         if ( locale == null )
         {
@@ -225,7 +118,9 @@ public class DefaultI18N
 
         // Find/retrieve/cache bundle.
         ResourceBundle rb = null;
+
         HashMap bundlesByLocale = (HashMap) bundles.get( bundleName );
+
         if ( bundlesByLocale != null )
         {
             // Cache of bundles by locale for the named bundle exists.
@@ -242,31 +137,8 @@ public class DefaultI18N
         {
             rb = cacheBundle( bundleName, locale );
         }
-        return rb;
-    }
 
-    /**
-     * This method sets the name of the first bundle in the search
-     * list (the "default" bundle).
-     *
-     * @param defaultBundle Name of default bundle.
-     */
-    public void setBundle( String defaultBundle )
-    {
-        if ( bundleNames.length > 0 )
-        {
-            bundleNames[0] = defaultBundle;
-        }
-        else
-        {
-            synchronized ( this )
-            {
-                if ( bundleNames.length <= 0 )
-                {
-                    bundleNames = new String[]{defaultBundle};
-                }
-            }
-        }
+        return rb;
     }
 
     /**
@@ -277,6 +149,7 @@ public class DefaultI18N
         if ( !StringUtils.isEmpty( header ) )
         {
             I18NTokenizer tok = new I18NTokenizer( header );
+
             if ( tok.hasNext() )
             {
                 return (Locale) tok.next();
@@ -298,7 +171,7 @@ public class DefaultI18N
     }
 
     /**
-     * @exception MissingResourceException Specified key cannot be matched.
+     * @throws MissingResourceException Specified key cannot be matched.
      * @see I18N#getString(String, Locale, String)
      */
     public String getString( String bundleName, Locale locale, String key )
@@ -312,6 +185,7 @@ public class DefaultI18N
 
         // Look for text in requested bundle.
         ResourceBundle rb = getBundle( bundleName, locale );
+
         value = getStringOrNull( rb, key );
 
         // Look for text in list of default bundles.
@@ -325,10 +199,13 @@ public class DefaultI18N
                 if ( !name.equals( bundleName ) )
                 {
                     rb = getBundle( name, locale );
+
                     value = getStringOrNull( rb, key );
+
                     if ( value != null )
                     {
                         locale = rb.getLocale();
+
                         break;
                     }
                 }
@@ -343,8 +220,9 @@ public class DefaultI18N
 
             getLogger().debug( mesg );
 
-            // Text not found in requested or default bundles.
-            throw new MissingResourceException( mesg, bundleName, key );
+            // Just send back the key, we don't need to throw an exception.
+
+            value = key;
         }
 
         return value;
@@ -390,12 +268,9 @@ public class DefaultI18N
      * specified <code>Locale</code> using <code>args</code>.
      *
      * @return Localized, formatted text identified by
-     * <code>key</code>.
+     *         <code>key</code>.
      */
-    public String format( String bundleName,
-                          Locale locale,
-                          String key,
-                          Object[] args )
+    public String format( String bundleName, Locale locale, String key, Object[] args )
     {
         if ( locale == null )
         {
@@ -432,19 +307,14 @@ public class DefaultI18N
 
         defaultLocale = new Locale( defaultLanguage, defaultCountry );
 
-        initializeBundleNames( null );
+        initializeBundleNames();
     }
 
     // ----------------------------------------------------------------------
     // Implementation
     // ----------------------------------------------------------------------
 
-    /**
-     * Initialize list of default bundle names.
-     *
-     * @param ignored names Ignored.
-     */
-    protected void initializeBundleNames( String[] ignored )
+    protected void initializeBundleNames()
     {
         //System.err.println("cfg=" + getConfiguration());
         if ( defaultBundleName != null && defaultBundleName.length() > 0 )
@@ -469,26 +339,24 @@ public class DefaultI18N
         }
     }
 
-
     /**
      * Caches the named bundle for fast lookups.  This operation is
      * relatively expesive in terms of memory use, but is optimized
      * for run-time speed in the usual case.
      *
-     * @exception MissingResourceException Bundle not found.
+     * @throws MissingResourceException Bundle not found.
      */
-    private synchronized ResourceBundle cacheBundle( String bundleName,
-                                                     Locale locale )
+    private synchronized ResourceBundle cacheBundle( String bundleName, Locale locale )
         throws MissingResourceException
     {
         HashMap bundlesByLocale = (HashMap) bundles.get( bundleName );
-        ResourceBundle rb = ( bundlesByLocale == null ? null :
-            (ResourceBundle) bundlesByLocale.get( locale ) );
+
+        ResourceBundle rb = ( bundlesByLocale == null ? null : (ResourceBundle) bundlesByLocale.get( locale ) );
 
         if ( rb == null )
         {
-            bundlesByLocale = ( bundlesByLocale == null ? new HashMap( 3 ) :
-                new HashMap( bundlesByLocale ) );
+            bundlesByLocale = ( bundlesByLocale == null ? new HashMap( 3 ) : new HashMap( bundlesByLocale ) );
+
             try
             {
                 rb = ResourceBundle.getBundle( bundleName, locale );
@@ -496,6 +364,7 @@ public class DefaultI18N
             catch ( MissingResourceException e )
             {
                 rb = findBundleByLocale( bundleName, locale, bundlesByLocale );
+
                 if ( rb == null )
                 {
                     throw (MissingResourceException) e.fillInStackTrace();
@@ -508,17 +377,20 @@ public class DefaultI18N
                 bundlesByLocale.put( rb.getLocale(), rb );
 
                 HashMap bundlesByName = new HashMap( bundles );
+
                 bundlesByName.put( bundleName, bundlesByLocale );
+
                 this.bundles = bundlesByName;
             }
         }
+
         return rb;
     }
 
     /**
      * <p>Retrieves the bundle most closely matching first against the
      * supplied inputs, then against the defaults.</p>
-     *
+     * <p/>
      * <p>Use case: some clients send a HTTP Accept-Language header
      * with a value of only the language to use
      * (i.e. "Accept-Language: en"), and neglect to include a country.
@@ -526,7 +398,7 @@ public class DefaultI18N
      * can be called to try the default country (checking internally
      * to assure the requested criteria matches the default to avoid
      * disconnects between language and country).</p>
-     *
+     * <p/>
      * <p>Since we're really just guessing at possible bundles to use,
      * we don't ever throw <code>MissingResourceException</code>.</p>
      */
@@ -578,8 +450,7 @@ public class DefaultI18N
      * returning <code>null</code> instead of throwing
      * <code>MissingResourceException</code>.
      */
-    private final ResourceBundle getBundleIgnoreException( String bundleName,
-                                                           Locale locale )
+    private final ResourceBundle getBundleIgnoreException( String bundleName, Locale locale )
     {
         try
         {
