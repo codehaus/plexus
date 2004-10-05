e3439153148e265dde60199250975a4b3013f396
dandiep <dandiep@30dfa635-7bf8-0310-9608-e41f2d776689>
2004-10-05 03:54:48 +0000

    A new werkflow component.

---
+++ trunk/plexus-components/plexus-werkflow/src/test/java/org/codehaus/plexus/werkflow/DefaultWerkflowServiceTest.java
@@ -0,0 +1,35 @@
+package org.codehaus.plexus.werkflow;
+import org.codehaus.plexus.PlexusTestCase;
+import org.codehaus.werkflow.Engine;
+import org.codehaus.werkflow.InitialContext;
+import org.codehaus.werkflow.Instance;
+
+
+/**
+ * DefaultWerkflowServiceTest
+ * 
+ * @author <a href="mailto:dan@envoisolutions.com">Dan Diephouse</a>
+ */
+public class DefaultWerkflowServiceTest
+	extends PlexusTestCase
+{
+	public void testWerkflow() throws Exception
+    {
+        WerkflowService s = (WerkflowService) lookup(WerkflowService.ROLE);
+        
+        Engine engine = s.getEngine();
+        assertNotNull( engine );
+        
+        InitialContext c = new InitialContext();
+        c.set("true", Boolean.TRUE);
+        c.set("false", Boolean.FALSE);
+        
+        Instance i = s.getEngine().newInstance("ted", "instance1", c);
+        
+        Thread.sleep( 2000 );
+        
+        assertNotNull(i.get("one"));
+        
+        assertTrue( i.isComplete() );
+    }
+}
---
+++ trunk/plexus-components/plexus-werkflow/src/test/java/org/codehaus/plexus/werkflow/TestAction.java
@@ -0,0 +1,29 @@
+package org.codehaus.plexus.werkflow;
+
+import java.util.Map;
+
+import junit.framework.Assert;
+
+import org.codehaus.plexus.action.Action;
+import org.codehaus.werkflow.Instance;
+
+/**
+ * @author <a href="mailto:dan@envoisolutions.com">Dan Diephouse</a>
+ */
+public class TestAction
+    extends Assert
+    implements Action
+{
+	/**
+	 * @see org.codehaus.plexus.action.Action#execute(java.util.Map)
+	 */
+	public void execute(Map context) throws Exception
+	{
+		Instance i = (Instance) context.get("instance");
+        
+        assertNotNull(i);
+        
+        i.put("one", "one");
+	}
+
+}
