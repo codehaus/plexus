b7c98f11518b846e40ac5dca5600af21298699c9
dandiep <dandiep@30dfa635-7bf8-0310-9608-e41f2d776689>
2004-10-05 17:36:36 +0000

    change service -> component

--- trunk/plexus-components/plexus-werkflow/src/main/java/org/codehaus/plexus/werkflow/DefaultWerkflowComponent.java
+++ trunk/plexus-components/plexus-werkflow/src/main/java/org/codehaus/plexus/werkflow/DefaultWerkflowComponent.java
@@ -1,26 +1,31 @@
 package org.codehaus.plexus.werkflow;
 
 import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.codehaus.plexus.action.ActionManager;
 import org.codehaus.plexus.logging.AbstractLogEnabled;
 import org.codehaus.plexus.personality.plexus.lifecycle.phase.Initializable;
 import org.codehaus.plexus.werkflow.ognl.OgnlExpressionFactory;
 import org.codehaus.werkflow.AutomaticEngine;
+import org.codehaus.werkflow.DuplicateWorkflowException;
 import org.codehaus.werkflow.Engine;
 import org.codehaus.werkflow.Workflow;
 import org.codehaus.werkflow.nonpersistent.NonPersistentInstanceManager;
 import org.codehaus.werkflow.simple.ExpressionFactory;
 import org.codehaus.werkflow.simple.SimpleWorkflowReader;
+import org.xml.sax.SAXException;
 
 /**
- * The default WerkflowService implementation.
+ * The default WerkflowComponent implementation.
  * 
  * @author <a href="mailto:dan@envoisolutions.com">Dan Diephouse </a>
  */
-public class DefaultWerkflowService
+public class DefaultWerkflowComponent
     extends AbstractLogEnabled
-    implements WerkflowService, Initializable
+    implements WerkflowComponent, Initializable
 {
     private String werkflowDirectory;
 
@@ -56,6 +61,11 @@ public class DefaultWerkflowService
 
         expressionFactory = new OgnlExpressionFactory();
 
+        loadWerkflows();
+    }
+
+    private void loadWerkflows() throws Exception
+    {
         File werkflowDirFile = new File(werkflowDirectory);
 
         if (!werkflowDirFile.isDirectory())
@@ -70,11 +80,15 @@ public class DefaultWerkflowService
 
             for (int i = 0; i < werkflows.length; i++)
             {
-                Workflow workflow = SimpleWorkflowReader.read(
-                        getActionManager(), getExpressionFactory(),
-                        werkflows[i]);
-
-                engine.addWorkflow(workflow);
+                if ( werkflows[i].getAbsolutePath().endsWith(".xml") )
+                {
+                    Workflow workflow = SimpleWorkflowReader.read(
+                            getActionManager(), 
+                            getExpressionFactory(),
+                            werkflows[i]);
+    
+                    engine.addWorkflow(workflow);
+                }
             }
         }
     }
