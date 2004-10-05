7acb610a3598edf4c4dbe0de76b74ee74b688119
dandiep <dandiep@30dfa635-7bf8-0310-9608-e41f2d776689>
2004-10-05 04:00:33 +0000

    fix tabs

--- plexus-components/plexus-werkflow/src/main/java/org/codehaus/plexus/werkflow/ognl/OgnlExpression.java
+++ plexus-components/plexus-werkflow/src/main/java/org/codehaus/plexus/werkflow/ognl/OgnlExpression.java
@@ -12,39 +12,40 @@ import org.codehaus.werkflow.spi.Expression;
 /**
  * OgnlExpression
  * 
- * @author <a href="mailto:dan@envoisolutions.com">Dan Diephouse</a>
+ * @author <a href="mailto:dan@envoisolutions.com">Dan Diephouse </a>
  */
 public class OgnlExpression
-	implements Expression
+    implements Expression
 {
 
     private String expression;
-    
-    public OgnlExpression( String expression )
+
+    public OgnlExpression(String expression)
     {
-    	this.expression = expression;
+        this.expression = expression;
     }
-    
-	public String getExpression()
-	{
-		return expression;
-	}
-    
-	/**
-	 * @see org.codehaus.werkflow.spi.Expression#evaluate(org.codehaus.werkflow.Context)
-	 */
-	public boolean evaluate(Context c)
-	{
+
+    public String getExpression()
+    {
+        return expression;
+    }
+
+    /**
+     * @see org.codehaus.werkflow.spi.Expression#evaluate(org.codehaus.werkflow.Context)
+     */
+    public boolean evaluate(Context c)
+    {
         Map context = new HashMap();
         context.put("context", c);
-		try
-		{
-			Boolean b = (Boolean) Ognl.getValue(expression, context, (Object) null);
+        try
+        {
+            Boolean b = (Boolean) Ognl.getValue(expression, context,
+                    (Object) null);
             return b.booleanValue();
-		}
-		catch (OgnlException e)
-		{
-			throw new RuntimeException("Couldn't evaluate expression: " + expression, e);
-		}
-	}
+        }
+        catch (OgnlException e)
+        {
+            throw new RuntimeException("Couldn't evaluate expression: " + expression, e);
+        }
+    }
 }
--- plexus-components/plexus-werkflow/src/test/java/org/codehaus/plexus/werkflow/DefaultWerkflowServiceTest.java
+++ plexus-components/plexus-werkflow/src/test/java/org/codehaus/plexus/werkflow/DefaultWerkflowServiceTest.java
@@ -1,35 +1,35 @@
 package org.codehaus.plexus.werkflow;
+
 import org.codehaus.plexus.PlexusTestCase;
 import org.codehaus.werkflow.Engine;
 import org.codehaus.werkflow.InitialContext;
 import org.codehaus.werkflow.Instance;
 
-
 /**
  * DefaultWerkflowServiceTest
  * 
- * @author <a href="mailto:dan@envoisolutions.com">Dan Diephouse</a>
+ * @author <a href="mailto:dan@envoisolutions.com">Dan Diephouse </a>
  */
 public class DefaultWerkflowServiceTest
-	extends PlexusTestCase
+    extends PlexusTestCase
 {
-	public void testWerkflow() throws Exception
+    public void testWerkflow() throws Exception
     {
         WerkflowService s = (WerkflowService) lookup(WerkflowService.ROLE);
-        
+
         Engine engine = s.getEngine();
-        assertNotNull( engine );
-        
+        assertNotNull(engine);
+
         InitialContext c = new InitialContext();
         c.set("true", Boolean.TRUE);
         c.set("false", Boolean.FALSE);
-        
+
         Instance i = s.getEngine().newInstance("ted", "instance1", c);
-        
-        Thread.sleep( 2000 );
-        
+
+        Thread.sleep(2000);
+
         assertNotNull(i.get("one"));
-        
-        assertTrue( i.isComplete() );
+
+        assertTrue(i.isComplete());
     }
 }
