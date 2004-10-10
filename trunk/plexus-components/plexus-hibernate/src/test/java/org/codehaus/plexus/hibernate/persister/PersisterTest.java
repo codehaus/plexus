558863212f289dd3bcb334ad922b9a5ef0cd2aba
trygvis <trygvis@30dfa635-7bf8-0310-9608-e41f2d776689>
2004-10-10 17:31:55 +0000

    o Adding the HibernateService back into components.xml. o Updating some javadocs and misc other eclipse warnings.

--- trunk/plexus-components/plexus-hibernate/src/test/java/org/codehaus/plexus/hibernate/persister/PersisterTest.java
+++ trunk/plexus-components/plexus-hibernate/src/test/java/org/codehaus/plexus/hibernate/persister/PersisterTest.java
@@ -14,25 +14,25 @@ public class PersisterTest
         extends LongPersistable
     {
     }
-    
+
     public void testEquivalence()
     {
         PersistableA a1 = new PersistableA();
         a1.setId( new Long(10) );
-        
+
         PersistableA a2 = new PersistableA();
-        a2.setId(  new Long(10) );
-        
+        a2.setId( new Long(10) );
+
         PersistableA a3 = new PersistableA();
-        a3.setId(  new Long(13) );
-        
+        a3.setId( new Long(13) );
+
         assertEquals(a1.hashCode(), a2.hashCode());
         assertEquals(a1, a2);
         assertEquals(a2, a1);
-        
+
         assertNotSame( a1, a3 );
         assertNotSame( a3, a1 );
-        
+
         assertTrue( a1.hashCode() != a3.hashCode() );
     }
 }
