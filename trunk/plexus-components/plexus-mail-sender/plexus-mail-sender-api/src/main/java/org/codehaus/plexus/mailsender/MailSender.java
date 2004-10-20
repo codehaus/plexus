6f2c42eaab80a5902876c90ddfca1667d39f0fd1
evenisse <evenisse@30dfa635-7bf8-0310-9608-e41f2d776689>
2004-10-20 14:21:42 +0000

    Refactoring.

--- trunk/plexus-components/plexus-mail-sender/plexus-mail-sender-api/src/main/java/org/codehaus/plexus/mailsender/MailSender.java
+++ trunk/plexus-components/plexus-mail-sender/plexus-mail-sender-api/src/main/java/org/codehaus/plexus/mailsender/MailSender.java
@@ -4,6 +4,7 @@ package org.codehaus.plexus.mailsender;
  * LICENSE
  */
 
+import java.util.Date;
 import java.util.Map;
 
 /**
@@ -14,9 +15,67 @@ public interface MailSender
 {
     String ROLE = MailSender.class.getName();
 
-    void sendMail( String subject, String content, String toAddress, String toName, String fromAddress, String fromName )
-    	throws MailSenderException;
+    String TYPE_HTML = "html";
 
-    void sendMail( String subject, String content, String toAddress, String toName, String fromAddress, String fromName, Map extraHeaders )
-    	throws MailSenderException;
+    String TYPE_TEST = "test";
+
+    String getFromAddress();
+
+    void setFromAddress( String fromAddress);
+
+    String getFromName();
+
+    void setFromName( String fromName );
+
+    Map getToAddresses();
+
+    void addTo( String toName, String toAddress );
+
+    Map getCcAddresses();
+
+    void addCc( String ccName, String ccAddress );
+
+    Map getBccAddresses();
+
+    void addBcc( String bccName, String bccAddress );
+
+    String getSubject();
+
+    void setSubject( String subject );
+
+    String getContent();
+
+    void setContent( String content );
+
+    String getContentType();
+
+    void setContentType( String contentType );
+
+    Date getSendDate();
+
+    void setSendDate( Date sendDate );
+
+    Map getHeaders();
+
+    void addHeader( String headerName, String headerValue );
+
+    String getSmtpHost();
+
+    void setSmtpHost( String smtpHost );
+
+    int getSmtpPort();
+
+    void setSmtpPort( int smtpPort );
+
+    String getUsername();
+
+    void setUsername( String username );
+
+    void send() throws MailSenderException;
+
+    void send( String subject, String content, String toAddress, String toName, String fromAddress, String fromName )
+         throws MailSenderException;
+
+    void send( String subject, String content, String toAddress, String toName, String fromAddress, String fromName, Map extraHeaders )
+         throws MailSenderException;
 }
