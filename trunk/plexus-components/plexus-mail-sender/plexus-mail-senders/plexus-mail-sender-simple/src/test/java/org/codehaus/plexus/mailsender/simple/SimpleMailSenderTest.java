2d31f3b93a60eb433c3b9a7780111250ba66d712
evenisse <evenisse@30dfa635-7bf8-0310-9608-e41f2d776689>
2004-10-22 11:03:27 +0000

    Make mailsender-simple operational. Use mailsender-test for test.

--- trunk/plexus-components/plexus-mail-sender/plexus-mail-senders/plexus-mail-sender-simple/src/test/java/org/codehaus/plexus/mailsender/simple/SimpleMailSenderTest.java
+++ trunk/plexus-components/plexus-mail-sender/plexus-mail-senders/plexus-mail-sender-simple/src/test/java/org/codehaus/plexus/mailsender/simple/SimpleMailSenderTest.java
@@ -1,32 +1,111 @@
 package org.codehaus.plexus.mailsender.simple;
 
 /*
- * LICENSE
+ * The MIT License
+ *
+ * Copyright (c) 2004, The Codehaus
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
-import org.codehaus.plexus.PlexusTestCase;
+import com.dumbster.smtp.SmtpMessage;
+
+import java.util.Iterator;
+
 import org.codehaus.plexus.mailsender.MailSender;
+import org.codehaus.plexus.mailsender.MailMessage;
+import org.codehaus.plexus.mailsender.test.MailSenderTestCase;
+import org.codehaus.plexus.mailsender.test.SmtpServer;
 
 /**
  * @author <a href="mailto:trygvis@inamo.no">Trygve Laugst&oslash;l</a>
+ * @author <a href="mailto:evenisse@codehaus.org">Emmanuel Venisse</a>
  * @version $Id$
  */
 public class SimpleMailSenderTest
-	extends PlexusTestCase
+	extends MailSenderTestCase
 {
     public void testDefaultComponentConfiguration()
     	throws Exception
     {
+        startSmtpServer( 4000 );
+
         MailSender mailSender = (MailSender) lookup( MailSender.ROLE );
 
         assertNotNull( mailSender );
 
         assertTrue( mailSender instanceof SimpleMailSender );
 
-        SimpleMailSender simpleMailSender = (SimpleMailSender) mailSender;
+        assertEquals( "localhost", mailSender.getSmtpHost() );
 
-        assertEquals( "localhost", simpleMailSender.getSmtpHost() );
+        assertEquals( 4000, mailSender.getSmtpPort() );
 
-        assertEquals( 25, simpleMailSender.getSmtpPort() );
+        mailSender.send( "mySubject", "myContent", "to@server.com", "to.name", "from@server.com", "from.name" );
+
+        assertEquals( 1, getReceievedEmailSize() );
+
+        SmtpMessage email = (SmtpMessage) getReceivedEmail().next();
+        
+        assertTrue( email.getHeaderValue( "Subject" ).equals( "mySubject" ) );
+
+        assertTrue( email.getBody().equals( "myContent" ) );
+    }
+
+    public void testSend() throws Exception
+    {
+        startSmtpServer( 4000 );
+
+        try
+        {
+            sendMessage( 4000, "sender@here.com", "Test", "Test Body", "receiver@there.com" );
+        }
+        catch ( Exception e )
+        {
+            e.printStackTrace();
+            fail( "Unexpected exception: " + e );
+        }
+
+        stopSmtpServer();
+
+        assertTrue( getReceievedEmailSize() == 1 );
+
+        Iterator emailIter = getReceivedEmail();
+
+        SmtpMessage email = (SmtpMessage) emailIter.next();
+
+        assertTrue( email.getHeaderValue( "Subject" ).equals( "Test" ) );
+
+        assertTrue( email.getBody().equals( "Test Body" ) );
+    }
+
+    private void sendMessage(int port, String from, String subject, String body, String to) throws Exception
+    {
+        org.codehaus.plexus.mailsender.simple.MailMessage message = new org.codehaus.plexus.mailsender.simple.MailMessage( "localhost", port );
+
+        message.from( from );
+
+        message.to( to );
+
+        message.setSubject( subject );
+
+        message.getPrintStream().print( body );
+
+        message.sendAndClose();
     }
 }
