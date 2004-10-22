c85cf2179308743a34956be0a514c1392bf34618
evenisse <evenisse@30dfa635-7bf8-0310-9608-e41f2d776689>
2004-10-22 10:57:30 +0000

    - Add License - Add verify method

--- trunk/plexus-components/plexus-mail-sender/plexus-mail-sender-api/src/main/java/org/codehaus/plexus/mailsender/MailSender.java
+++ trunk/plexus-components/plexus-mail-sender/plexus-mail-sender-api/src/main/java/org/codehaus/plexus/mailsender/MailSender.java
@@ -1,7 +1,27 @@
 package org.codehaus.plexus.mailsender;
 
 /*
- * LICENSE
+ * The MIT License
+ *
+ * Copyright (c) 2004, The Codehaus
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 import java.util.Date;
@@ -9,29 +29,82 @@ import java.util.Map;
 
 /**
  * @author <a href="mailto:trygvis@inamo.no">Trygve Laugst&oslash;l</a>
+ * @author <a href="mailto:evenisse@codehaus.org">Emmanuel Venisse</a>
  * @version $Id$
  */
 public interface MailSender
 {
     String ROLE = MailSender.class.getName();
 
+    /**
+     * Get the smtp hostname.
+     *
+     * @return The hostname of smtp server
+     */
     String getSmtpHost();
 
+    /**
+     * Set the smtp hostname.
+     *
+     * @param smtpHost The hostname of smtp server
+     */
     void setSmtpHost( String smtpHost );
 
+    /**
+     * Get the smtp port.
+     *
+     * @return The port of smtp server
+     */
     int getSmtpPort();
 
+    /**
+     * Set the smtp port.
+     *
+     * @param smtpPort The port of smtp server
+     */
     void setSmtpPort( int smtpPort );
 
+    /**
+     * Get the smtp username.
+     *
+     * @return The username of smtp server
+     */
     String getUsername();
 
+    /**
+     * Set the smtp username.
+     *
+     * @param username The username of smtp server
+     */
     void setUsername( String username );
 
+    /**
+     * Send a mail.
+     *
+     * @throws MailSenderException
+     */
     void send( MailMessage message ) throws MailSenderException;
 
+    /**
+     * Send a mail message.
+     *
+     * @throws MailSenderException
+     */
     void send( String subject, String content, String toAddress, String toName, String fromAddress, String fromName )
          throws MailSenderException;
 
+    /**
+     * Send a mail message.
+     *
+     * @throws MailSenderException
+     */
     void send( String subject, String content, String toAddress, String toName, String fromAddress, String fromName, Map extraHeaders )
          throws MailSenderException;
+
+    /**
+     * Verify the content of a mail message.
+     *
+     * @throws MailSenderException
+     */
+    void verify( MailMessage message ) throws MailSenderException;
 }
