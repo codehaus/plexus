f991b3802acac2cbd009ad5d329237253242dade
evenisse <evenisse@30dfa635-7bf8-0310-9608-e41f2d776689>
2004-10-22 11:01:32 +0000

    Make mailsender-test operational for use in other mail senders.

--- plexus-components/plexus-mail-sender/plexus-mail-senders/plexus-mail-sender-test/src/main/java/org/codehaus/plexus/mailsender/test/MockSmtpServer.java
+++ plexus-components/plexus-mail-sender/plexus-mail-senders/plexus-mail-sender-test/src/main/java/org/codehaus/plexus/mailsender/test/MockSmtpServer.java
@@ -1,17 +1,47 @@
 package org.codehaus.plexus.mailsender.test;
 
-/**
- * License
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2004, The Codehaus
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 import com.dumbster.smtp.SimpleSmtpServer;
 
+import org.codehaus.plexus.personality.plexus.lifecycle.phase.Initializable;
+import org.codehaus.plexus.personality.plexus.lifecycle.phase.Startable;
+
 import java.util.Iterator;
 
+/**
+ * @author <a href="mailto:trygvis@inamo.no">Trygve Laugst&oslash;l</a>
+ * @author <a href="mailto:evenisse@codehaus.org">Emmanuel Venisse</a>
+ * @version $Id$
+ */
 public class MockSmtpServer
-    implements SmtpServer, Initilizable, Startable
+    implements SmtpServer, Initializable
 {
     private SimpleSmtpServer smtpServer;
+
+    private int port;
 
     /**
     * Get email received by this instance since start up.
@@ -34,13 +64,12 @@ public class MockSmtpServer
     public void initialize()
         throws Exception
     {
-        smtpServer = new SimpleSmtpServer( port );
     }
 
     public void start()
         throws Exception
     {
-        smtpServer.start();
+        smtpServer = SimpleSmtpServer.start( port );
     }
 
     public void stop()
