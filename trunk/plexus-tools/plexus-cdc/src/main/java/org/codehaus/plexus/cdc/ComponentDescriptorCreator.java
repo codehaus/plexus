0a616934a4de58c50ec298c871b0b4e7a906615c
jvanzyl <jvanzyl@30dfa635-7bf8-0310-9608-e41f2d776689>
2004-11-02 04:15:47 +0000

    o updated to use plexus-utils o pushed the gleaning code in one file, i think the pluggable gleaners   will come back.

--- trunk/plexus-tools/plexus-cdc/src/main/java/org/codehaus/plexus/cdc/ComponentDescriptorCreator.java
+++ trunk/plexus-tools/plexus-cdc/src/main/java/org/codehaus/plexus/cdc/ComponentDescriptorCreator.java
@@ -1,26 +1,22 @@
 package org.codehaus.plexus.cdc;
 
 import com.thoughtworks.qdox.JavaDocBuilder;
-import com.thoughtworks.qdox.model.DocletTag;
 import com.thoughtworks.qdox.model.JavaClass;
 import com.thoughtworks.qdox.model.JavaSource;
-import com.thoughtworks.qdox.model.Type;
-import com.thoughtworks.qdox.model.JavaField;
 import org.apache.maven.model.Dependency;
 import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.component.repository.ComponentDependency;
 import org.codehaus.plexus.component.repository.ComponentDescriptor;
 import org.codehaus.plexus.component.repository.ComponentRequirement;
 import org.codehaus.plexus.component.repository.ComponentSetDescriptor;
-import org.codehaus.plexus.configuration.xml.xstream.PlexusXStream;
+import org.codehaus.plexus.util.xml.PrettyPrintXMLWriter;
+import org.codehaus.plexus.util.xml.XMLWriter;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import java.util.HashMap;
 
 //!! Manager might not use a Map. Could use a container for per-lookup
 //!! Need to walk up through super classes looking for requirements
@@ -100,11 +96,13 @@ public class ComponentDescriptorCreator
 
         List componentDescriptors = new ArrayList();
 
+        ComponentGleaner gleaner = new ComponentGleaner();
+
         for ( int i = 0; i < javaSources.length; i++ )
         {
             JavaClass javaClass = getJavaClass( javaSources[i] );
 
-            ComponentDescriptor componentDescriptor = gleanComponent( builder, javaClass );
+            ComponentDescriptor componentDescriptor = gleaner.glean( builder, javaClass );
 
             if ( componentDescriptor != null && !javaClass.isAbstract() )
             {
@@ -117,10 +115,6 @@ public class ComponentDescriptorCreator
         componentSetDescriptor.setComponents( componentDescriptors );
 
         componentSetDescriptor.setDependencies( componentDependencies );
-
-        PlexusXStream xstream = new PlexusXStream();
-
-        String components = xstream.toXML( componentSetDescriptor );
 
         File f;
 
@@ -140,19 +134,164 @@ public class ComponentDescriptorCreator
 
         File outputFile = new File( f, "components.xml" );
 
+
         if ( !outputFile.getParentFile().exists() && !outputFile.getParentFile().mkdirs() )
         {
             throw new Exception( "Could not create parent directories for " + outputFile.getPath() );
         }
 
-        System.out.println( "Writing component descriptor to " + outputFile.getPath() );
+        // ----------------------------------------------------------------------
+        //
+        // ----------------------------------------------------------------------
 
         FileWriter writer = new FileWriter( outputFile );
 
-        writer.write( components );
+        XMLWriter w = new PrettyPrintXMLWriter( writer );
+
+        w.startElement( "component-set" );
+
+        w.startElement( "components" );
+
+        for ( Iterator i = componentDescriptors.iterator(); i.hasNext(); )
+        {
+            w.startElement( "component" );
+
+            ComponentDescriptor cd = (ComponentDescriptor) i.next();
+
+            element( w, "role", cd.getRole() );
+
+            element( w, "role-hint", cd.getRoleHint() );
+
+            element( w, "implementation", cd.getImplementation() );
+
+            element( w, "instantiation-strategy", cd.getInstantiationStrategy() );
+
+            // ----------------------------------------------------------------------
+            // Configuration
+            // ----------------------------------------------------------------------
+
+
+            // ----------------------------------------------------------------------
+            // Requirements
+            // ----------------------------------------------------------------------
+
+            if ( cd.getRequirements() != null && cd.getRequirements().size() != 0 )
+            {
+                w.startElement( "requirements" );
+
+                for ( Iterator j = cd.getRequirements().iterator(); j.hasNext(); )
+                {
+                    ComponentRequirement cr = (ComponentRequirement) j.next();
+
+                    w.startElement( "requirement" );
+
+                    element( w, "role", cr.getRole() );
+
+                    element( w, "role-hint", cr.getRoleHint() );
+
+                    element( w, "field-name", cr.getFieldName() );
+
+                    w.endElement();
+                }
+
+                w.endElement();
+            }
+
+            w.endElement();
+        }
+
+        w.endElement();
+
+        writeDependencies( w, componentSetDescriptor );
+
+        w.endElement();
 
         writer.close();
+
+        // ----------------------------------------------------------------------
+        //
+        // ----------------------------------------------------------------------
+
     }
+
+    public void writeDependencies( XMLWriter w, ComponentSetDescriptor pomDom )
+        throws Exception
+    {
+
+        w.startElement( "dependencies" );
+
+        List deps = pomDom.getDependencies();
+
+        if ( deps != null )
+        {
+            for ( int i = 0; i < deps.size(); i++ )
+            {
+                writeDependencyElement( (ComponentDependency) deps.get( i ), w );
+            }
+        }
+
+        w.endElement();
+    }
+
+    private void writeDependencyElement( ComponentDependency dependency, XMLWriter w )
+        throws Exception
+    {
+        w.startElement( "dependency" );
+
+        String groupId = dependency.getGroupId();
+
+        if ( groupId == null )
+        {
+            throw new Exception( "Missing dependency: 'groupId'." );
+        }
+
+        element( w, "groupId", groupId );
+
+        String artifactId = dependency.getArtifactId();
+
+        if ( artifactId == null )
+        {
+            throw new Exception( "Missing dependency: 'artifactId'." );
+        }
+
+        element( w, "artifactId", artifactId );
+
+        String type = dependency.getType();
+
+        if ( type != null )
+        {
+            element( w, "type", type );
+        }
+
+        String version = dependency.getVersion();
+
+        if ( version == null )
+        {
+            throw new Exception( "Missing dependency: 'version'." );
+        }
+
+        element( w, "version", version );
+
+        w.endElement();
+    }
+
+    public void element( XMLWriter w, String name, String value )
+    {
+        if ( value == null )
+        {
+            return;
+        }
+
+        w.startElement( name );
+
+        w.writeText( value );
+
+        w.endElement();
+    }
+
+    // ----------------------------------------------------------------------
+    //
+    // ----------------------------------------------------------------------
 
     private void initialize()
         throws Exception
@@ -190,358 +329,6 @@ public class ComponentDescriptorCreator
         return componentDependencies;
     }
 
-    private ComponentDescriptor gleanComponent( JavaDocBuilder builder, JavaClass javaClass )
-    {
-        String packageName = javaClass.getPackage();
-
-        DocletTag tag = javaClass.getTagByName( "component" );
-
-        if ( tag == null )
-        {
-            return null;
-        }
-
-        String className = javaClass.getName();
-
-        boolean isManager = false;
-
-        if ( className.endsWith( "Manager" ) )
-        {
-            isManager = true;
-        }
-
-        boolean isDefault = false;
-
-        if ( className.startsWith( "Default" ) )
-        {
-            isDefault = true;
-        }
-
-        ComponentDescriptor componentDescriptor = new ComponentDescriptor();
-
-        String version = tag.getNamedParameter( "version" );
-
-        if ( version != null )
-        {
-            componentDescriptor.setVersion( version );
-        }
-
-        String role = tag.getNamedParameter( "role" );
-
-        if ( role != null )
-        {
-            componentDescriptor.setRole( role );
-        }
-        else
-        {
-            if ( isManager )
-            {
-                // org.codehaus.foo.PluginManager
-                // org.codehaus.foo.DefaultPluginManager
-
-                componentDescriptor.setRole( packageName + "." + javaClass.getName().substring( 7 ) );
-            }
-            else if ( isDefault )
-            {
-                componentDescriptor.setRole( packageName + "." + javaClass.getName().substring( 7 ) );
-            }
-        }
-
-        String roleHint = tag.getNamedParameter( "roleHint" );
-
-        if ( roleHint != null )
-        {
-            componentDescriptor.setRoleHint( roleHint );
-        }
-        else
-        {
-            if ( !isManager && !isDefault )
-            {
-                findRoleHint( componentDescriptor, javaClass, javaClass.getName() );
-            }
-        }
-
-        componentDescriptor.setImplementation( javaClass.getFullyQualifiedName() );
-
-        DocletTag[] tags = javaClass.getTagsByName( "requirement" );
-
-        if ( tag != null )
-        {
-            for ( int i = 0; i < tags.length; i++ )
-            {
-                ComponentRequirement requirement = new ComponentRequirement();
-
-                String requirementRole = tag.getNamedParameter( "role" );
-
-                requirement.setRole( requirementRole );
-
-                componentDescriptor.addRequirement( requirement );
-            }
-        }
-
-        // ----------------------------------------------------------------------
-        // Handle the requirements for the manager
-        // ----------------------------------------------------------------------
-
-        if ( isManager )
-        {
-            // DefaultPluginManager
-            //
-            // So we need the goop between Default and Manager ... Plugin
-            // So lookup the Plugin class and use that to create the Map
-            // requirement.
-            //
-            //!! We need to look for the presence of the field, we might have
-            // a manager that uses a container to lookup per lookup components.
-
-            String s = className.substring( 7 );
-
-            s = s.substring( 0, s.length() - 7 );
-
-            JavaClass managed = findClassByName( builder, s );
-
-            if ( managed != null )
-            {
-                ComponentRequirement requirement = new ComponentRequirement();
-
-                requirement.setRole( managed.getFullyQualifiedName() );
-
-                String fieldName = uncapitalise( managed.getName() );
-
-                if ( fieldName.endsWith( "y" ) )
-                {
-                    fieldName = fieldName.substring( fieldName.length() - 1 ) + "ies";
-                }
-                else if ( fieldName.endsWith( "s" ) )
-                {
-                    fieldName = fieldName + "es";
-                }
-                else
-                {
-                    fieldName = fieldName + "s";
-                }
-
-                requirement.setFieldName( fieldName );
-
-                componentDescriptor.addRequirement( requirement );
-            }
-            else
-            {
-                // The interface for what this manager is supposed to be managing
-                // cannot be found which is not a good thing. There are warnings about
-                // component interfaces not having a ROLE field.
-
-                System.out.println( "Check the errors to make sure the interface of the managed class has a ROLE field." );
-
-                System.out.println( "This may be why the interface could not be found because it was not recorded as a component interface." );
-            }
-        }
-
-        findRequirements( javaClass, componentDescriptor );
-
-        return componentDescriptor;
-    }
-
-    private void findRequirements( JavaClass javaClass, ComponentDescriptor componentDescriptor )
-    {
-        JavaField[] fields = javaClass.getFields();
-
-        for ( int i = 0; i < fields.length; i++ )
-        {
-            JavaField field = fields[i];
-
-            String classType = field.getType().getValue();
-
-            if ( field.getClass().isInterface() )
-            {
-                // If this is not a primitive field then we will attempt
-                // to look for an interface without our set of sources
-                // that is a component and try to set it as a requirement
-                //
-                // Right now we are not doing anything to search out cases
-                // where the requirement is external. We will need to build
-                // up a little database of some kind that can be queried for
-                // all the available components. But this will do for now as
-                // the requirement can be set explicity.
-
-                String roleName = classType.substring( classType.lastIndexOf( "." ) + 1 );
-
-                JavaClass jc = (JavaClass) componentCache.get( roleName );
-
-                if ( jc != null )
-                {
-                    ComponentRequirement cr = new ComponentRequirement();
-
-                    cr.setRole( classType );
-
-                    componentDescriptor.addRequirement( cr );
-                }
-            }
-        }
-
-        if ( javaClass.getSuperJavaClass() != null )
-        {
-            findRequirements( javaClass.getSuperJavaClass(), componentDescriptor );
-        }
-    }
-
-
-    // Qdox doesn't keep a map of short name to JavaClass
-    private Map componentCache;
-
-    private JavaClass findClassByName( JavaDocBuilder builder, String name )
-    {
-        // We are trying to cache classes that are components and
-        // interfaces for components.
-        //
-        // Component classes are designated with the @component tag
-        // Component interfaces contain a field named ROLE.
-
-        if ( componentCache == null )
-        {
-            componentCache = new HashMap();
-
-            JavaSource[] javaSources = builder.getSources();
-
-            for ( int i = 0; i < javaSources.length; i++ )
-            {
-                JavaClass f = getJavaClass( javaSources[i] );
-
-                if ( f.getTagByName( "component" ) != null )
-                {
-                    componentCache.put( f.getName(), f );
-                }
-                else if ( f.isInterface() )
-                {
-                    boolean hasRoleField = false;
-
-                    JavaField[] fields = f.getFields();
-
-                    for ( int j = 0; j < fields.length; j++ )
-                    {
-                        if ( fields[j].getName().equals( "ROLE" ) )
-                        {
-                            hasRoleField = true;
-
-                            break;
-                        }
-                    }
-
-                    if ( hasRoleField )
-                    {
-                        componentCache.put( f.getName(), f );
-                    }
-                    else
-                    {
-                        System.out.println( f.getFullyQualifiedName() + " is an interface but doesn't have a ROLE field!" );
-
-                        System.out.println( "If this is a plexus component interface it should have a ROLE field." );
-                    }
-                }
-            }
-        }
-
-        return (JavaClass) componentCache.get( name );
-    }
-
-    private void findRoleHint( ComponentDescriptor cd, JavaClass javaClass, String startingClassName )
-    {
-        String roleHint = null;
-
-        String className = startingClassName;
-
-        Type[] types = javaClass.getImplements();
-
-        for ( int i = 0; i < types.length; i++ )
-        {
-            String interfaceName = types[i].getValue();
-
-            String roleName = interfaceName.substring( interfaceName.lastIndexOf( "." ) + 1 );
-
-            // org.codehaus.foo.Sink
-            // org.codehaus.bar.BigSink
-
-            if ( className.endsWith( roleName ) )
-            {
-                roleHint = className.substring( 0, className.length() - roleName.length() );
-
-                roleHint = addAndDeHump( roleHint );
-
-                cd.setRoleHint( roleHint );
-
-                cd.setRole( interfaceName );
-            }
-        }
-
-        // ----------------------------------------------------------------------
-        // We back track up the hierarchy until we find an implementation that
-        // matches the naming convention of the class in question. The default
-        // plexus naming conventions are assumed.
-        // ----------------------------------------------------------------------
-
-        // org.codehaus.foo.Sink
-        // ^
-        // |
-        // org.codehaus.foo.AbstractSink
-        // ^
-        // |
-        // org.codehaus.foo.AbstractXhtmlSink
-        // ^
-        // |
-        // org.codehaus.bar.CodehausXhtmlSink
-
-        JavaClass jc = javaClass.getSuperJavaClass();
-
-        if ( jc != null )
-        {
-            types = javaClass.getImplements();
-
-            findRoleHint( cd, jc, startingClassName );
-        }
-    }
-
-    private JavaClass getJavaClass( JavaSource javaSource )
-    {
-        return javaSource.getClasses()[0];
-    }
-
-    private String addAndDeHump( String view )
-    {
-        StringBuffer sb = new StringBuffer();
-
-        for ( int i = 0; i < view.length(); i++ )
-        {
-            if ( i != 0 &&
-                Character.isUpperCase( view.charAt( i ) ) )
-            {
-                sb.append( '-' );
-            }
-
-            sb.append( view.charAt( i ) );
-        }
-
-        return sb.toString().trim().toLowerCase();
-    }
-
-    private String uncapitalise( String str )
-    {
-        if ( str == null )
-        {
-            return null;
-        }
-        else if ( str.length() == 0 )
-        {
-            return "";
-        }
-        else
-        {
-            return new StringBuffer( str.length() )
-                .append( Character.toLowerCase( str.charAt( 0 ) ) )
-                .append( str.substring( 1 ) )
-                .toString();
-        }
-    }
-
     private Class[] primitiveClasses = new Class[]{
         String.class,
         Boolean.class, Boolean.TYPE,
@@ -565,5 +352,10 @@ public class ComponentDescriptorCreator
         }
 
         return false;
+    }
+
+    private JavaClass getJavaClass( JavaSource javaSource )
+    {
+        return javaSource.getClasses()[0];
     }
 }
