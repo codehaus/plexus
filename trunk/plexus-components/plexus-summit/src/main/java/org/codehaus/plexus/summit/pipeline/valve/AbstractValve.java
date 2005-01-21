0b150a8a20fda83af8379637ee7ca017e79abcff
dandiep <dandiep@30dfa635-7bf8-0310-9608-e41f2d776689>
2005-01-21 15:39:43 +0000

    Make valves components. If a valve component can't be found, try to instantiate it directly. New syntax:
    <valve>valve.role</valve>
    
    The <lifecycle-handler> on SummitPipeline must be "plexus-configurable"

--- trunk/plexus-components/plexus-summit/src/main/java/org/codehaus/plexus/summit/pipeline/valve/AbstractValve.java
+++ trunk/plexus-components/plexus-summit/src/main/java/org/codehaus/plexus/summit/pipeline/valve/AbstractValve.java
@@ -1,12 +1,6 @@
 package org.codehaus.plexus.summit.pipeline.valve;
 
-import java.io.IOException;
-
-import org.codehaus.plexus.summit.exception.SummitException;
-import org.codehaus.plexus.summit.rundata.RunData;
-import org.codehaus.plexus.summit.AbstractSummitComponent;
 import org.codehaus.plexus.logging.AbstractLogEnabled;
-import org.codehaus.plexus.PlexusContainer;
 
 /**
  * Valve that can be used as the basis of Valve implementations.
@@ -18,26 +12,6 @@ public abstract class AbstractValve
     extends AbstractLogEnabled
     implements Valve
 {
-    private PlexusContainer container;
-
-    public void initialize()
-        throws Exception
-    {
-    }
-
-    public abstract void invoke( RunData data )
-        throws IOException, SummitException;
-
-    public PlexusContainer getServiceManager()
-    {
-        return container;
-    }
-
-    public void setServiceManager( PlexusContainer container )
-    {
-        this.container = container;
-    }
-
 
 }
 
---
+++ trunk/plexus-components/plexus-summit/src/test/java/org/codehaus/plexus/summit/pipeline/PipelineTest.java
@@ -0,0 +1,17 @@
+package org.codehaus.plexus.summit.pipeline;
+
+import org.codehaus.plexus.PlexusTestCase;
+
+/**
+ * @author <a href="mailto:dan@envoisolutions.com">Dan Diephouse</a>
+ */
+public class PipelineTest
+    extends PlexusTestCase
+{
+    public void testPipeline()
+        throws Exception
+    {
+        Pipeline pipeline = (Pipeline) lookup(Pipeline.ROLE);
+        assertNotNull(pipeline);
+    }
+}
