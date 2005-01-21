6b233908d434a257f4b8a402d5dd978c74f03174
dandiep <dandiep@30dfa635-7bf8-0310-9608-e41f2d776689>
2005-01-21 20:19:45 +0000

    Rename rendererResolver to TemplateRenderer and add a component definition.

--- plexus-components/plexus-summit/src/main/java/org/codehaus/plexus/summit/pull/tools/TemplateRenderer.java
+++ plexus-components/plexus-summit/src/main/java/org/codehaus/plexus/summit/pull/tools/TemplateRenderer.java
@@ -2,7 +2,8 @@ package org.codehaus.plexus.summit.pull.tools;
 
 import org.codehaus.plexus.summit.pull.RequestTool;
 import org.codehaus.plexus.summit.renderer.Renderer;
-import org.codehaus.plexus.summit.resolver.ClassicResolver;
+import org.codehaus.plexus.summit.resolver.AbstractResolver;
+import org.codehaus.plexus.summit.resolver.Resolution;
 import org.codehaus.plexus.summit.rundata.RunData;
 import org.codehaus.plexus.summit.view.View;
 
@@ -14,11 +15,13 @@ import org.codehaus.plexus.summit.view.View;
  * @author <a href="dan@envoisolutions.com">Dan Diephouse</a>
  * @since Feb 13, 2003
  */
-public class RendererResolver
-    extends ClassicResolver
+public class TemplateRenderer
+    extends AbstractResolver
     implements RequestTool
 {
     private RunData data;
+
+    private Renderer renderer;
     
     public void setRunData(RunData data)
     {
@@ -33,10 +36,20 @@ public class RendererResolver
     public String render( String basedir, String target ) 
         throws Exception
     {
-        Renderer renderer = ( Renderer ) lookup( Renderer.ROLE );
-
         View view = getView( target, basedir );
 
-        return renderer.render( data, view.getName() );
+        return getRenderer( target ).render( data, view.getName() );
+    }
+
+    protected Renderer getRenderer(String target)
+        throws Exception
+    {
+        return renderer;
+    }
+
+    public Resolution resolve(String view)
+        throws Exception
+    {
+        return null;
     }
 }
