44e154934d787266970092ba07ba46cfe8d447ee
trygvis <trygvis@30dfa635-7bf8-0310-9608-e41f2d776689>
2005-01-28 12:28:17 +0000

    o Updating the existing mojos so they use the new build() method in the   builder. o Adding mojos to bundle the application and runtime.

---
+++ trunk/plexus-maven-plugin/src/main/java/org/codehaus/plexus/maven/plugin/PlexusBundleRuntimeMojo.java
@@ -0,0 +1,92 @@
+package org.apache.maven.plugin.plexus;
+
+/*
+ * Copyright (c) 2004, Codehaus.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import java.io.File;
+
+import org.apache.maven.plugin.AbstractPlugin;
+import org.apache.maven.plugin.PluginExecutionRequest;
+import org.apache.maven.plugin.PluginExecutionResponse;
+
+import org.codehaus.plexus.builder.runtime.PlexusRuntimeBuilder;
+
+/**
+ * @goal bundle-runtime
+ *
+ * @requiresDependencyResolution
+ *
+ * @description Packages the runtime into a redistributable jar file.
+ *
+ * @prereq plexus:runtime
+ *
+ * @parameter name="basedir"
+ * type="String"
+ * required="true"
+ * validator=""
+ * expression="#basedir"
+ * description=""
+ *
+ * @parameter name="finalName"
+ * type="java.lang.String"
+ * required="true"
+ * validator=""
+ * expression="#maven.final.name"
+ * description=""
+ *
+ * @parameter name="runtimeBuilder"
+ * type="org.codehaus.plexus.builder.runtime.PlexusRuntimeBuilder"
+ * required="true"
+ * validator=""
+ * expression="#component.org.codehaus.plexus.builder.runtime.PlexusRuntimeBuilder"
+ * description=""
+ *
+ * @author <a href="mailto:trygvis@inamo.no">Trygve Laugst&oslash;l</a>
+ * @version $Id$
+ */
+public class PlexusBundleRuntimeMojo
+    extends AbstractPlugin
+{
+    public void execute( PluginExecutionRequest request, PluginExecutionResponse response )
+        throws Exception
+    {
+        // ----------------------------------------------------------------------
+        //
+        // ----------------------------------------------------------------------
+
+        String basedir = (String) request.getParameter( "basedir" );
+
+        String finalName = (String) request.getParameter( "finalName" );
+
+        PlexusRuntimeBuilder builder = (PlexusRuntimeBuilder) request.getParameter( "runtimeBuilder" );
+
+        // ----------------------------------------------------------------------
+        //
+        // ----------------------------------------------------------------------
+
+        File runtimeDirectory = new File( basedir, "plexus-runtime" );
+
+        File outputFile = new File( basedir, finalName + "-runtime.jar" );
+
+        builder.bundle( outputFile, runtimeDirectory );
+    }
+}
