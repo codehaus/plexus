6ccfcb5542b2cfcb81aea275239e76ca2e7d9858
nobody <nobody@30dfa635-7bf8-0310-9608-e41f2d776689>
2005-02-12 20:28:04 +0000

    This commit was manufactured by cvs2svn to create branch 'CH'.

---
+++ plexus-sandbox/plexus-taskqueue/src/main/java/org/codehaus/plexus/taskqueue/TaskViabilityEvaluator.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2005 Your Corporation. All Rights Reserved.
+ */
+package org.codehaus.plexus.taskqueue;
+
+/**
+ * @author <a href="mailto:jason@maven.org">Jason van Zyl</a>
+ * @version $Id$
+ */
+public interface TaskViabilityEvaluator
+{
+}
