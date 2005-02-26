2e122c0515f5565dd0e00a1a5f7ccf4d7e692cdf
jdcasey <jdcasey@30dfa635-7bf8-0310-9608-e41f2d776689>
2005-02-26 06:18:42 +0000

    o Fixed problems with classloading and discovery of marmalade taglibs.

--- trunk/plexus-component-factories/plexus-marmalade-factory/src/test/java/org/codehaus/plexus/component/factory/marmalade/MarmaladeClasspathComponentFactoryTest.java
+++ trunk/plexus-component-factories/plexus-marmalade-factory/src/test/java/org/codehaus/plexus/component/factory/marmalade/MarmaladeClasspathComponentFactoryTest.java
@@ -1,17 +1,14 @@
 /* Created on Aug 9, 2004 */
 package org.codehaus.plexus.component.factory.marmalade;
 
+import org.codehaus.classworlds.ClassRealm;
+import org.codehaus.classworlds.ClassWorld;
+import org.codehaus.plexus.component.repository.ComponentDescriptor;
+import org.codehaus.plexus.embed.Embedder;
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
-import java.io.IOException;
-
-import org.codehaus.classworlds.ClassRealm;
-import org.codehaus.classworlds.ClassWorld;
-import org.codehaus.classworlds.DefaultClassRealm;
-import org.codehaus.plexus.DefaultPlexusContainer;
-import org.codehaus.plexus.component.factory.ComponentInstantiationException;
-import org.codehaus.plexus.component.repository.ComponentDescriptor;
 
 import junit.framework.TestCase;
 
@@ -23,7 +20,7 @@ public class MarmaladeClasspathComponentFactoryTest extends TestCase {
     private static final String TEST_SCRIPT = "<?xml version=\"1.0\"?>" +
         "<testScript xmlns=\"marmalade:org.codehaus.plexus.component.factory.marmalade.TestTaglib\"/>";
     
-    public void testShouldLoadComponentFromScript() throws IOException, ComponentInstantiationException {
+    public void testShouldLoadComponentFromScript() throws Exception {
         File file = new File("test.mmld");
         try {
             BufferedWriter out = new BufferedWriter(new FileWriter(file));
@@ -33,14 +30,25 @@ public class MarmaladeClasspathComponentFactoryTest extends TestCase {
             
             MarmaladeClasspathComponentFactory factory = new MarmaladeClasspathComponentFactory();
             
-            DefaultPlexusContainer container = new DefaultPlexusContainer();
-            ClassRealm realm = new DefaultClassRealm(new ClassWorld(), "test");
-            realm.addConstituent(new File(".").toURL());
+            Embedder container = new Embedder();
+            ClassWorld world = new ClassWorld();
+            
+            String role="role";
+            String hint = "hint";
+            
+            String key = role + hint;
+            
+            container.start(world);
+            
+            ClassRealm realm = container.getContainer().getComponentRealm(key);
+            realm.addConstituent(file.getAbsoluteFile().getParentFile().toURL());
             
             ComponentDescriptor descriptor = new ComponentDescriptor();
+            descriptor.setRole(role);
+            descriptor.setRoleHint(hint);
             descriptor.setImplementation(file.getPath());
             
-            Object component = factory.newInstance(descriptor, realm, container);
+            Object component = factory.newInstance(descriptor, realm, container.getContainer());
             
             assertNotNull(component);
             assertEquals("TestComponent", component);
