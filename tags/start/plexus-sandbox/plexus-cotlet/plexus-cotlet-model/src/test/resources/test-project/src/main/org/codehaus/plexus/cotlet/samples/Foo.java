f145509b083a771c10f4786d4f0d6b73419dfe5e
nobody <nobody@30dfa635-7bf8-0310-9608-e41f2d776689>
2005-03-12 01:28:21 +0000

    This commit was manufactured by cvs2svn to create tag 'start'.

---
+++ tags/start/plexus-sandbox/plexus-cotlet/plexus-cotlet-model/src/test/resources/test-project/src/main/org/codehaus/plexus/cotlet/samples/Foo.java
@@ -0,0 +1,23 @@
+package org.codehaus.plexus.cotlet.samples;
+
+/**
+ */
+public class Foo
+{
+    /**
+     *
+     * A.
+     * @component.configuration
+     */
+    private int a;
+
+    /**
+     *
+     * B.
+     *
+     * @component.configuration
+     */
+    private String b;
+
+
+}
