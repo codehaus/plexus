8487d2e823a274a3a9f1a9e416ad47541675000f
trygvis <trygvis@30dfa635-7bf8-0310-9608-e41f2d776689>
2005-03-21 12:59:54 +0000

    o Updating WRT the latest Maven API.

--- trunk/plexus-tools/plexus-runtime-builder/src/test/java/org/codehaus/plexus/builder/runtime/PlexusRuntimeBuilderTest.java
+++ trunk/plexus-tools/plexus-runtime-builder/src/test/java/org/codehaus/plexus/builder/runtime/PlexusRuntimeBuilderTest.java
@@ -31,8 +31,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.artifact.repository.ArtifactRepositoryFactory;
+import org.apache.maven.artifact.repository.layout.DefaultRepositoryLayout;
+import org.apache.maven.artifact.repository.layout.ArtifactRepositoryLayout;
 import org.apache.maven.artifact.DefaultArtifact;
 import org.apache.maven.artifact.Artifact;
+import org.apache.maven.settings.MavenSettings;
+import org.apache.maven.model.Repository;
 
 import org.codehaus.plexus.PlexusTestCase;
 import org.codehaus.plexus.util.FileUtils;
@@ -49,6 +54,7 @@ public class PlexusRuntimeBuilderTest
     {
         PlexusRuntimeBuilder runtimeBuilder = (PlexusRuntimeBuilder) lookup( PlexusRuntimeBuilder.ROLE );
 
+        ArtifactRepositoryFactory artifactRepositoryFactory = (ArtifactRepositoryFactory) lookup( ArtifactRepositoryFactory.ROLE );
         // ----------------------------------------------------------------------
         // Clean the output directory
         // ----------------------------------------------------------------------
@@ -63,7 +69,17 @@ public class PlexusRuntimeBuilderTest
 
         List remoteRepositories = new ArrayList();
 
-        ArtifactRepository localRepository = new ArtifactRepository( "local", "file://" + getTestFile( "src/test/repository" ).getAbsolutePath() );
+        Repository repository = new Repository();
+
+        repository.setId( "local" );
+        repository.setUrl( "file://" + getTestFile( "src/test/repository" ).getAbsolutePath() );
+
+        MavenSettings settings = new MavenSettings();
+
+        ArtifactRepositoryLayout repositoryLayout = (ArtifactRepositoryLayout) lookup( ArtifactRepositoryLayout.ROLE, "legacy" );
+
+        ArtifactRepository localRepository = artifactRepositoryFactory.
+            createArtifactRepository( repository, settings, repositoryLayout );
 
         Set extraArtifacts = new HashSet();
 /*
