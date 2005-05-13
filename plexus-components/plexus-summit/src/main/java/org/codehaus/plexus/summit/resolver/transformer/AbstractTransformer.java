fde28e95b529f93b8cca70563d14ba7cbbaf4daf
nobody <nobody@30dfa635-7bf8-0310-9608-e41f2d776689>
2005-05-13 03:17:24 +0000

    This commit was manufactured by cvs2svn to create tag 'PLEXUS_CONTAINERS_1_0_ALPHA_3'.

--- plexus-components/plexus-summit/src/main/java/org/codehaus/plexus/summit/resolver/transformer/AbstractTransformer.java
+++
@@ -1,8 +0,0 @@
-package org.codehaus.plexus.summit.resolver.transformer;
-
-public abstract class AbstractTransformer
-    implements Transformer
-{
-    public abstract String transform( String view )
-        throws Exception;
-}
