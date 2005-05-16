b399a9b39cb498a9153f9bff497a7e4780e0f40f
jvanzyl <jvanzyl@30dfa635-7bf8-0310-9608-e41f2d776689>
2005-05-16 19:06:43 +0000

    o turfing the idea of java based form renderers, just too inflexible.

--- trunk/plexus-components/plexus-formica-web/src/main/java/org/codehaus/plexus/formica/action/EntityAction.java
+++ trunk/plexus-components/plexus-formica-web/src/main/java/org/codehaus/plexus/formica/action/EntityAction.java
@@ -1,9 +1,5 @@
 package org.codehaus.plexus.formica.action;
 
-import org.codehaus.plexus.formica.Form;
-
-import java.util.Map;
-
 /**
  * @author <a href="mailto:jason@maven.org">Jason van Zyl</a>
  * @version $Id$
