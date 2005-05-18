e2b499dc49d5224680d0820672f693c2bc3cb155
jvanzyl <jvanzyl@30dfa635-7bf8-0310-9608-e41f2d776689>
2005-05-18 16:04:30 +0000

    o using URL.toExternalForm()

--- plexus-appserver/src/main/java/org/codehaus/plexus/application/deploy/DefaultApplicationDeployer.java
+++ plexus-appserver/src/main/java/org/codehaus/plexus/application/deploy/DefaultApplicationDeployer.java
@@ -115,7 +115,7 @@ public class DefaultApplicationDeployer
             throw new Exception( "This deployer can only deploy *.jar files." );
         }
 
-        File file = new File( new URI( url.toString() ) );
+        File file = new File( new URI( url.toExternalForm() ) );
 
         deployJar( file, applicationsDirectory );
     }
