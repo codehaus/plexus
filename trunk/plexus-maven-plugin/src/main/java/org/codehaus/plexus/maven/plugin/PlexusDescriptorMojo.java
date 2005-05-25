5ebb3cd528281598929ee1f80d7384d384746373
trygvis <trygvis@30dfa635-7bf8-0310-9608-e41f2d776689>
2005-05-25 10:47:44 +0000

    o Updating to use the new CDC.

--- trunk/plexus-maven-plugin/src/main/java/org/codehaus/plexus/maven/plugin/PlexusDescriptorMojo.java
+++ trunk/plexus-maven-plugin/src/main/java/org/codehaus/plexus/maven/plugin/PlexusDescriptorMojo.java
@@ -1,8 +1,32 @@
 package org.codehaus.plexus.maven.plugin;
 
+/*
+ * Copyright (c) 2004-2005, Codehaus.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+import java.io.File;
+import java.util.List;
+import java.util.Iterator;
+
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.project.MavenProject;
 
 import org.codehaus.plexus.cdc.ComponentDescriptorCreator;
 
@@ -19,41 +43,51 @@ import org.codehaus.plexus.cdc.ComponentDescriptorCreator;
 public class PlexusDescriptorMojo
     extends AbstractMojo
 {
-    /**
-     * @parameter expression="${basedir}"
-     *
-     * @required
-     */
-    private String basedir;
+    // ----------------------------------------------------------------------
+    // Parameters
+    // ----------------------------------------------------------------------
 
     /**
-     * @parameter expression="${project}"
-     *
+     * @parameter expression="${project.compileSourceRoots}"
      * @required
      */
-    private MavenProject project;
+    private List sourceDirectories;
 
     /**
      * @parameter expression="${project.build.outputDirectory}"
      *
      * @required
      */
-    private String output;
+    private File output;
+
+    // ----------------------------------------------------------------------
+    // Components
+    // ----------------------------------------------------------------------
+
+    /**
+     * @parameter expression="${component.org.codehaus.plexus.cdc.ComponentDescriptorCreator}"
+     *
+     * @required
+     */
+    private ComponentDescriptorCreator cdc;
 
     public void execute()
         throws MojoExecutionException
     {
-        ComponentDescriptorCreator cdc = new ComponentDescriptorCreator();
+        File[] sources = new File[ sourceDirectories.size() ];
 
-        cdc.setBasedir( basedir );
+        Iterator it = sourceDirectories.iterator();
 
-        cdc.setProject( project );
+        for ( int i = 0; i < sources.length; i++ )
+        {
+            sources[ i ] = new File( (String) it.next() );
+        }
 
-        cdc.setDestDir( output + "/META-INF/plexus" );
+        File outputDirectory = new File( output, "/META-INF/plexus" );
 
         try
         {
-            cdc.execute();
+            cdc.processSources( sources, outputDirectory );
         }
         catch ( Exception e )
         {
