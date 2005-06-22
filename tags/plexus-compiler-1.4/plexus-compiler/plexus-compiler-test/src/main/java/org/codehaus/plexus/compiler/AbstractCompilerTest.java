443feeb0f94475cc9a260123a07bae398e0b3a8d
jdcasey <jdcasey@30dfa635-7bf8-0310-9608-e41f2d776689>
2005-06-22 07:59:13 +0000

    releasing

---
+++ tags/plexus-compiler-1.4/plexus-compiler/plexus-compiler-test/src/main/java/org/codehaus/plexus/compiler/AbstractCompilerTest.java
@@ -0,0 +1,81 @@
+package org.codehaus.plexus.compiler;
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.DefaultArtifact;
+import org.apache.maven.artifact.test.ArtifactTestCase;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+public abstract class AbstractCompilerTest
+    extends ArtifactTestCase
+{
+    private Compiler compiler = null;
+
+    protected String roleHint;
+
+    private CompilerConfiguration compilerConfig;
+
+    public AbstractCompilerTest()
+    {
+        super();
+    }
+
+    protected abstract String getRoleHint();
+
+    public void setUp() throws Exception
+    {
+        super.setUp();
+
+        String basedir = getBasedir();
+
+        basedir = System.getProperty( "basedir" );
+
+        compiler = (Compiler) lookup( Compiler.ROLE, getRoleHint() );
+
+        compilerConfig = new CompilerConfiguration();
+
+        compilerConfig.setClasspathEntries( getClasspath() );
+
+        compilerConfig.addSourceLocation( basedir + "/src/test-input/src/main" );
+
+        compilerConfig.setOutputLocation( basedir + "/target/" + getRoleHint() + "/classes" );
+
+        compilerConfig.addInclude( "**/*.java" );
+    }
+
+    protected List getClasspath()
+        throws Exception
+    {
+        Artifact artifact = new DefaultArtifact( "commons-lang", "commons-lang", "2.0", "jar" );
+
+        List cp = new ArrayList();
+
+        cp.add( getLocalArtifactPath( artifact ) );
+
+        return cp;
+    }
+
+    protected CompilerConfiguration getCompilerConfiguration()
+    {
+        return compilerConfig;
+    }
+    
+    public void testCompilingSources() throws Exception
+    {
+        List messages = compiler.compile( compilerConfig );
+
+        for ( Iterator iter = messages.iterator(); iter.hasNext(); )
+        {
+            System.out.println( iter.next() );
+        }
+        assertEquals( "Expected number of compilation errors is" + expectedErrors(), expectedErrors(), messages.size() );
+    }
+
+    protected int expectedErrors()
+    {
+        return 1;
+    }
+}
