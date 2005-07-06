67507f2129f5833914306e7946e8774537762817
trygvis <trygvis@30dfa635-7bf8-0310-9608-e41f2d776689>
2005-07-06 09:46:57 +0000

    o Removing System.out usage.

--- trunk/plexus-components/plexus-summit/src/main/java/org/codehaus/plexus/summit/display/ClassicBufferedDisplay.java
+++ trunk/plexus-components/plexus-summit/src/main/java/org/codehaus/plexus/summit/display/ClassicBufferedDisplay.java
@@ -95,8 +95,6 @@ public class ClassicBufferedDisplay
 
         View layoutView = data.getResolution().getView( ClassicResolver.LAYOUT_VIEW );
 
-        System.out.println( "renderer = " + renderer );
-
         renderer.render( data, layoutView.getName(), getWriter( data ) );
     }
 }
