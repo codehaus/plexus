8235dee6e5620f0f2e11de7ee416477b092176fb
jvanzyl <jvanzyl@30dfa635-7bf8-0310-9608-e41f2d776689>
2005-07-15 22:34:39 +0000

    updated to work with plexus

--- plexus-sandbox/plexus-quartz/src/test/java/org/codehaus/plexus/scheduler/SchedulerTest.java
+++ plexus-sandbox/plexus-quartz/src/test/java/org/codehaus/plexus/scheduler/SchedulerTest.java
@@ -6,34 +6,12 @@ import java.io.File;
 
 public class SchedulerTest extends PlexusTestCase
 {
-    public SchedulerTest(String name)
-    {
-        super( name );
-    }
-
     public void testCreation()
         throws Exception
     {
-        Scheduler scheduler = (Scheduler) getComponent( Scheduler.ROLE );
+        Scheduler scheduler = (Scheduler) lookup( Scheduler.ROLE );
 
         assertNotNull( scheduler );
-    }
-
-    public void testAddJobFile()
-        throws Exception
-    {
-        Scheduler scheduler = (Scheduler) getComponent( Scheduler.ROLE );
-
-        File jobFile = new File( getTestInputDir(), "plexus-jobs-test-0.1.jar" );
-        scheduler.addJobFile( jobFile );
-        Thread.sleep( 7000 );
-    }
-
-    protected File getTestInputDir()
-    {
-        String baseDir = System.getProperty( "basedir" );
-        File testInputDir = new File ( baseDir, "target/test-plexus/work/jobs" );
-        return testInputDir;
     }
 }
 
