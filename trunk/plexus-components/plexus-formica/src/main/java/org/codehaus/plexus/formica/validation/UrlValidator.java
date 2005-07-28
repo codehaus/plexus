13e2b4c4e6acde62456cdfd3134ddb81398e2674
evenisse <evenisse@30dfa635-7bf8-0310-9608-e41f2d776689>
2005-07-28 13:50:06 +0000

    o Update to the latest UrlValidator from jakarta commons validator o Configure default allowed schemes in configuration

--- trunk/plexus-components/plexus-formica/src/main/java/org/codehaus/plexus/formica/validation/UrlValidator.java
+++ trunk/plexus-components/plexus-formica/src/main/java/org/codehaus/plexus/formica/validation/UrlValidator.java
@@ -23,6 +23,7 @@ package org.codehaus.plexus.formica.validation;
  */
 
 import org.apache.oro.text.perl.Perl5Util;
+import org.codehaus.plexus.formica.validation.util.Flags;
 
 import java.util.Arrays;
 import java.util.HashSet;
@@ -40,14 +41,14 @@ import java.util.Set;
  * included then fragments are flagged as illegal.</li>
  * <li>ALLOW_ALL_SCHEMES - [FALSE] By default only http, https, and ftp are
  * considered valid schemes.  Enabling this option will let any scheme pass validation.</li>
- * <p/>
+ *
  * <p>Originally based in on php script by Debbie Dyer, validation.php v1.2b, Date: 03/07/02,
  * http://javascript.internet.com. However, this validation now bears little resemblance
  * to the php original.</p>
  * <pre>
  *   Example of usage:
  *   Construct a UrlValidator with valid schemes of "http", and "https".
- * <p/>
+ *
  *    String[] schemes = {"http","https"}.
  *    Urlvalidator urlValidator = new Urlvalidator(schemes);
  *    if (urlValidator.isValid("ftp")) {
@@ -55,22 +56,20 @@ import java.util.Set;
  *    } else {
  *       System.out.println("url is invalid");
  *    }
- * <p/>
+ *
  *    prints "url is invalid"
  *   If instead the default constructor is used.
- * <p/>
+ *
  *    Urlvalidator urlValidator = new Urlvalidator();
  *    if (urlValidator.isValid("ftp")) {
  *       System.out.println("url is valid");
  *    } else {
  *       System.out.println("url is invalid");
  *    }
- * <p/>
+ *
  *   prints out "url is valid"
  *  </pre>
  *
- * @author Robert Leland
- * @author David Graham
  * @version $Revision$ $Date$
  * @see <a href='http://www.ietf.org/rfc/rfc2396.txt' >
  *      Uniform Resource Identifiers (URI): Generic Syntax
@@ -80,86 +79,97 @@ public class UrlValidator
     extends AbstractValidator
 {
     /**
-     * Allows all validly formatted schemes to pass validation instead of supplying a
-     * set of valid schemes.
+     * Allows all validly formatted schemes to pass validation instead of 
+     * supplying a set of valid schemes.
      */
-    public static int ALLOW_ALL_SCHEMES = 1;
+    public static final int ALLOW_ALL_SCHEMES = 1 << 0;
 
+    /**
+     * Allow two slashes in the path component of the URL.
+     */
+    public static final int ALLOW_2_SLASHES = 1 << 1;
 
-    private static String ALPHA_CHARS = "a-zA-Z";
+    /**
+     * Enabling this options disallows any URL fragments.
+     */
+    public static final int NO_FRAGMENTS = 1 << 2;
 
-    private static String ALPHA_NUMERIC_CHARS = ALPHA_CHARS + "\\d";
+    private static final String ALPHA_CHARS = "a-zA-Z";
 
-    private static String SPECIAL_CHARS = ";/@&=,.?:+$";
+    private static final String ALPHA_NUMERIC_CHARS = ALPHA_CHARS + "\\d";
 
-    private static String VALID_CHARS = "[^\\s" + SPECIAL_CHARS + "]";
+    private static final String SPECIAL_CHARS = ";/@&=,.?:+$";
 
-    private static String SCHEME_CHARS = ALPHA_CHARS;
+    private static final String VALID_CHARS = "[^\\s" + SPECIAL_CHARS + "]";
+
+    private static final String SCHEME_CHARS = ALPHA_CHARS;
 
     // Drop numeric, and  "+-." for now
-    private static String AUTHORITY_CHARS = ALPHA_NUMERIC_CHARS + "\\-\\.";
+    private static final String AUTHORITY_CHARS = ALPHA_NUMERIC_CHARS + "\\-\\.";
 
-    private static String ATOM = VALID_CHARS + '+';
+    private static final String ATOM = VALID_CHARS + '+';
 
     /**
      * This expression derived/taken from the BNF for URI (RFC2396).
      */
-    private static String URL_PATTERN =
-        "/^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\\?([^#]*))?(#(.*))?/";
+    private static final String URL_PATTERN = "/^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\\?([^#]*))?(#(.*))?/";
+
     //                                                                      12            3  4          5       6   7        8 9
 
     /**
      * Schema/Protocol (ie. http:, ftp:, file:, etc).
      */
-    private static int PARSE_URL_SCHEME = 2;
+    private static final int PARSE_URL_SCHEME = 2;
 
     /**
      * Includes hostname/ip and port number.
      */
-    private static int PARSE_URL_AUTHORITY = 4;
+    private static final int PARSE_URL_AUTHORITY = 4;
 
-    private static int PARSE_URL_PATH = 5;
+    private static final int PARSE_URL_PATH = 5;
 
-    private static int PARSE_URL_QUERY = 7;
+    private static final int PARSE_URL_QUERY = 7;
 
-    private static int PARSE_URL_FRAGMENT = 9;
+    private static final int PARSE_URL_FRAGMENT = 9;
 
     /**
      * Protocol (ie. http:, ftp:,https:).
      */
-    private static String SCHEME_PATTERN = "/^[" + SCHEME_CHARS + "]/";
+    private static final String SCHEME_PATTERN = "/^[" + SCHEME_CHARS + "]/";
 
-    private static String AUTHORITY_PATTERN =
-        "/^([" + AUTHORITY_CHARS + "]*)(:\\d*)?(.*)?/";
+    private static final String AUTHORITY_PATTERN = "/^([" + AUTHORITY_CHARS + "]*)(:\\d*)?(.*)?/";
+
     //                                                                            1                          2  3       4
 
-    private static int PARSE_AUTHORITY_HOST_IP = 1;
+    private static final int PARSE_AUTHORITY_HOST_IP = 1;
 
-    private static int PARSE_AUTHORITY_PORT = 2;
+    private static final int PARSE_AUTHORITY_PORT = 2;
 
     /**
      * Should always be empty.
      */
-    private static int PARSE_AUTHORITY_EXTRA = 3;
+    private static final int PARSE_AUTHORITY_EXTRA = 3;
 
-    private static String PATH_PATTERN =
-        "/^(/[-a-zA-Z0-9_:@&?=+,.!/~*'%$]*)$/";
+    private static final String PATH_PATTERN = "/^(/[-\\w:@&?=+,.!/~*'%$]*)$/";
 
-    private static String QUERY_PATTERN = "/^(.*)$/";
+    private static final String QUERY_PATTERN = "/^(.*)$/";
 
-    private static String LEGAL_ASCII_PATTERN = "/^[\\000-\\177]+$/";
+    private static final String LEGAL_ASCII_PATTERN = "/^[\\000-\\177]+$/";
 
-    private static String IP_V4_DOMAIN_PATTERN =
-        "/^(\\d{1,3})[.](\\d{1,3})[.](\\d{1,3})[.](\\d{1,3})$/";
+    private static final String IP_V4_DOMAIN_PATTERN = "/^(\\d{1,3})[.](\\d{1,3})[.](\\d{1,3})[.](\\d{1,3})$/";
 
-    private static String DOMAIN_PATTERN =
-        "/^" + ATOM + "(\\." + ATOM + ")*$/";
+    private static final String DOMAIN_PATTERN = "/^" + ATOM + "(\\." + ATOM + ")*$/";
 
-    private static String PORT_PATTERN = "/^:(\\d{1,5})$/";
+    private static final String PORT_PATTERN = "/^:(\\d{1,5})$/";
 
-    private static String ATOM_PATTERN = "/(" + ATOM + ")/";
+    private static final String ATOM_PATTERN = "/(" + ATOM + ")/";
 
-    private static String ALPHA_PATTERN = "/^[" + ALPHA_CHARS + "]/";
+    private static final String ALPHA_PATTERN = "/^[" + ALPHA_CHARS + "]/";
+
+    /**
+     * Holds the set of current validation options.
+     */
+    private Flags options = null;
 
     /**
      * The set of schemes that are allowed to be in a URL.
@@ -169,7 +179,7 @@ public class UrlValidator
     /**
      * If no schemes are provided, default to this set.
      */
-    protected String[] defaultSchemes = {"http", "https", "ftp" };
+    protected String[] defaultSchemes = { "http", "https", "ftp" };
 
     /**
      * Create a UrlValidator with default properties.
@@ -181,12 +191,11 @@ public class UrlValidator
 
     /**
      * Behavior of validation is modified by passing in several strings options:
-     *
      * @param schemes Pass in one or more url schemes to consider valid, passing in
-     *                a null will default to "http,https,ftp" being valid.
-     *                If a non-null schemes is specified then all valid schemes must
-     *                be specified. Setting the ALLOW_ALL_SCHEMES option will
-     *                ignore the contents of schemes.
+     *        a null will default to "http,https,ftp" being valid.
+     *        If a non-null schemes is specified then all valid schemes must
+     *        be specified. Setting the ALLOW_ALL_SCHEMES option will
+     *        ignore the contents of schemes.
      */
     public UrlValidator( String[] schemes )
     {
@@ -195,10 +204,9 @@ public class UrlValidator
 
     /**
      * Initialize a UrlValidator with the given validation options.
-     *
      * @param options The options should be set using the public constants declared in
-     *                this class.  To set multiple options you simply add them together.  For example,
-     *                ALLOW_2_SLASHES + NO_FRAGMENTS enables both of those options.
+     * this class.  To set multiple options you simply add them together.  For example,
+     * ALLOW_2_SLASHES + NO_FRAGMENTS enables both of those options.
      */
     public UrlValidator( int options )
     {
@@ -207,14 +215,21 @@ public class UrlValidator
 
     /**
      * Behavour of validation is modified by passing in options:
-     *
      * @param schemes The set of valid schemes.
      * @param options The options should be set using the public constants declared in
-     *                this class.  To set multiple options you simply add them together.  For example,
+     * this class.  To set multiple options you simply add them together.  For example,
+     * ALLOW_2_SLASHES + NO_FRAGMENTS enables both of those options.
      */
     public UrlValidator( String[] schemes, int options )
     {
-        if ( schemes == null )
+        this.options = new Flags( options );
+
+        if ( this.options.isOn( ALLOW_ALL_SCHEMES ) )
+        {
+            return;
+        }
+
+        if ( schemes == null && this.allowedSchemes().isEmpty() )
         {
             schemes = this.defaultSchemes;
         }
@@ -226,10 +241,22 @@ public class UrlValidator
      * <p>Checks if a field has a valid url address.</p>
      *
      * @param value The value validation is being performed on.  A <code>null</code>
-     *              value is considered invalid.
+     * value is considered invalid.
      * @return true if the url is valid.
      */
     public boolean validate( String value )
+    {
+        return isValid( value );
+    }
+
+    /**
+     * <p>Checks if a field has a valid url address.</p>
+     *
+     * @param value The value validation is being performed on.  A <code>null</code>
+     * value is considered invalid.
+     * @return true if the url is valid.
+     */
+    public boolean isValid( String value )
     {
         if ( value == null )
         {
@@ -237,7 +264,6 @@ public class UrlValidator
         }
 
         Perl5Util matchUrlPat = new Perl5Util();
-
         Perl5Util matchAsciiPat = new Perl5Util();
 
         if ( !matchAsciiPat.match( LEGAL_ASCII_PATTERN, value ) )
@@ -283,9 +309,8 @@ public class UrlValidator
      * Validate scheme. If schemes[] was initialized to a non null,
      * then only those scheme's are allowed.  Note this is slightly different
      * than for the constructor.
-     *
      * @param scheme The scheme to validate.  A <code>null</code> value is considered
-     *               invalid.
+     * invalid.
      * @return true if valid.
      */
     protected boolean isValidScheme( String scheme )
@@ -299,6 +324,15 @@ public class UrlValidator
         if ( !schemeMatcher.match( SCHEME_PATTERN, scheme ) )
         {
             return false;
+        }
+
+        if ( this.options.isOff( ALLOW_ALL_SCHEMES ) )
+        {
+
+            if ( !this.allowedSchemes.contains( scheme ) )
+            {
+                return false;
+            }
         }
 
         return true;
@@ -323,9 +357,8 @@ public class UrlValidator
             return false;
         }
 
-        boolean ipV4Address;
+        boolean ipV4Address = false;
         boolean hostname = false;
-
         // check if authority is IP address or hostname
         String hostIP = authorityMatcher.group( PARSE_AUTHORITY_HOST_IP );
         ipV4Address = matchIPV4Pat.match( IP_V4_DOMAIN_PATTERN, hostIP );
@@ -368,7 +401,7 @@ public class UrlValidator
             String[] domainSegment = new String[10];
             boolean match = true;
             int segmentCount = 0;
-            int segmentLength;
+            int segmentLength = 0;
             Perl5Util atomMatcher = new Perl5Util();
 
             while ( match )
@@ -378,10 +411,7 @@ public class UrlValidator
                 {
                     domainSegment[segmentCount] = atomMatcher.group( 1 );
                     segmentLength = domainSegment[segmentCount].length() + 1;
-                    hostIP =
-                        ( segmentLength >= hostIP.length() )
-                        ? ""
-                        : hostIP.substring( segmentLength );
+                    hostIP = ( segmentLength >= hostIP.length() ) ? "" : hostIP.substring( segmentLength );
 
                     segmentCount++;
                 }
@@ -422,18 +452,12 @@ public class UrlValidator
         }
 
         String extra = authorityMatcher.group( PARSE_AUTHORITY_EXTRA );
-
         if ( !isBlankOrNull( extra ) )
         {
             return false;
         }
 
         return true;
-    }
-
-    protected boolean isBlankOrNull( String s )
-    {
-        return ( ( s == null ) || ( s.trim().length() ) == 0 );
     }
 
     /**
@@ -459,9 +483,13 @@ public class UrlValidator
         }
 
         int slash2Count = countToken( "//", path );
+        if ( this.options.isOff( ALLOW_2_SLASHES ) && ( slash2Count > 0 ) )
+        {
+            return false;
+        }
+
         int slashCount = countToken( "/", path );
         int dot2Count = countToken( "..", path );
-
         if ( dot2Count > 0 )
         {
             if ( ( slashCount - slash2Count - 1 ) <= dot2Count )
@@ -489,8 +517,6 @@ public class UrlValidator
 
     /**
      * Returns true if the given fragment is null or fragments are allowed.
-     *
-     * @todo this alwyas return true
      */
     protected boolean isValidFragment( String fragment )
     {
@@ -499,8 +525,7 @@ public class UrlValidator
             return true;
         }
 
-        //return this.options.isOff( NO_FRAGMENTS );
-        return true;
+        return this.options.isOff( NO_FRAGMENTS );
     }
 
     /**
@@ -520,5 +545,10 @@ public class UrlValidator
             }
         }
         return count;
+    }
+
+    private static boolean isBlankOrNull( String value )
+    {
+        return ( ( value == null ) || ( value.trim().length() == 0 ) );
     }
 }
