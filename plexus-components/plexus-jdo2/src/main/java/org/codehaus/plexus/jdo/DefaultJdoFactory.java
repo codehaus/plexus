9f7eaa60c027f2a9f1c33f00194d11e7bccacfe5
trygvis <trygvis@30dfa635-7bf8-0310-9608-e41f2d776689>
2005-08-01 00:52:56 +0000

    o Adding a ConfigurableJdoFactory which is programmatically configurable.

--- plexus-components/plexus-jdo2/src/main/java/org/codehaus/plexus/jdo/DefaultJdoFactory.java
+++ plexus-components/plexus-jdo2/src/main/java/org/codehaus/plexus/jdo/DefaultJdoFactory.java
@@ -45,6 +45,10 @@ public class DefaultJdoFactory
     /** @configuration */
     private Properties properties;
 
+    // ----------------------------------------------------------------------
+    //
+    // ----------------------------------------------------------------------
+
     private PersistenceManagerFactory persistenceManagerFactory;
 
     // ----------------------------------------------------------------------
