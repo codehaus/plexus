65cb0093ae6b8a349dad50d29214f832dee62751
evenisse <evenisse@30dfa635-7bf8-0310-9608-e41f2d776689>
2005-08-26 08:51:09 +0000

    Add MungedHttpsURL class that handle https connection with username/password I don't think it's the best place here, but it's a first solution. Thanks to Jesse McConnell

--- trunk/plexus-components/plexus-formica/src/main/java/org/codehaus/plexus/formica/validation/UrlSourceValidator.java
+++ trunk/plexus-components/plexus-formica/src/main/java/org/codehaus/plexus/formica/validation/UrlSourceValidator.java
@@ -3,18 +3,12 @@
  */
 package org.codehaus.plexus.formica.validation;
 
-import org.apache.oro.text.regex.MalformedPatternException;
-import org.apache.oro.text.regex.Pattern;
-import org.apache.oro.text.regex.Perl5Compiler;
-import org.apache.oro.text.regex.Perl5Matcher;
-import org.codehaus.plexus.formica.FormicaException;
-import org.codehaus.plexus.personality.plexus.lifecycle.phase.Initializable;
+import org.codehaus.plexus.formica.util.MungedHttpsURL;
 import org.codehaus.plexus.util.StringUtils;
 
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.MalformedURLException;
 
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
@@ -58,10 +52,11 @@ public class UrlSourceValidator
             // want to make sure the URL corresponds to an existing resource.
             // ----------------------------------------------------------------------
 
-        	// if it is a https connection then we have a bit more work to do
-            if ( urlString != null && urlString.startsWith("https") )
+            // if it is a https connection then we have a bit more work to do, currently 
+            // only accepting the munged https url that is resolved in MungedHttpsURL
+            if ( urlString != null && urlString.startsWith( "https" ) )
             {
-            	return validateSecureURL( urlString );
+                return new MungedHttpsURL( urlString ).isValid();
             }
             else
             {
@@ -79,114 +74,4 @@ public class UrlSourceValidator
 
         return true;
     }
-
-    /**
-     * This bit attempts to ignore certificates that might need to be accepted and also tries to 
-     * conform to the format https://<username>:<password>@host
-     *  
-     * adapted from code snippets from http://javaalmanac.com/egs/javax.net.ssl/TrustAll.html *
-     */
-    public boolean validateSecureURL( String urlString )
-    {
-        try
-        {
-    		// Create a trust manager that does not validate certificate
-    		// chains
-    	    TrustManager[] trustAllCerts = new TrustManager[]
-    	    {
-    	         new X509TrustManager()
-    	         {
-		              public java.security.cert.X509Certificate[] getAcceptedIssuers()
-		              {
-		                 return null;
-		              }
-
-		              public void checkClientTrusted( java.security.cert.X509Certificate[] certs, String authType )
-		              {
-		              }
-
-		              public void checkServerTrusted( java.security.cert.X509Certificate[] certs, String authType )
-		              {
-		              }
-    		     }
-    		};
-
-    		// Install the all-trusting trust manager
-
-    	    SSLContext sslContext = SSLContext.getInstance( "SSL" );
-
-    		sslContext.init( null, trustAllCerts, new java.security.SecureRandom() );
-
-    		HttpsURLConnection.setDefaultSSLSocketFactory( sslContext.getSocketFactory() );
-
-            String username = scrapeUsername( urlString );
-
-            String password = scrapePassword( urlString );
-
-            String authString = username + ":" + password;
-
-            String cleanUrl = scrapeUrl( urlString );
-
-            URL url = new URL( cleanUrl );
-
-            HttpURLConnection urlc = (HttpURLConnection) url.openConnection();
-
-            urlc.setDoInput( true );
-
-            urlc.setUseCaches( false );
-
-            urlc.setRequestProperty( "Content-Type", "application/octet-stream" );
-
-            if ( username != null && password != null )
-            {
-                urlc.setRequestProperty( "Authorization", "Basic " + new sun.misc.BASE64Encoder().encode( authString.getBytes() ) );
-            }
-
-            InputStream is = urlc.getInputStream();
-
-            is.close();
-          
-        }
-        catch ( Exception e )
-        {
-           return false;
-        }
-
-        return true;
-    }
-
-    /**
-     * return the username from https://<name>:<password>@<url>
-     */
-    private String scrapeUsername( String url )
-    {
-        String t = url.substring(8, url.length());
-
-        return t.substring(0, t.indexOf(":"));
-    }
-
-    /**
-     * return to password from https://<name>:<password>@<url>
-     */
-    private String scrapePassword( String url )
-    {
-       String t = url.substring( 8, url.length() );
-
-       return t.substring( t.indexOf( ":" ) + 1, t.indexOf( "@" ) );
-    }
-
-     /**
-      * return the url from https://<name>:<password>@<url>
-      */
-     private String scrapeUrl( String url )
-     {
-    	 if ( url.indexOf( "@" ) != -1 )
-    	 {
-    		 return "https://" + url.substring(url.indexOf( "@" ) + 1, url.length() );
-    	 }
-    	 else
-    	 {
-    		 return url;
-    	 }
-     }
 }
