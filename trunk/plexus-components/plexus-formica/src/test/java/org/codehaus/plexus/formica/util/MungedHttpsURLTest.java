dc2bb6b17a1d1ad43a6823c64f28232c9305c9a8
evenisse <evenisse@30dfa635-7bf8-0310-9608-e41f2d776689>
2005-09-21 06:47:23 +0000

    [PLX-157] parse username/password from URLs with paths

--- trunk/plexus-components/plexus-formica/src/test/java/org/codehaus/plexus/formica/util/MungedHttpsURLTest.java
+++ trunk/plexus-components/plexus-formica/src/test/java/org/codehaus/plexus/formica/util/MungedHttpsURLTest.java
@@ -24,23 +24,13 @@ import junit.framework.TestCase;
  * @author Christoph Sturm
  * @version $Id$
  */
-public class MungedHttpsURLTest extends TestCase
+public class MungedHttpsURLTest
+    extends TestCase
 {
-    public void testHttpsURL() throws MalformedURLException
+    public void testHttpsURL()
+        throws MalformedURLException
     {
-        MungedHttpsURL mungedHttpsURL;
-
-        mungedHttpsURL = new MungedHttpsURL( "https://user:password@host" );
-
-        assertEquals( "https://host", mungedHttpsURL.getUrlString() );
-
-        assertEquals( "user", mungedHttpsURL.getUsername() );
-
-        assertEquals( "password", mungedHttpsURL.getPassword() );
-
-        mungedHttpsURL.getURL();
-
-        mungedHttpsURL = new MungedHttpsURL( "https://host" );
+        MungedHttpsURL mungedHttpsURL = new MungedHttpsURL( "https://host" );
 
         assertEquals( "https://host", mungedHttpsURL.getUrlString() );
 
@@ -50,21 +40,53 @@ public class MungedHttpsURLTest extends TestCase
 
         mungedHttpsURL.getURL();
     }
-    public void testHttpURL() throws MalformedURLException
+
+    public void testHttpsURLPath()
+        throws MalformedURLException
     {
-        MungedHttpsURL mungedHttpsURL;
+        MungedHttpsURL mungedHttpsURL = new MungedHttpsURL( "https://host/path" );
 
-        mungedHttpsURL = new MungedHttpsURL( "http://user:password@host" );
+        assertEquals( "https://host/path", mungedHttpsURL.getUrlString() );
 
-        assertEquals( "http://host", mungedHttpsURL.getUrlString() );
+        assertEquals( null, mungedHttpsURL.getUsername() );
+
+        assertEquals( null, mungedHttpsURL.getPassword() );
+
+        mungedHttpsURL.getURL();
+    }
+
+    public void testHttpsURLAuthentication()
+        throws MalformedURLException
+    {
+        MungedHttpsURL mungedHttpsURL = new MungedHttpsURL( "https://user:password@host" );
+
+        assertEquals( "https://host", mungedHttpsURL.getUrlString() );
 
         assertEquals( "user", mungedHttpsURL.getUsername() );
 
         assertEquals( "password", mungedHttpsURL.getPassword() );
 
         mungedHttpsURL.getURL();
+    }
 
-        mungedHttpsURL = new MungedHttpsURL( "http://host" );
+    public void testHttpsURLAuthenticationPath()
+        throws MalformedURLException
+    {
+        MungedHttpsURL mungedHttpsURL = new MungedHttpsURL( "https://user:password@host/path" );
+
+        assertEquals( "https://host/path", mungedHttpsURL.getUrlString() );
+
+        assertEquals( "user", mungedHttpsURL.getUsername() );
+
+        assertEquals( "password", mungedHttpsURL.getPassword() );
+
+        mungedHttpsURL.getURL();
+    }
+
+    public void testHttpURL()
+        throws MalformedURLException
+    {
+        MungedHttpsURL mungedHttpsURL = new MungedHttpsURL( "http://host" );
 
         assertEquals( "http://host", mungedHttpsURL.getUrlString() );
 
@@ -75,4 +97,45 @@ public class MungedHttpsURLTest extends TestCase
         mungedHttpsURL.getURL();
     }
 
+    public void testHttpURLPath()
+        throws MalformedURLException
+    {
+        MungedHttpsURL mungedHttpsURL = new MungedHttpsURL( "http://host/path" );
+
+        assertEquals( "http://host/path", mungedHttpsURL.getUrlString() );
+
+        assertEquals( null, mungedHttpsURL.getUsername() );
+
+        assertEquals( null, mungedHttpsURL.getPassword() );
+
+        mungedHttpsURL.getURL();
+    }
+
+    public void testHttpURLAuthentication()
+        throws MalformedURLException
+    {
+        MungedHttpsURL mungedHttpsURL = new MungedHttpsURL( "http://user:password@host" );
+
+        assertEquals( "http://host", mungedHttpsURL.getUrlString() );
+
+        assertEquals( "user", mungedHttpsURL.getUsername() );
+
+        assertEquals( "password", mungedHttpsURL.getPassword() );
+
+        mungedHttpsURL.getURL();
+    }
+
+    public void testHttpURLAuthenticationPath()
+        throws MalformedURLException
+    {
+        MungedHttpsURL mungedHttpsURL = new MungedHttpsURL( "http://user:password@host/path" );
+
+        assertEquals( "http://host/path", mungedHttpsURL.getUrlString() );
+
+        assertEquals( "user", mungedHttpsURL.getUsername() );
+
+        assertEquals( "password", mungedHttpsURL.getPassword() );
+
+        mungedHttpsURL.getURL();
+    }
 }
