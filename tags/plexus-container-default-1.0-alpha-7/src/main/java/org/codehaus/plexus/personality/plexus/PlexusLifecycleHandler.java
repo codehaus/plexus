8300cb32b35a298f314aa621df7e6cf3f6667286
brett <brett@30dfa635-7bf8-0310-9608-e41f2d776689>
2005-10-02 06:13:36 +0000

    [maven-scm] copy for tag plexus-container-default-1.0-alpha-7

---
+++ tags/plexus-container-default-1.0-alpha-7/src/main/java/org/codehaus/plexus/personality/plexus/PlexusLifecycleHandler.java
@@ -0,0 +1,18 @@
+package org.codehaus.plexus.personality.plexus;
+
+import org.codehaus.plexus.lifecycle.AbstractLifecycleHandler;
+
+public class PlexusLifecycleHandler
+    extends AbstractLifecycleHandler
+{
+    public static String COMPONENT_CONFIGURATOR = "component.configurator";
+
+    public PlexusLifecycleHandler()
+    {
+        super();
+    }
+
+    public void initialize()
+    {
+    }
+}
