d86dc524b89c3501b907f20b6af0ff6c946724f5
jdcasey <jdcasey@30dfa635-7bf8-0310-9608-e41f2d776689>
2005-12-12 01:16:12 +0000

    [maven-scm] copy for tag plexus-archiver-1.0-alpha-4

--- plexus-personalities/plexus-pico-personality/src/test/java/org/codehaus/plexus/personality/pico/composition/basic/Boy.java
+++
@@ -1,9 +0,0 @@
-package org.codehaus.plexus.personality.pico.composition.basic;
-
-public class Boy implements Kissable
-{
-    public void kiss( Object kisser )
-    {
-        System.out.println( "I was kissed by " + kisser );
-    }
-}
