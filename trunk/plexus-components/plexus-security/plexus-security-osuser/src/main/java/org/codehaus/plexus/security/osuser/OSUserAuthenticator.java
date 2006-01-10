07b6dbf87dc1434cb0aef327e93a544462b996db
evenisse <evenisse@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-01-10 14:15:01 +0000

    Declare component

--- trunk/plexus-components/plexus-security/plexus-security-osuser/src/main/java/org/codehaus/plexus/security/osuser/OSUserAuthenticator.java
+++ trunk/plexus-components/plexus-security/plexus-security-osuser/src/main/java/org/codehaus/plexus/security/osuser/OSUserAuthenticator.java
@@ -1,6 +1,11 @@
 package org.codehaus.plexus.security.osuser;
 
+import org.codehaus.plexus.PlexusContainer;
+import org.codehaus.plexus.PlexusConstants;
+import org.codehaus.plexus.context.Context;
+import org.codehaus.plexus.context.ContextException;
 import org.codehaus.plexus.logging.AbstractLogEnabled;
+import org.codehaus.plexus.personality.plexus.lifecycle.phase.Contextualizable;
 import org.codehaus.plexus.security.Authentication;
 import org.codehaus.plexus.security.Authenticator;
 import org.codehaus.plexus.security.DefaultAuthentication;
@@ -20,11 +25,13 @@ import javax.servlet.http.HttpServletRequest;
  * @author <a href="mailto:evenisse@codehaus.org">Emmanuel Venisse</a>
  * @version $Id$
  */
-public abstract class OSUserAuthenticator
+public class OSUserAuthenticator
     extends AbstractLogEnabled
-    implements Authenticator
+    implements Authenticator, Contextualizable
 {
-    private UserManager userManager;
+    private UserManager userManager = UserManager.getInstance();
+
+    private PlexusContainer container;
 
     public Authentication authenticate( Map tokens )
         throws UnknownEntityException, AuthenticationException, UnauthorizedException
@@ -32,6 +39,11 @@ public abstract class OSUserAuthenticator
         String username = (String) tokens.get( "username" );
         String password = (String) tokens.get( "password" );
         HttpServletRequest request = (HttpServletRequest) tokens.get( "request" );
+
+        if ( request != null && container != null )
+        {
+            request.setAttribute( PlexusConstants.PLEXUS_KEY, container );
+        }
 
         getLogger().debug( "username = " + username );
 
@@ -107,5 +119,11 @@ public abstract class OSUserAuthenticator
     protected UserManager getUserManager()
     {
         return userManager;
+    }
+
+    public void contextualize( Context context )
+        throws ContextException
+    {
+        container = (PlexusContainer) context.get( PlexusConstants.PLEXUS_KEY );
     }
 }
