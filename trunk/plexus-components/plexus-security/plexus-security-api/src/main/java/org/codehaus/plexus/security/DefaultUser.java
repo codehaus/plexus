2f72e6d152d9e63640f007f12bd85ce5ee5e91e5
evenisse <evenisse@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-01-10 14:14:18 +0000

    Add fullName and email

--- trunk/plexus-components/plexus-security/plexus-security-api/src/main/java/org/codehaus/plexus/security/DefaultUser.java
+++ trunk/plexus-components/plexus-security/plexus-security-api/src/main/java/org/codehaus/plexus/security/DefaultUser.java
@@ -5,6 +5,8 @@ public class DefaultUser
 {
     private String username;
     private String password;
+    private String fullName;
+    private String email;
     private boolean enabled;
     private boolean accountNonExpired;
     private boolean accountNonLocked;
@@ -29,6 +31,26 @@ public class DefaultUser
     public void setPassword( String password )
     {
         this.password = password;
+    }
+
+    public String getFullName()
+    {
+        return fullName;
+    }
+
+    public void setFullName( String fullName )
+    {
+        this.fullName = fullName;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail( String email )
+    {
+        this.email = email;
     }
 
     public boolean isEnabled()
