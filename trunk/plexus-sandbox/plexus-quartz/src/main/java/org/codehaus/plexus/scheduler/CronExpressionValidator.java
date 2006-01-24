340d230ee4d5eb3132e5f58e659fa7f9811066b4
evenisse <evenisse@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-01-24 21:08:07 +0000

    [PLX-194] Add more check in cron validator relative to all explanations in CronTrigger javadoc

--- trunk/plexus-sandbox/plexus-quartz/src/main/java/org/codehaus/plexus/scheduler/CronExpressionValidator.java
+++ trunk/plexus-sandbox/plexus-quartz/src/main/java/org/codehaus/plexus/scheduler/CronExpressionValidator.java
@@ -1,4 +1,5 @@
 package org.codehaus.plexus.scheduler;
+
 /*
  * Copyright 2001-2005 The Apache Software Foundation.
  *
@@ -19,11 +20,25 @@ import org.codehaus.plexus.util.StringUtils;
 import org.quartz.CronTrigger;
 
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.StringTokenizer;
 
+/**
+ * @author <a href="mailto:evenisse@codehaus.org">Emmanuel Venisse</a>
+ * @version $Id$
+ */
 public class CronExpressionValidator
 {
     public static final String ROLE = CronExpressionValidator.class.getName();
 
+    /**
+     * Validates a cron expression.
+     *
+     * @param cronExpression The expression to validate
+     * @return True is expression is valid
+     */
     public boolean validate( String cronExpression )
     {
         try
@@ -39,43 +54,59 @@ public class CronExpressionValidator
 
             cronTrigger.setCronExpression( cronExpression );
 
-            //Check year param
-            if ( cronParams.length == 7 )
+            if ( cronParams[3].equals( "?" ) || cronParams[5].equals( "?" ) )
             {
-                String yearField = cronParams[6];
-
-                boolean yearIsOk;
-
-                if ( yearField.indexOf( "-" ) > -1 )
-                {
-                    yearIsOk = checkYear( yearField.substring( 0, yearField.indexOf( "-" ) ) ) &&
-                               checkYear( yearField.substring( yearField.indexOf( "-" ) + 1 ) );
-                }
-                else if ( yearField.indexOf( "/" ) > -1 )
-                {
-                    yearIsOk = checkYear( yearField.substring( 0, yearField.indexOf( "/" ) ) );
-                }
-                else if ( yearField.indexOf( "*" ) != -1 )
-                {
-                    yearIsOk = true;
-                }
-                else
-                {
-                    yearIsOk = checkYear( yearField );
-                }
-
-                if ( !yearIsOk )
+                //Check seconds param
+                if ( !checkSecondsField( cronParams[0] ) )
                 {
                     return false;
                 }
-            }
 
-            if ( cronParams[3].equals( "?" ) || cronParams[5].equals( "?" ) )
-            {
+                //Check minutes param
+                if ( !checkMinutesField( cronParams[1] ) )
+                {
+                    return false;
+                }
+
+                //Check hours param
+                if ( !checkHoursField( cronParams[2] ) )
+                {
+                    return false;
+                }
+
+                //Check day-of-month param
+                if ( !checkDayOfMonthField( cronParams[3] ) )
+                {
+                    return false;
+                }
+
+                //Check months param
+                if ( !checkMonthsField( cronParams[4] ) )
+                {
+                    return false;
+                }
+
+                //Check day-of-week param
+                if ( !checkDayOfWeekField( cronParams[5] ) )
+                {
+                    return false;
+                }
+
+                //Check year param
+                if ( cronParams.length == 7 )
+                {
+                    if ( !checkYearField( cronParams[6] ) )
+                    {
+                        return false;
+                    }
+                }
+
                 return true;
             }
-
-            return false;
+            else
+            {
+                return false;
+            }
         }
         catch ( ParseException e )
         {
@@ -83,22 +114,330 @@ public class CronExpressionValidator
         }
     }
 
-    private boolean checkYear( String yearField )
+    private boolean checkSecondsField( String secondsField )
     {
-        try
-        {
-            int year = Integer.parseInt( yearField );
+        return checkField( secondsField, 0, 59 );
+    }
 
-            if ( year < 1970 || year > 2099 )
+    private boolean checkMinutesField( String minutesField )
+    {
+        return checkField( minutesField, 0, 59 );
+    }
+
+    private boolean checkHoursField( String hoursField )
+    {
+        return checkField( hoursField, 0, 23 );
+    }
+
+    private boolean checkDayOfMonthField( String dayOfMonthField )
+    {
+        if ( "?".equals( dayOfMonthField ) )
+        {
+            return true;
+        }
+
+        if ( dayOfMonthField.indexOf( "L" ) >= 0 )
+        {
+            return checkFieldWithLetter( dayOfMonthField, "L" );
+        }
+        else if ( dayOfMonthField.indexOf( "W" ) >= 0 )
+        {
+            return checkFieldWithLetter( dayOfMonthField, "W" );
+        }
+        else if ( dayOfMonthField.indexOf( "C" ) >= 0 )
+        {
+            return checkFieldWithLetter( dayOfMonthField, "C" );
+        }
+        else
+        {
+            return checkField( dayOfMonthField, 1, 31 );
+        }
+    }
+
+    private boolean checkMonthsField( String monthsField )
+    {
+        monthsField = StringUtils.replace( monthsField, "JAN", "1" );
+        monthsField = StringUtils.replace( monthsField, "FEB", "2" );
+        monthsField = StringUtils.replace( monthsField, "MAR", "3" );
+        monthsField = StringUtils.replace( monthsField, "APR", "4" );
+        monthsField = StringUtils.replace( monthsField, "MAY", "5" );
+        monthsField = StringUtils.replace( monthsField, "JUN", "6" );
+        monthsField = StringUtils.replace( monthsField, "JUL", "7" );
+        monthsField = StringUtils.replace( monthsField, "AUG", "8" );
+        monthsField = StringUtils.replace( monthsField, "SEP", "9" );
+        monthsField = StringUtils.replace( monthsField, "OCT", "10" );
+        monthsField = StringUtils.replace( monthsField, "NOV", "11" );
+        monthsField = StringUtils.replace( monthsField, "DEC", "12" );
+
+        return checkField( monthsField, 1, 31 );
+    }
+
+    private boolean checkDayOfWeekField( String dayOfWeekField )
+    {
+        dayOfWeekField = StringUtils.replace( dayOfWeekField, "SUN", "1" );
+        dayOfWeekField = StringUtils.replace( dayOfWeekField, "MON", "2" );
+        dayOfWeekField = StringUtils.replace( dayOfWeekField, "TUE", "3" );
+        dayOfWeekField = StringUtils.replace( dayOfWeekField, "WED", "4" );
+        dayOfWeekField = StringUtils.replace( dayOfWeekField, "THU", "5" );
+        dayOfWeekField = StringUtils.replace( dayOfWeekField, "FRI", "6" );
+        dayOfWeekField = StringUtils.replace( dayOfWeekField, "SAT", "7" );
+
+        if ( "?".equals( dayOfWeekField ) )
+        {
+            return true;
+        }
+
+        if ( dayOfWeekField.indexOf( "L" ) >= 0 )
+        {
+            return checkFieldWithLetter( dayOfWeekField, "L" );
+        }
+        else if ( dayOfWeekField.indexOf( "C" ) >= 0 )
+        {
+            return checkFieldWithLetter( dayOfWeekField, "C" );
+        }
+        else if ( dayOfWeekField.indexOf( "#" ) >= 0 )
+        {
+            return checkFieldWithLetter( dayOfWeekField, "#" );
+        }
+        else
+        {
+            return checkField( dayOfWeekField, 1, 7 );
+        }
+    }
+
+    private boolean checkYearField( String yearField )
+    {
+        return checkField( yearField, 1970, 2099 );
+    }
+
+    private boolean checkField( String secondsField, int minimal, int maximal )
+    {
+        if ( secondsField.indexOf( "-" ) > -1 )
+        {
+            String startValue = secondsField.substring( 0, secondsField.indexOf( "-" ) );
+            String endValue = secondsField.substring( secondsField.indexOf( "-" ) + 1 );
+
+            if ( !( checkIntValue( startValue, minimal, maximal ) && checkIntValue( endValue, minimal, maximal ) ) )
             {
                 return false;
             }
-            else
+            try
             {
+                int startVal = Integer.parseInt( startValue );
+                int endVal = Integer.parseInt( endValue );
+
+                if ( endVal <= startVal )
+                {
+                    return false;
+                }
+
                 return true;
             }
+            catch ( NumberFormatException e )
+            {
+                return false;
+            }
         }
-        catch( NumberFormatException e )
+        else if ( secondsField.indexOf( "," ) > -1 )
+        {
+            return checkListField( secondsField, minimal, maximal );
+        }
+        else if ( secondsField.indexOf( "/" ) > -1 )
+        {
+            return checkIncrementField( secondsField, minimal, maximal );
+        }
+        else if ( secondsField.indexOf( "*" ) != -1 )
+        {
+            return true;
+        }
+        else
+        {
+            return checkIntValue( secondsField, minimal, maximal );
+        }
+    }
+
+    private boolean checkFieldWithLetter( String value, String letter )
+    {
+        boolean canBeAlone = false;
+        boolean canHaveIntBefore = false;
+        boolean canHaveIntAfter = false;
+        boolean mustHaveIntBefore = false;
+        boolean mustHaveIntAfter = false;
+
+        if ( "L".equals( letter ) )
+        {
+            canBeAlone = true;
+            canHaveIntBefore = true;
+            canHaveIntAfter = false;
+            mustHaveIntBefore = false;
+            mustHaveIntAfter = false;
+        }
+        if ( "W".equals( letter ) || "C".equals( letter ) )
+        {
+            canBeAlone = false;
+            canHaveIntBefore = true;
+            canHaveIntAfter = false;
+            mustHaveIntBefore = true;
+            mustHaveIntAfter = false;
+        }
+        if ( "#".equals( letter ) )
+        {
+            canBeAlone = false;
+            canHaveIntBefore = true;
+            canHaveIntAfter = true;
+            mustHaveIntBefore = true;
+            mustHaveIntAfter = true;
+        }
+
+        String beforeLetter = "";
+        String afterLetter = "";
+
+        if ( value.indexOf( letter ) >= 0 )
+        {
+            beforeLetter = value.substring( 0, value.indexOf( letter ) );
+        }
+
+        if ( !value.endsWith( letter ) )
+        {
+            afterLetter = value.substring( value.indexOf( letter ) + 1 );
+        }
+
+        if ( value.indexOf( letter ) >= 0 )
+        {
+            if ( letter.equals( value ) )
+            {
+                if ( canBeAlone )
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            if ( canHaveIntBefore )
+            {
+                if ( !mustHaveIntBefore && beforeLetter.length() == 0 )
+                {
+                    return false;
+                }
+
+                try
+                {
+                    Integer.parseInt( beforeLetter );
+                    return true;
+                }
+                catch ( NumberFormatException e )
+                {
+                    return false;
+                }
+            }
+
+            if ( canHaveIntAfter )
+            {
+                if ( !mustHaveIntAfter && afterLetter.length() == 0 )
+                {
+                    return false;
+                }
+
+                try
+                {
+                    Integer.parseInt( afterLetter );
+                    return true;
+                }
+                catch ( NumberFormatException e )
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private boolean checkIncrementField( String value, int minimal, int maximal )
+    {
+        String start = value.substring( 0, value.indexOf( "/" ) );
+
+        String increment = value.substring( value.indexOf( "/" ) + 1 );
+
+        if ( !"*".equals( start ) )
+        {
+            return checkIntValue( start, minimal, maximal ) && checkIntValue( increment, minimal, maximal, false );
+        }
+        else
+        {
+            return checkIntValue( increment, minimal, maximal );
+        }
+    }
+
+    private boolean checkListField( String value, int minimal, int maximal )
+    {
+        StringTokenizer st = new StringTokenizer( value, "," );
+
+        List values = new ArrayList();
+
+        while ( st.hasMoreTokens() )
+        {
+            values.add( st.nextToken() );
+        }
+
+        int previousValue = -1;
+
+        for ( Iterator i = values.iterator(); i.hasNext(); )
+        {
+            String currentValue = (String) i.next();
+
+            if ( !checkIntValue( currentValue, minimal, maximal ) )
+            {
+                return false;
+            }
+
+            try
+            {
+                int val = Integer.parseInt( currentValue );
+
+                if ( val <= previousValue )
+                {
+                    return false;
+                }
+                else
+                {
+                    previousValue = val;
+                }
+            }
+            catch ( NumberFormatException e )
+            {
+                // we have always an int
+            }
+        }
+
+        return true;
+    }
+
+    private boolean checkIntValue( String value, int minimal, int maximal )
+    {
+        return checkIntValue( value, minimal, maximal, true );
+    }
+
+    private boolean checkIntValue( String value, int minimal, int maximal, boolean checkExtremity )
+    {
+        try
+        {
+            int val = Integer.parseInt( value );
+
+            if ( checkExtremity )
+            {
+                if ( val < minimal || val > maximal )
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        catch ( NumberFormatException e )
         {
             return false;
         }
