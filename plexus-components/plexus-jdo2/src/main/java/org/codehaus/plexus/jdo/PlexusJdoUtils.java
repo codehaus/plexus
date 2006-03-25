daca256c92dc0914879fba7a68b4dfaf8343c339
trygvis <trygvis@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-03-25 11:17:06 +0000

    o Adding a JDO utility class. This code came from Continuum.

---
+++ plexus-components/plexus-jdo2/src/main/java/org/codehaus/plexus/jdo/PlexusJdoUtils.java
@@ -0,0 +1,41 @@
+package org.codehaus.plexus.jdo;
+
+import javax.jdo.Transaction;
+import javax.jdo.PersistenceManager;
+import javax.jdo.JDOUserException;
+
+/**
+ * @author <a href="mailto:trygvis@inamo.no">Trygve Laugst&oslash;l</a>
+ * @version $Id$
+ */
+public class PlexusJdoUtils
+{
+    public static void rollbackIfActive( Transaction tx )
+    {
+        PersistenceManager pm = tx.getPersistenceManager();
+
+        try
+        {
+            if ( tx.isActive() )
+            {
+                tx.rollback();
+            }
+        }
+        finally
+        {
+            closePersistenceManager( pm );
+        }
+    }
+
+    public static void closePersistenceManager( PersistenceManager pm )
+    {
+        try
+        {
+            pm.close();
+        }
+        catch ( JDOUserException e )
+        {
+            // ignore
+        }
+    }
+}
