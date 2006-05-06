b883522dce63fb9161efd274fdab7e6c8c830563
jvanzyl <jvanzyl@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-05-06 22:26:13 +0000

    o moving the xfire plexus code from xfire to plexus, preparation for making a plexus xfire service

---
+++ plexus-components/plexus-xfire/src/main/org/codehaus/xfire/plexus/transport/PlexusTransportManager.java
@@ -0,0 +1,17 @@
+package org.codehaus.xfire.plexus.transport;
+
+import org.codehaus.plexus.personality.plexus.lifecycle.phase.Initializable;
+import org.codehaus.xfire.transport.DefaultTransportManager;
+
+/**
+ * @author <a href="mailto:dan@envoisolutions.com">Dan Diephouse</a>
+ */
+public class PlexusTransportManager
+    extends DefaultTransportManager
+    implements Initializable
+{
+    public PlexusTransportManager()
+    {
+        super();
+    }
+}
