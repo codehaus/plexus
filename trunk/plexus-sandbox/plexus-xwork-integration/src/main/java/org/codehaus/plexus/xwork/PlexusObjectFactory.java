c36eec93a3a7e880414c141488b25813d20ce880
evenisse <evenisse@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-05-29 15:29:04 +0000

    Migration to webwork 2.2.2. This code is based on plexus package in webwork distribution but support actions, validators, results... name defined in role-hint so action class name used in xwork.xml can be helloAction and in plexus-request.xml, we have role=com.opensymphony.xwork.Action and role-hint=helloAction

--- trunk/plexus-sandbox/plexus-xwork-integration/src/main/java/org/codehaus/plexus/xwork/PlexusObjectFactory.java
+++ trunk/plexus-sandbox/plexus-xwork-integration/src/main/java/org/codehaus/plexus/xwork/PlexusObjectFactory.java
@@ -1,96 +1,276 @@
 package org.codehaus.plexus.xwork;
 
+import com.opensymphony.webwork.util.ObjectFactoryInitializable;
 import com.opensymphony.xwork.ObjectFactory;
 import com.opensymphony.xwork.Action;
+import com.opensymphony.xwork.Result;
+import com.opensymphony.xwork.config.ConfigurationException;
 import com.opensymphony.xwork.config.entities.ActionConfig;
-import org.codehaus.plexus.personality.plexus.lifecycle.phase.Contextualizable;
-import org.codehaus.plexus.personality.plexus.lifecycle.phase.Initializable;
-import org.codehaus.plexus.personality.plexus.lifecycle.phase.InitializationException;
-import org.codehaus.plexus.context.Context;
-import org.codehaus.plexus.context.ContextException;
+import com.opensymphony.xwork.config.entities.InterceptorConfig;
+import com.opensymphony.xwork.config.entities.ResultConfig;
+import com.opensymphony.xwork.interceptor.Interceptor;
+import com.opensymphony.xwork.util.OgnlUtil;
+import com.opensymphony.xwork.validator.Validator;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.codehaus.plexus.PlexusContainer;
-import org.codehaus.plexus.PlexusConstants;
-import org.codehaus.plexus.logging.LogEnabled;
-import org.codehaus.plexus.logging.Logger;
-import org.codehaus.plexus.component.repository.exception.ComponentLookupException;
+
+import javax.servlet.ServletContext;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author <a href="mailto:trygvis@inamo.no">Trygve Laugst&oslash;l</a>
+ * @author <a href="mailto:evenisse@codehaus.org">Emmanuel Venisse</a>
  * @version $Id$
  */
 public class PlexusObjectFactory
-    extends ObjectFactory
-    implements Initializable, Contextualizable, LogEnabled
+     extends ObjectFactory
+     implements ObjectFactoryInitializable
 {
-    // ----------------------------------------------------------------------
-    // Configuration
-    // ----------------------------------------------------------------------
+    private static final Log log = LogFactory.getLog(PlexusObjectFactory.class);
 
-    private boolean debugMode;
+    private static final String PLEXUS_COMPONENT_TYPE = "plexus.component.type";
 
     // ----------------------------------------------------------------------
     // Privates
     // ----------------------------------------------------------------------
 
-    private PlexusContainer container;
-
-    private boolean chainObjectFactory = true;
-
-    private ObjectFactory objectFactory;
-
-    private Logger logger;
+    private PlexusContainer base;
 
     // ----------------------------------------------------------------------
     // ObjectFactory overrides
     // ----------------------------------------------------------------------
 
-    public void initialize()
-        throws InitializationException
-    {
-        objectFactory = ObjectFactory.getObjectFactory();
+    public void init(ServletContext servletContext) {
+        if (!PlexusLifecycleListener.loaded || !PlexusFilter.loaded) {
+            // uh oh! looks like the lifecycle listener wasn't installed. Let's inform the user
+            String message = "********** FATAL ERROR STARTING UP PLEXUS-WEBWORK INTEGRATION **********\n" +
+                    "Looks like the Plexus listener was not configured for your web app! \n" +
+                    "You need to add the following to web.xml: \n" +
+                    "\n" +
+                    "    <!-- this should be before the webwork filter -->\n" +
+                    "    <filter>\n" +
+                    "        <filter-name>plexus</filter-name>\n" +
+                    "        <filter-class>com.opensymphony.webwork.plexus.PlexusFilter</filter-class>\n" +
+                    "    </filter>\n" +
+                    "\n" +
+                    "...\n" +
+                    "\n" +
+                    "    <!-- this should be before the webwork filter -->\n" +
+                    "    <filter-mapping>\n" +
+                    "        <filter-name>plexus</filter-name>\n" +
+                    "        <url-pattern>/*</url-pattern>\n" +
+                    "    </filter-mapping>\n" +
+                    "\n" +
+                    "...\n" +
+                    "\n" +
+                    "    <listener>\n" +
+                    "        <listener-class>com.opensymphony.webwork.plexus.PlexusLifecycleListener</listener-class>\n" +
+                    "    </listener>";
+            log.fatal(message);
+            return;
+        }
+
+        base = (PlexusContainer) servletContext.getAttribute(PlexusLifecycleListener.KEY);
     }
 
-    public Action buildAction( ActionConfig config )
+    public Object buildAction( String actionName, String namespace, ActionConfig config, Map extraContext )
         throws Exception
     {
-        String roleHint = config.getClassName();
+        if ( extraContext == null )
+        {
+            extraContext = new HashMap();
+        }
+
+        extraContext.put( PLEXUS_COMPONENT_TYPE, Action.class.getName() );
+
+        return super.buildAction( actionName, namespace, config, extraContext );
+    }
+
+    public Interceptor buildInterceptor( InterceptorConfig interceptorConfig, Map interceptorRefParams )
+        throws ConfigurationException
+    {
+        String interceptorClassName = interceptorConfig.getClassName();
+        Map thisInterceptorClassParams = interceptorConfig.getParams();
+        Map params = ( thisInterceptorClassParams == null ) ? new HashMap() : new HashMap( thisInterceptorClassParams );
+        params.putAll( interceptorRefParams );
+
+        String message;
+        Throwable cause;
 
         try
         {
-            return (Action) container.lookup( Action.class.getName(), roleHint );
-        }
-        catch ( ComponentLookupException e )
-        {
-            if ( chainObjectFactory && objectFactory != null )
-            {
-                if ( debugMode )
-                {
-                    logger.info( "Could not look up component '" + roleHint + "', chaining to " + objectFactory, e );
-                }
+            Map extraContext = new HashMap();
+            extraContext.put( PLEXUS_COMPONENT_TYPE, Interceptor.class.getName() );
+            Interceptor interceptor = (Interceptor) buildBean( interceptorClassName, extraContext );
+            OgnlUtil.setProperties( params, interceptor );
+            interceptor.init();
 
-                return objectFactory.buildAction( config );
-            }
-            else if ( debugMode )
+            return interceptor;
+        }
+        catch ( InstantiationException e )
+        {
+            cause = e;
+            message = "Unable to instantiate an instance of Interceptor class [" + interceptorClassName + "].";
+        }
+        catch ( IllegalAccessException e )
+        {
+            cause = e;
+            message = "IllegalAccessException while attempting to instantiate an instance of Interceptor class [" + interceptorClassName + "].";
+        }
+        catch ( ClassCastException e )
+        {
+            cause = e;
+            message = "Class [" + interceptorClassName + "] does not implement com.opensymphony.xwork.interceptor.Interceptor";
+        }
+        catch ( Exception e )
+        {
+            cause = e;
+            message = "Caught Exception while registering Interceptor class " + interceptorClassName;
+        }
+        catch ( NoClassDefFoundError e )
+        {
+            cause = e;
+            message = "Could not load class " + interceptorClassName + ". Perhaps it exists but certain dependencies are not available?";
+        }
+
+        throw new ConfigurationException(message, cause);
+    }
+
+    public Result buildResult( ResultConfig resultConfig, Map extraContext )
+        throws Exception
+    {
+        if ( extraContext == null )
+        {
+            extraContext = new HashMap();
+        }
+
+        extraContext.put( PLEXUS_COMPONENT_TYPE, Result.class.getName() );
+
+        return super.buildResult( resultConfig, extraContext );
+    }
+
+    public Validator buildValidator( String className, Map params, Map extraContext )
+        throws Exception
+    {
+        Map context = new HashMap();
+        context.put( PLEXUS_COMPONENT_TYPE, Validator.class.getName() );
+        Validator validator = (Validator) buildBean( className, context );
+        OgnlUtil.setProperties( params, validator );
+
+        return validator;
+    }
+
+    public Object buildBean( Class clazz, Map extraContext )
+        throws Exception
+    {
+        try
+        {
+            return lookup( clazz.getName(), extraContext );
+        }
+        catch ( Exception e )
+        {
+            if ( extraContext != null )
             {
-                logger.info( "Could not look up component '" + roleHint + "', not chaining.", e );
+                String type = (String) extraContext.get( PLEXUS_COMPONENT_TYPE );
+                
+                if ( type != null )
+                {
+                    return lookup( type, clazz.getName(), extraContext );
+                }
             }
 
             throw e;
         }
     }
 
-    // ----------------------------------------------------------------------
-    // Plexus Lifecycle
-    // ----------------------------------------------------------------------
-
-    public void contextualize( Context context )
-        throws ContextException
+    public Class getClassInstance( String className )
+        throws ClassNotFoundException
     {
-        container = (PlexusContainer) context.get( PlexusConstants.PLEXUS_KEY );
+        PlexusContainer pc = PlexusThreadLocal.getPlexusContainer();
+
+        if ( pc == null )
+        {
+            pc = base;
+        }
+
+        try
+        {
+            return pc.lookup( className ).getClass();
+        }
+        catch ( Exception e1 )
+        {
+            try
+            {
+                return pc.lookup( Action.class.getName(), className ).getClass();
+            }
+            catch ( Exception e2 )
+            {
+                try
+                {
+                    return pc.lookup( Interceptor.class.getName(), className ).getClass();
+                }
+                catch ( Exception e3 )
+                {
+                    try
+                    {
+                        return pc.lookup( Validator.class.getName(), className ).getClass();
+                    }
+                    catch ( Exception e4 )
+                    {
+                        try
+                        {
+                            return pc.lookup( Result.class.getName(), className ).getClass();
+                        }
+                        catch ( Exception e5 )
+                        {
+                            return super.getClassInstance( className );
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    private Object lookup( String role )
+        throws Exception
+    {
+        return lookup( role, null, null );
     }
 
-    public void enableLogging( Logger logger )
+    private Object lookup( String role, Map extraContext )
+        throws Exception
     {
-        this.logger = logger;
+        return lookup( role, null, extraContext );
+    }
+
+    private Object lookup( String role, String roleHint )
+        throws Exception
+    {
+        return lookup( role, roleHint, null );
+    }
+
+    private Object lookup( String role, String roleHint, Map extraContext )
+        throws Exception
+    {
+        PlexusContainer pc = PlexusThreadLocal.getPlexusContainer();
+
+        if ( pc == null )
+        {
+            pc = base;
+        }
+
+        try
+        {
+            return pc.lookup( role, roleHint );
+        }
+        catch ( Exception e )
+        {
+            log.debug( "Can't load component with plexus, try now with webwork.", e );
+            Object o = super.buildBean( super.getClassInstance( role ), extraContext );
+            pc.autowire( o );
+            return o;
+        }
     }
 }
