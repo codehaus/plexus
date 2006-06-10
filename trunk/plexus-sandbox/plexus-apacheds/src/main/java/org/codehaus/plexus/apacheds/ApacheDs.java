5a6056e1b2b37c04d4f2c80048cfc39f35cac688
trygvis <trygvis@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-06-10 19:08:36 +0000

    o Committing some missing stuff.

--- trunk/plexus-sandbox/plexus-apacheds/src/main/java/org/codehaus/plexus/apacheds/ApacheDs.java
+++ trunk/plexus-sandbox/plexus-apacheds/src/main/java/org/codehaus/plexus/apacheds/ApacheDs.java
@@ -2,7 +2,9 @@ package org.codehaus.plexus.apacheds;
 
 import javax.naming.NamingException;
 import javax.naming.directory.InitialDirContext;
+import javax.naming.directory.Attributes;
 import java.io.File;
+import java.util.Set;
 
 /**
  * @author <a href="mailto:trygvis@inamo.no">Trygve Laugst&oslash;l</a>
@@ -20,7 +22,19 @@ public interface ApacheDs
 
     void setEnableNetworking( boolean enableNetworking );
 
+    void addPartition( String name, String root, Set indexedAttributes, Attributes partitionAttributes )
+        throws NamingException;
+
     void addPartition( Partition partition )
+        throws NamingException;
+
+    /**
+     * Creates a partition usable for testing and other light usage.
+     *
+     * @param domainComponents E.g. "plexus", "codehaus", "org"
+     * @throws NamingException
+     */
+    Partition addSimplePartition( String ... domainComponents )
         throws NamingException;
 
     // ----------------------------------------------------------------------
