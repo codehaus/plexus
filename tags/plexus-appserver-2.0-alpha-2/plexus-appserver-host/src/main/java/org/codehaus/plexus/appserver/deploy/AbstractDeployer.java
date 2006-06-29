e0ad45300160c853291c70a878eb981e3340ca87
jesse <jesse@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-06-29 15:26:11 +0000

    [maven-scm] copy for tag plexus-appserver-2.0-alpha-2

---
+++ tags/plexus-appserver-2.0-alpha-2/plexus-appserver-host/src/main/java/org/codehaus/plexus/appserver/deploy/AbstractDeployer.java
@@ -0,0 +1,12 @@
+package org.codehaus.plexus.appserver.deploy;
+
+import org.codehaus.plexus.appserver.AppServerObject;
+
+/**
+ * @author Jason van Zyl
+ */
+public abstract class AbstractDeployer
+    extends AppServerObject
+    implements Deployer
+{
+}
