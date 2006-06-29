e4e6193464a64863cc3510f8827ae35a31230a03
rahul <rahul@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-06-29 09:00:46 +0000

    o  Started with Mojo unit tests.

---
+++ plexus-examples/plexus-website-monitor-plugin/src/test/java/org/codehaus/plexus/MonitorMojoTest.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package org.codehaus.plexus;
+
+import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+
+/**
+ * @author <a href='mailto:rahul.thakur.xdev@gmail.com'>Rahul Thakur</a>
+ * @version $Id:$
+ */
+public class MonitorMojoTest
+    extends AbstractMojoTestCase
+{
+    
+    
+}
