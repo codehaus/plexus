d75fa83cdf839521a638948e9d9962f415e4301e
brett <brett@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-07-06 10:49:43 +0000

    make xwork integration work under jetty6 plugin. A better solution should be found before the next release

--- plexus-sandbox/plexus-xwork-integration/src/main/java/org/codehaus/plexus/xwork/PlexusLifecycleListener.java
+++ plexus-sandbox/plexus-xwork-integration/src/main/java/org/codehaus/plexus/xwork/PlexusLifecycleListener.java
@@ -11,7 +11,12 @@ import javax.servlet.ServletContextListener;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
+import java.util.Enumeration;
 
 public class PlexusLifecycleListener implements ServletContextListener, HttpSessionListener {
     private static final Log log = LogFactory.getLog(PlexusLifecycleListener.class);
@@ -23,7 +28,45 @@ public class PlexusLifecycleListener implements ServletContextListener, HttpSess
         loaded = true;
 
         try {
-            PlexusContainer pc = new DefaultPlexusContainer();
+            // TODO: we should sort something out in Plexus so that this isn't necessary.
+            // When run inside a Maven plugin (eg, the Jetty plugin), this will pick up the Maven core class
+            // configuration instead of the webapp. While ideally Maven would isolate the plugin classes from itself,
+            // Plexus should also be more flexible about how it is configured.
+            // The workaround is just to filter out requests for META-INF/plexus/plexus.xml when it is requested
+            // during construction of the container
+            PlexusContainer pc = new DefaultPlexusContainer( "default", new ClassLoader( getClass().getClassLoader() )
+            {
+                public Enumeration getResources( String name )
+                    throws IOException
+                {
+                    List l = new ArrayList();
+                    for ( Enumeration e = super.getResources( name ); e.hasMoreElements(); )
+                    {
+                        URL url = (URL) e.nextElement();
+
+                        if ( !url.toExternalForm().endsWith( "META-INF/plexus/plexus.xml" ) )
+                        {
+                            l.add( url );
+                        }
+                    }
+
+                    return Collections.enumeration( l );
+                }
+
+                protected Enumeration findResources( String name )
+                    throws IOException
+                {
+                    if ( !name.equals( "META-INF/plexus/plexus.xml" ) )
+                    {
+                        return super.findResources( name );
+                    }
+                    else
+                    {
+                        return Collections.enumeration( Collections.EMPTY_LIST );
+                    }
+                }
+            } );
+
             PlexusUtils.configure(pc, "plexus-application.xml");
             ServletContext ctx = servletContextEvent.getServletContext();
             ctx.setAttribute(KEY, pc);
