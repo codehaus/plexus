589a7eb2963b1ef2dcdb9fb6e9cef0087fc1c249
rahul <rahul@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-07-06 08:07:23 +0000

    o Updates to MonitorMojo unit tests.

--- trunk/plexus-examples/plexus-website-monitor-plugin/src/test/java/org/codehaus/plexus/MonitorMojoTest.java
+++ trunk/plexus-examples/plexus-website-monitor-plugin/src/test/java/org/codehaus/plexus/MonitorMojoTest.java
@@ -5,13 +5,29 @@ package org.codehaus.plexus;
 
 import org.apache.maven.plugin.testing.AbstractMojoTestCase;
 
+import java.io.File;
+
 /**
  * @author <a href='mailto:rahul.thakur.xdev@gmail.com'>Rahul Thakur</a>
- * @version $Id:$
+ * @version $Id$
  */
 public class MonitorMojoTest
     extends AbstractMojoTestCase
 {
-    
-    
+
+    public void testMojoLookup()
+        throws Exception
+    {
+        File pluginXml = new File( getBasedir(), "src/test/resources/unit/plugin-config.xml" );
+        MonitorMojo mojo = (MonitorMojo) lookupMojo( "monitor", pluginXml );
+        assertNotNull( mojo );
+    }
+
+    public void testMojoExecution()
+        throws Exception
+    {
+        File pluginXml = new File( getBasedir(), "src/test/resources/unit/plugin-config.xml" );
+        MonitorMojo mojo = (MonitorMojo) lookupMojo( "monitor", pluginXml );
+        mojo.execute();
+    }
 }
