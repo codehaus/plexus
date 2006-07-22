8c44089184506b59ec45695eadeefd82c9183399
jesse <jesse@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-07-22 22:03:47 +0000

    security api start and simple implementation

---
+++ trunk/plexus-sandbox/plexus-security/plexus-security-api/src/main/java/org/codehaus/plexus/security/DefaultAuthorizer.java
@@ -0,0 +1,55 @@
+package org.codehaus.plexus.security;
+
+import org.codehaus.plexus.security.exception.AuthorizationException;
+import org.codehaus.plexus.security.exception.NotAuthorizedException;
+
+import java.util.Map;
+/*
+ * Copyright 2005 The Codehaus.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * DefaultAuthorizer:
+ *
+ * @author: Jesse McConnell <jesse@codehaus.org>
+ * @version: $ID:$
+ *
+ * @plexus.component
+ *   role="org.codehaus.plexus.security.Authorizer"
+ *   role-hint="default"
+ */
+public class DefaultAuthorizer
+    implements Authorizer
+{
+
+    /**
+     * @plexus.requirement
+     */
+    private AuthorizationStore store;
+
+
+    public boolean isAuthorized( Map tokens )
+        throws AuthorizationException
+    {
+       return store.isAuthorized( tokens );
+    }
+
+    public Authorization authorize( Map tokens )
+        throws NotAuthorizedException, AuthorizationException
+    {
+        return store.authorize( tokens );
+    }
+}
+
