8952d2293a50cf2cd9d70c97f06fdc0321018f15
jesse <jesse@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-07-24 20:44:12 +0000

    apply missing licenses and correct some javadoc

--- trunk/plexus-sandbox/plexus-security/plexus-security-api/src/main/java/org/codehaus/plexus/security/AuthenticationResult.java
+++ trunk/plexus-sandbox/plexus-security/plexus-security-api/src/main/java/org/codehaus/plexus/security/AuthenticationResult.java
@@ -1,10 +1,25 @@
 package org.codehaus.plexus.security;
+/*
+ * Copyright 2005 The Codehaus.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import java.io.Serializable;
 
 /**
- * @author <a hrel="mailto:evenisse@codehaus.org">Emmanuel Venisse</a>
- * @version $Id: AuthenticationResult.java 2990 2006-01-09 16:33:52Z evenisse $
+ * @author Jesse McConnell <jesse@codehaus.org>
+ * @version $ID:$
  */
 public class AuthenticationResult
     implements Serializable
