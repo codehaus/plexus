2b028dc3ea670c284673d34bad3d74bf21e9f36e
eredmond <eredmond@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-08-09 13:49:00 +0000

    All java classes may be passed into the script.

--- trunk/plexus-component-factories/plexus-jruby-factory/src/test/resources/hello.rb
+++ trunk/plexus-component-factories/plexus-jruby-factory/src/test/resources/hello.rb
@@ -1,10 +1,29 @@
+require 'java'
+
+#include_class 'java.util.Random'
+#include_class 'java.lang.Integer'
 
 class Hello
-  def execute
-    puts "from: #{$INPUTS[ 'from_class' ]}"
+  # All non-primitives must be wrapped in "JavaObject"s. This looks
+  # to be the only way to call a method.
+  def java_method( method_sym, java_object, *params )
+    return java_object.java_class.java_method( method_sym.to_s ).invoke( java_object, *params )
   end
+
+  def execute
+    puts $INPUTS[ 'from_class' ].name
+
+#    puts java_method( :setSeed, $INPUTS[ 'random' ], 1 )
+#    $INPUTS[ 'random' ].java_class.java_instance_methods[0].class
+  end
+
   def print( msg )
     puts "printed: #{msg}"
+#    $INPUTS[ 'random' ].java_class.java_instance_methods.each { |m| puts m.name }
+    $INPUTS[ 'random' ].java_class.java_instance_methods.each { |m| puts m.name } #java_instance_methods.each { |m| puts m.name }
+    puts "-------------"
+    $INPUTS[ 'random' ].java_class.methods.each { |m| puts m } #java_instance_methods.each { |m| puts m.name }
+#    puts java_method( :setSeed, $INPUTS[ 'random' ], 1 )
   end
 end
 
