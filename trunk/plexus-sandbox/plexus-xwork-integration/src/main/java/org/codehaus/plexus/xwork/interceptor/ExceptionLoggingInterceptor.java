5179db30e504b085b3e31a52be885cc7ecba2d1e
carlos <carlos@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-08-13 20:04:15 +0000

    [CONTINUUM-778] Show internal error page on unhandled exceptions. Add a exception handler that logs the exceptions too.

---
+++ trunk/plexus-sandbox/plexus-xwork-integration/src/main/java/org/codehaus/plexus/xwork/interceptor/ExceptionLoggingInterceptor.java
@@ -0,0 +1,47 @@
+package org.codehaus.plexus.xwork.interceptor;
+
+/*
+ * Copyright 2006 The Codehaus.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.opensymphony.xwork.ActionInvocation;
+import com.opensymphony.xwork.interceptor.ExceptionHolder;
+import com.opensymphony.xwork.interceptor.ExceptionMappingInterceptor;
+
+/**
+ * Exception interceptor that will also log the exception.
+ * 
+ * @author <a href="mailto:nramirez@exist.com">Napoleon Esmundo C. Ramirez</a>
+ * @version $Id$
+ */
+public class ExceptionLoggingInterceptor
+    extends ExceptionMappingInterceptor
+{
+
+    private static final long serialVersionUID = -1776743136472264546L;
+
+    private static final Log log = LogFactory.getLog( ExceptionLoggingInterceptor.class );
+
+    protected void publishException( ActionInvocation invocation, ExceptionHolder exceptionHolder )
+    {
+        Throwable e = exceptionHolder.getException();
+        log.info( "Error ocurred during execution", e );
+
+        super.publishException( invocation, exceptionHolder );
+    }
+}
