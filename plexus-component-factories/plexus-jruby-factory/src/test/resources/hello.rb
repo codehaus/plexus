2ae2e48d3bec7dd97e0c45cfaa34c305658ee8a6
eredmond <eredmond@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-08-19 04:12:43 +0000

    Now use BSF in Invoker

--- plexus-component-factories/plexus-jruby-factory/src/test/resources/hello.rb
+++ plexus-component-factories/plexus-jruby-factory/src/test/resources/hello.rb
@@ -1,29 +1,12 @@
-require 'java'
-
-#include_class 'java.util.Random'
-#include_class 'java.lang.Integer'
 
 class Hello
-  # All non-primitives must be wrapped in "JavaObject"s. This looks
-  # to be the only way to call a method.
-  def java_method( method_sym, java_object, *params )
-    return java_object.java_class.java_method( method_sym.to_s ).invoke( java_object, *params )
-  end
-
   def execute
-    puts $INPUTS[ 'from_class' ].name
-
-#    puts java_method( :setSeed, $INPUTS[ 'random' ], 1 )
-#    $INPUTS[ 'random' ].java_class.java_instance_methods[0].class
+    puts "java.util.Random.nextInt: #{$random.nextInt}"
+    puts "from_class: #{$from_class.to_s}"
   end
 
   def print( msg )
     puts "printed: #{msg}"
-#    $INPUTS[ 'random' ].java_class.java_instance_methods.each { |m| puts m.name }
-    $INPUTS[ 'random' ].java_class.java_instance_methods.each { |m| puts m.name } #java_instance_methods.each { |m| puts m.name }
-    puts "-------------"
-    $INPUTS[ 'random' ].java_class.methods.each { |m| puts m } #java_instance_methods.each { |m| puts m.name }
-#    puts java_method( :setSeed, $INPUTS[ 'random' ], 1 )
   end
 end
 
