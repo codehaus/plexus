86a6060d48afcdb6823d2ecf05f0a23f5801df84
joakime <joakime@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-09-06 15:39:44 +0000

    * Added missing JavaDoc. * Added List UserManager.getUsers() * Added User UserManager.createUser(username, fullname, email) * Added User UserManager.findUser(username) * Added void UserManager.deleteUser(username) * Added needed UserManagerException (RuntimeException) for providers to toss in case of misconfiguration or other internal error.

--- trunk/plexus-sandbox/plexus-components/plexus-security/plexus-security-user-management/plexus-security-user-management-api/src/main/java/org/codehaus/plexus/security/user/UserManager.java
+++ trunk/plexus-sandbox/plexus-components/plexus-security/plexus-security-user-management/plexus-security-user-management-api/src/main/java/org/codehaus/plexus/security/user/UserManager.java
@@ -1,20 +1,91 @@
 package org.codehaus.plexus.security.user;
 
+import java.util.List;
+
 /**
  * User Manager Interface
  * 
  * @author Jason van Zyl
+ * @author <a href="mailto:joakim@erdfelt.com">Joakim Erdfelt</a>
  */
 public interface UserManager
 {
     public static final String ROLE = UserManager.class.getName();
+    
+    /**
+     * Factory method to create new User Objects based on provider specific
+     * implementation.
+     * 
+     * User objects created this way do not exist in the provider's underlying
+     * data store until a call to {@link #addUser(User)} is made.
+     * 
+     * @param username the username for this user.
+     * @param fullName the full name for this user.
+     * @param emailAddress the email address for this user.
+     * 
+     * @return the new user object ready to use.
+     */
+    User createUser(String username, String fullName, String emailAddress);
+    
+    /**
+     * Get the List of {@link User} objects.
+     * 
+     * @return the List of {@link User} Objects.
+     */
+    List getUsers();
 
+    /**
+     * Add a User.
+     * 
+     * @param user the user to add.
+     * @return the user that was just added.
+     */
     User addUser( User user );
 
+    /**
+     * Update a User.
+     * 
+     * @param user the user to update.
+     * @return the user that was just updated.
+     * @throws UserNotFoundException if the user was not found to update.
+     */
     User updateUser( User user );
+    
+    /**
+     * Find a User using a User name.
+     * 
+     * @param username the username to find.
+     * @return the user.
+     * @throws UserNotFoundException if the user was not found.
+     */
+    User findUser( String username )
+        throws UserNotFoundException;
 
+    /**
+     * Find a User using the principal.
+     * 
+     * @param principal the principal to look for.
+     * @return the user.
+     * @throws UserNotFoundException if the user was not found.
+     */
     User findUser( Object principal )
         throws UserNotFoundException;
 
-    void deleteUser( Object principal );
+    /**
+     * Delete a user using the principal.
+     * 
+     * @param principal the principal to look for.
+     * @throws UserNotFoundException the user was not found.
+     */
+    void deleteUser( Object principal )
+        throws UserNotFoundException;
+    
+    /**
+     * Delete a user using the username.
+     * 
+     * @param principal the principal to look for.
+     * @throws UserNotFoundException the user was not found.
+     */
+    void deleteUser( String username )
+        throws UserNotFoundException;
 }
