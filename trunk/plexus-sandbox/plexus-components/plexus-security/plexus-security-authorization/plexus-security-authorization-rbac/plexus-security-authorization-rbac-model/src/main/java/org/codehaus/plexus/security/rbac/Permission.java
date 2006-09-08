81785429d37883c0926bd3e288045ffffd399992
joakime <joakime@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-09-08 07:38:42 +0000

    authorization rbac rearchitect for to conform to api/provider pattern needed by jpox/xstream/hibernate.

---
+++ trunk/plexus-sandbox/plexus-components/plexus-security/plexus-security-authorization/plexus-security-authorization-rbac/plexus-security-authorization-rbac-model/src/main/java/org/codehaus/plexus/security/rbac/Permission.java
@@ -0,0 +1,70 @@
+package org.codehaus.plexus.security.rbac;
+
+/*
+ * Copyright 2001-2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Permission 
+ *
+ * @author Jesse McConnell <jmcconnell@apache.org>
+ * @author <a href="mailto:joakim@erdfelt.com">Joakim Erdfelt</a>
+ * @version $Id$
+ * @todo fix and expand javadoc
+ */
+public interface Permission
+{
+    /**
+     * Plexus Role Name
+     */
+    public static final String ROLE = Permission.class.getName();
+
+    /**
+     * Get null
+     */
+    public String getDescription();
+
+    /**
+     * Get null
+     */
+    public String getName();
+
+    /**
+     * Get null
+     */
+    public Operation getOperation();
+
+    /**
+     * Set null
+     * 
+     * @param description
+     */
+    public void setDescription( String description );
+
+    /**
+     * Set null
+     * 
+     * @param name
+     */
+    public void setName( String name );
+
+    /**
+     * Set null
+     * 
+     * @param operation
+     */
+    public void setOperation( Operation operation );
+
+}
---
+++ trunk/plexus-sandbox/plexus-components/plexus-security/plexus-security-authorization/plexus-security-authorization-rbac/plexus-security-authorization-rbac-model/src/main/java/org/codehaus/plexus/security/rbac/RBACModel.java
@@ -0,0 +1,87 @@
+package org.codehaus.plexus.security.rbac;
+
+import java.util.List;
+
+/*
+ * Copyright 2001-2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * RBACModel 
+ *
+ * @author Jesse McConnell <jmcconnell@apache.org>
+ * @author <a href="mailto:joakim@erdfelt.com">Joakim Erdfelt</a>
+ * @version $Id$
+ * @todo expand on javadoc
+ */
+public interface RBACModel
+{
+    /**
+     * Plexus Role Name
+     */
+    public static final String ROLE = RBACModel.class.getName();
+
+    /**
+     * Method addAssignment
+     * 
+     * @param userAssignment
+     */
+    public void addAssignment( UserAssignment userAssignment );
+
+    /**
+     * Method addRole
+     * 
+     * @param role
+     */
+    public void addRole( Role role );
+
+    /**
+     * Method getAssignments
+     */
+    public List getAssignments();
+
+    /**
+     * Method getRoles
+     */
+    public List getRoles();
+
+    /**
+     * Method removeAssignment
+     * 
+     * @param userAssignment
+     */
+    public void removeAssignment( UserAssignment userAssignment );
+
+    /**
+     * Method removeRole
+     * 
+     * @param role
+     */
+    public void removeRole( Role role );
+
+    /**
+     * Set null
+     * 
+     * @param assignments
+     */
+    public void setAssignments( List assignments );
+
+    /**
+     * Set The roles available to be assigned
+     * 
+     * @param roles
+     */
+    public void setRoles( List roles );
+}
