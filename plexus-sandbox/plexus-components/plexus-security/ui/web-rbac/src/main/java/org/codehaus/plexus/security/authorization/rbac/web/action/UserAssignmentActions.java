dfa621d08b2423960dd4bb41688682691344c711
jesse <jesse@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-09-09 13:05:24 +0000

    fixing api changes on rbac manager

--- plexus-sandbox/plexus-components/plexus-security/ui/web-rbac/src/main/java/org/codehaus/plexus/security/authorization/rbac/web/action/UserAssignmentActions.java
+++ plexus-sandbox/plexus-components/plexus-security/ui/web-rbac/src/main/java/org/codehaus/plexus/security/authorization/rbac/web/action/UserAssignmentActions.java
@@ -56,7 +56,7 @@ public class UserAssignmentActions
     {
         try
         {
-            assignedRoles = manager.getAssignedRoles( principal );
+            assignedRoles = manager.getAssignedRoles( principal ).getRoles();
 
             availableRoles = manager.getAllAssignableRoles();
         }
