ab66baf8b481f3d5064d22c240237014919c7350
rahul <rahul@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-09-10 03:16:30 +0000

    o  refactorings for updated Authentication API

--- plexus-sandbox/plexus-components/plexus-security/authentication/providers/acegi/src/main/java/org/codehaus/plexus/security/acegi/DefaultAuthenticationTokenFactory.java
+++ plexus-sandbox/plexus-components/plexus-security/authentication/providers/acegi/src/main/java/org/codehaus/plexus/security/acegi/DefaultAuthenticationTokenFactory.java
@@ -2,24 +2,9 @@ package org.codehaus.plexus.security.acegi;
 
 import org.acegisecurity.Authentication;
 import org.acegisecurity.providers.UsernamePasswordAuthenticationToken;
-import org.codehaus.plexus.security.exception.AuthenticationException;
+import org.codehaus.plexus.security.authentication.AuthenticationException;
 
 import java.util.Map;
-/*
- * Copyright 2005 The Codehaus.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 /**
  * DefaultAuthenticationTokenFactory:
@@ -31,8 +16,7 @@ import java.util.Map;
  * @author: Jesse McConnell <jesse@codehaus.org>
  * @version: $ID:$
  *
- * @plexus.component
- *   role="org.codehaus.plexus.security.acegi.AuthenticationTokenFactory"
+ * @plexus.component role="org.codehaus.plexus.security.acegi.AuthenticationTokenFactory"
  */
 public class DefaultAuthenticationTokenFactory
     implements AuthenticationTokenFactory
@@ -50,7 +34,7 @@ public class DefaultAuthenticationTokenFactory
         // if not configured, the check the tokenMap
         if ( tokenType == null )
         {
-            tokenType = (String)tokenMap.get( "authTokenType" );
+            tokenType = (String) tokenMap.get( "authTokenType" );
         }
 
         // if tokenType is still null then throw exception
