b9e651ec227de56fca9a718f4e7ac6503869ee48
joakime <joakime@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-09-12 02:47:17 +0000

    Adding new unit tests.

--- trunk/plexus-sandbox/plexus-components/plexus-security/authorization/rbac/stores/test/src/main/java/org/codehaus/plexus/security/authorization/store/test/AbstractRbacManagerTestCase.java
+++ trunk/plexus-sandbox/plexus-components/plexus-security/authorization/rbac/stores/test/src/main/java/org/codehaus/plexus/security/authorization/store/test/AbstractRbacManagerTestCase.java
@@ -17,6 +17,7 @@ package org.codehaus.plexus.security.authorization.store.test;
  */
 
 import org.codehaus.plexus.PlexusTestCase;
+import org.codehaus.plexus.security.rbac.Operation;
 import org.codehaus.plexus.security.rbac.Permission;
 import org.codehaus.plexus.security.rbac.RBACManager;
 import org.codehaus.plexus.security.rbac.RbacObjectInvalidException;
@@ -389,5 +390,147 @@ public class AbstractRbacManagerTestCase
 
         assertNotNull( assignedPermissions );
         assertEquals( 1, assignedPermissions.size() );
+    }
+    
+    public void testLargeApplicationInit() throws RbacStoreException, RbacObjectNotFoundException
+    {
+        RBACManager manager = getRbacManager();
+
+        if ( !manager.operationExists( "add-repository" ) )
+        {
+            Operation operation = manager.createOperation( "add-repository" );
+            manager.saveOperation( operation );
+        }
+
+        if ( !manager.operationExists( "edit-repository" ) )
+        {
+            Operation operation = manager.createOperation( "edit-repository" );
+            manager.saveOperation( operation );
+        }
+
+        if ( !manager.operationExists( "delete-repository" ) )
+        {
+            Operation operation = manager.createOperation( "delete-repository" );
+            manager.saveOperation( operation );
+        }
+
+        if ( !manager.operationExists( "edit-configuration" ) )
+        {
+            Operation operation = manager.createOperation( "edit-configuration" );
+            manager.saveOperation( operation );
+        }
+
+        if ( !manager.operationExists( "run-indexer" ) )
+        {
+            Operation operation = manager.createOperation( "run-indexer" );
+            manager.saveOperation( operation );
+        }
+
+        if ( !manager.operationExists( "regenerate-index" ) )
+        {
+            Operation operation = manager.createOperation( "regenerate-index" );
+            manager.saveOperation( operation );
+        }
+
+        if ( !manager.operationExists( "get-reports" ) )
+        {
+            Operation operation = manager.createOperation( "get-reports" );
+            manager.saveOperation( operation );
+        }
+
+        if ( !manager.operationExists( "regenerate-reports" ) )
+        {
+            Operation operation = manager.createOperation( "regenerate-reports" );
+            manager.saveOperation( operation );
+        }
+
+        if ( !manager.operationExists( "edit-user" ) )
+        {
+            Operation operation = manager.createOperation( "edit-user" );
+            manager.saveOperation( operation );
+        }
+
+        if ( !manager.operationExists( "edit-all-users" ) )
+        {
+            Operation operation = manager.createOperation( "edit-all-users" );
+            manager.saveOperation( operation );
+        }
+
+        if ( !manager.operationExists( "remove-roles" ) )
+        {
+            Operation operation = manager.createOperation( "remove-roles" );
+            manager.saveOperation( operation );
+        }
+        
+        if ( !manager.permissionExists( "Edit Configuration" ) )
+        {
+            Permission editConfiguration = manager.createPermission( "Edit Configuration", "edit-configuration",
+                                                                     manager.getGlobalResource().getIdentifier() );
+            manager.savePermission( editConfiguration );
+        }
+
+        if ( !manager.permissionExists( "Run Indexer" ) )
+        {
+            Permission runIndexer = manager.createPermission( "Run Indexer", "run-indexer", manager.getGlobalResource()
+                .getIdentifier() );
+
+            manager.savePermission( runIndexer );
+        }
+
+        if ( !manager.permissionExists( "Add Repository" ) )
+        {
+            Permission runIndexer = manager.createPermission( "Add Repository", "add-repository", manager
+                .getGlobalResource().getIdentifier() );
+            manager.savePermission( runIndexer );
+        }
+
+        if ( !manager.permissionExists( "Edit All Users" ) )
+        {
+            Permission editAllUsers = manager.createPermission( "Edit All Users", "edit-all-users", manager
+                .getGlobalResource().getIdentifier() );
+
+            manager.savePermission( editAllUsers );
+        }
+
+        if ( !manager.permissionExists( "Remove Roles" ) )
+        {
+            Permission editAllUsers = manager.createPermission( "Remove Roles", "remove-roles", manager
+                .getGlobalResource().getIdentifier() );
+
+            manager.savePermission( editAllUsers );
+        }
+
+        if ( !manager.permissionExists( "Regenerate Index" ) )
+        {
+            Permission regenIndex = manager.createPermission( "Regenerate Index", "regenerate-index", manager
+                .getGlobalResource().getIdentifier() );
+
+            manager.savePermission( regenIndex );
+        }
+        
+        if ( !manager.roleExists( "User Administrator" ) )
+        {
+            Role userAdmin = manager.createRole( "User Administrator" );
+            userAdmin.addPermission( manager.getPermission( "Edit All Users" ) );
+            userAdmin.addPermission( manager.getPermission( "Remove Roles" ) );
+            userAdmin.setAssignable( true );
+            manager.saveRole( userAdmin );
+        }
+        
+        if ( !manager.roleExists( "System Administrator" ) )
+        {
+            Role admin = manager.createRole( "System Administrator" );
+            admin.addChildRole( manager.getRole( "User Administrator" ) );
+            admin.addPermission( manager.getPermission( "Edit Configuration" ) );
+            admin.addPermission( manager.getPermission( "Run Indexer" ) );
+            admin.addPermission( manager.getPermission( "Add Repository" ) );
+            admin.addPermission( manager.getPermission( "Regenerate Index" ) );
+            admin.setAssignable( true );
+            manager.saveRole( admin );
+        }
+        
+        assertEquals( 6, manager.getAllPermissions().size() );
+        assertEquals( 11, manager.getAllOperations().size() );
+        assertEquals( 2, manager.getAllRoles().size() );
     }
 }
