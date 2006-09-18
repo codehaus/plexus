2030d7f2005246ab271aefad2a937df7a082f94d
jesse <jesse@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-09-18 19:30:14 +0000

    added user management basic role

---
+++ trunk/plexus-sandbox/plexus-components/plexus-security/ui/web/integration/src/main/java/org/codehaus/plexus/security/ui/web/role/template/RoleConstants.java
@@ -0,0 +1,31 @@
+package org.codehaus.plexus.security.ui.web.role.template;
+/*
+ * Copyright 2005 The Codehaus.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * RoleConstants:
+ *
+ * @author: Jesse McConnell <jesse@codehaus.org>
+ * @version: $ID:$
+ */
+public class RoleConstants
+{
+    public static final String USER_MANAGEMENT_ROLE = "User Administrator";
+    public static final String USER_MANAGEMENT_PERMISSION = "user-management-permission";
+    public static final String USER_MANAGEMENT_MASTER_OPERATION = "user-management-master-operation";
+    public static final String USER_MANAGEMENT_ROLE_ADMIN_OPERATION = "user-management-role-admin-operation";
+    public static final String USER_MANAGEMENT_EDIT_DETAILS_OPERATION = "user-management-edit-details-operation";                   
+}
