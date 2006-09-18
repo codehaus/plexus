2e2911171fb231d660cd7c333b7560ca8e857e9e
joakime <joakime@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-09-18 13:32:49 +0000

    Adding RBACManagerListener implementation

--- trunk/plexus-sandbox/plexus-components/plexus-security/authorization/rbac/model/src/main/java/org/codehaus/plexus/security/rbac/RBACManager.java
+++ trunk/plexus-sandbox/plexus-components/plexus-security/authorization/rbac/model/src/main/java/org/codehaus/plexus/security/rbac/RBACManager.java
@@ -1,7 +1,7 @@
 package org.codehaus.plexus.security.rbac;
 
 /*
- * Copyright 2001-2006 The Apache Software Foundation.
+ * Copyright 2001-2006 The Codehaus.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,6 +35,10 @@ public interface RBACManager
      * Plexus Role Name
      */
     public static final String ROLE = RBACManager.class.getName();
+    
+    public void addListener( RBACManagerListener listener );
+    
+    public void removeListener( RBACManagerListener listener );
 
     // ------------------------------------------------------------------
     // Role Methods
