95f6cdcd13004299457edd064d0cc80be2bbd761
jesse <jesse@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-09-18 16:16:13 +0000

    adding role template interface for organizing roles in the client application

---
+++ trunk/plexus-sandbox/plexus-components/plexus-security/authorization/rbac/template/src/main/java/org/codehaus/plexus/rbac/template/RoleTemplate.java
@@ -0,0 +1,49 @@
+package org.codehaus.plexus.rbac.template;
+
+/*
+ * Copyright 2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * RoleTemplate: Implementations of this interface should construct a role and pass it
+ * back to the calling code.  The implementation should also add the role to the store as well
+ * via the RBACManager.
+ *
+ * Implementations of this interface address the need for the generation of a new role
+ * specifically targeted at a particular resource.  This necessitates the creation of a
+ * consistent set of Permissions for dealing with Operations on the resource, and then the
+ * adding of the Permissions to the Role and finally the Role itself.
+ *
+ * @author Jesse McConnell <jmcconnell@apache.org>
+ * @version $Id:$
+ */
+public interface RoleTemplate
+{
+    public static final String ROLE = RoleTemplate.class.getName();
+
+    /**
+     * Create the role desired using the resource as the potentially dynamic resource.
+     *
+     * In strict RBAC terms the Resource is actually the Object in P( Operation, Object )
+     * notations.
+     *
+     * @param resource
+     * @return
+     * @throws RoleTemplateException
+     */
+    public boolean createRole( String roleName, String resource )
+        throws RoleTemplateException;
+
+}
