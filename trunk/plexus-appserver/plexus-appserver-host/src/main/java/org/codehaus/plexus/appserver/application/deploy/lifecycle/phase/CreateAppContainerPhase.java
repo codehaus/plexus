a88c24e03660c6cfb5729b061793642759d30a89
brett <brett@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-10-01 00:21:39 +0000

    fix container instantiation on reload Submitted by: Pete Marvin King

--- trunk/plexus-appserver/plexus-appserver-host/src/main/java/org/codehaus/plexus/appserver/application/deploy/lifecycle/phase/CreateAppContainerPhase.java
+++ trunk/plexus-appserver/plexus-appserver-host/src/main/java/org/codehaus/plexus/appserver/application/deploy/lifecycle/phase/CreateAppContainerPhase.java
@@ -24,6 +24,7 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.Collections;
 import java.util.Enumeration;
 import java.util.Iterator;
 import java.util.Map;
@@ -48,8 +49,8 @@ public class CreateAppContainerPhase
 
         try
         {
-            applicationContainer =
-                new DefaultPlexusContainer( name, appServerContainer.getClassWorld(), appServerContainer );
+            applicationContainer = ( DefaultPlexusContainer )
+                appServerContainer.createChildContainer( name, Collections.EMPTY_LIST, Collections.EMPTY_MAP );
         }
         catch ( PlexusContainerException e )
         {
