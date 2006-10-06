23724b2efd881eeef9e1c937372e740becf334c0
jesse <jesse@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-10-06 20:08:52 +0000

    [maven-scm] copy for tag plexus-security-1.0-alpha-5

---
+++ tags/plexus-security-1.0-alpha-5/authentication/keys/api/src/main/java/org/codehaus/plexus/security/keys/utils/Hex.java
@@ -0,0 +1,48 @@
+package org.codehaus.plexus.security.keys.utils;
+
+/*
+ * Copyright 2001-2006 The Codehaus.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Hex 
+ *
+ * @author <a href="mailto:joakim@erdfelt.com">Joakim Erdfelt</a>
+ * @version $Id$
+ */
+public class Hex
+{
+    private static final byte[] DIGITS = "0123456789abcdef".getBytes();
+
+    public static String encode( byte[] data )
+    {
+        int l = data.length;
+
+        byte[] raw = new byte[l * 2];
+
+        for ( int i = 0, j = 0; i < l; i++ )
+        {
+            raw[j++] = DIGITS[( 0xF0 & data[i] ) >>> 4];
+            raw[j++] = DIGITS[0x0F & data[i]];
+        }
+
+        return new String( raw );
+    }
+
+    public static String encode( String raw )
+    {
+        return encode( raw.getBytes() );
+    }
+}
---
+++ tags/plexus-security-1.0-alpha-5/ui/web/integration/src/main/java/org/codehaus/plexus/security/ui/web/action/admin/UserCreateAction.java
@@ -0,0 +1,127 @@
+package org.codehaus.plexus.security.ui.web.action.admin;
+
+/*
+ * Copyright 2001-2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.codehaus.plexus.security.rbac.Resource;
+import org.codehaus.plexus.security.ui.web.action.AbstractUserCredentialsAction;
+import org.codehaus.plexus.security.ui.web.interceptor.SecureActionBundle;
+import org.codehaus.plexus.security.ui.web.interceptor.SecureActionException;
+import org.codehaus.plexus.security.ui.web.model.CreateUserCredentials;
+import org.codehaus.plexus.security.ui.web.role.profile.RoleConstants;
+import org.codehaus.plexus.security.user.User;
+
+/**
+ * UserCreateAction 
+ *
+ * @author <a href="mailto:joakim@erdfelt.com">Joakim Erdfelt</a>
+ * @version $Id$
+ * 
+ * @plexus.component role="com.opensymphony.xwork.Action"
+ *                   role-hint="pss-admin-user-create"
+ *                   instantiation-strategy="per-lookup"
+ */
+public class UserCreateAction
+    extends AbstractUserCredentialsAction
+{
+    // ------------------------------------------------------------------
+    // Action Parameters
+    // ------------------------------------------------------------------
+    
+    private CreateUserCredentials user;
+    
+    // ------------------------------------------------------------------
+    // Action Entry Points - (aka Names)
+    // ------------------------------------------------------------------
+
+    public String show()
+    {
+        if ( user == null )
+        {
+            user = new CreateUserCredentials();
+        }
+
+        return INPUT;
+    }
+
+    public String submit()
+    {
+        if ( user == null )
+        {
+            user = new CreateUserCredentials( );
+            addActionError( "Invalid user credentials." );
+            return ERROR;
+        }
+        
+        internalUser = user;
+        
+        validateCredentialsLoose();
+
+        // NOTE: Do not perform Password Rules Validation Here.
+
+        if ( manager.userExists( user.getUsername() ) )
+        {
+            // Means that the role name doesn't exist.
+            // We need to fail fast and return to the previous page.
+            addActionError( "User '" + user.getUsername() + "' already exists." );
+        }
+
+        if ( hasActionErrors() || hasFieldErrors() )
+        {
+            return ERROR;
+        }
+
+        User u = manager.createUser( user.getUsername(), user.getFullName(), user.getEmail() );
+        u.setPassword( user.getPassword() );
+
+        // Disable Password Rules for this creation.
+        securityPolicy.setEnabled( false );
+        try
+        {
+            manager.addUser( u );
+        }
+        finally
+        {
+            securityPolicy.setEnabled( true );
+        }
+
+        return SUCCESS;
+    }
+
+    // ------------------------------------------------------------------
+    // Parameter Accessor Methods
+    // ------------------------------------------------------------------
+
+    public CreateUserCredentials getUser()
+    {
+        return user;
+    }
+
+    public void setUser( CreateUserCredentials user )
+    {
+        this.user = user;
+    }
+    
+    public SecureActionBundle initSecureActionBundle()
+        throws SecureActionException
+    {
+        SecureActionBundle bundle = new SecureActionBundle();
+        bundle.setRequiresAuthentication( true );
+        bundle.addRequiredAuthorization( RoleConstants.USER_MANAGEMENT_USER_CREATE_OPERATION, Resource.GLOBAL );
+        return bundle;
+    }
+
+}
