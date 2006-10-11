7a7c18ab46daee41ef944649d604584d0db86127
handyande <handyande@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-10-11 22:16:47 +0000

    [maven-scm] copy for tag plexus-appserver-service-irc-1.0-alpha-1

---
+++ tags/plexus-appserver-service-irc-1.0-alpha-1/plexus-appserver-service-irc-api/src/main/java/org/codehaus/plexus/service/irc/AbstractIRCCommand.java
@@ -0,0 +1,19 @@
+package org.codehaus.plexus.service.irc;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: aje
+ * Date: 29-Jul-2006
+ * Time: 21:27:51
+ */
+public abstract class AbstractIRCCommand implements IRCCommand {
+  public void onCommand(String channel, IRCUser user, String message) { }
+
+  public void onPrivateCommand(IRCUser user, String message) {
+    onCommand(user.getNick(), user, message);
+  }
+
+  public String getHelp(String parameters) {
+    return "No help avaialble for this command";
+  }
+}
