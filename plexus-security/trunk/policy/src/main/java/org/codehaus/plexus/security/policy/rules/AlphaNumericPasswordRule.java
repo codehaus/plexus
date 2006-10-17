da0104139833759d07a448e0415432f098074b68
joakime <joakime@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-10-17 21:19:52 +0000

    Merging functionality from maven-user into plexus-security. * Adding AcegiPasswordEncoder. * Modifying AcegiPasswordEncoder to support encoding technique identical to SHA256PasswordEncoder. * Adding AlphaNumericPasswordRule. * Adding WhitespacePasswordRule.

---
+++ plexus-security/trunk/policy/src/main/java/org/codehaus/plexus/security/policy/rules/AlphaNumericPasswordRule.java
@@ -0,0 +1,52 @@
+package org.codehaus.plexus.security.policy.rules;
+
+/*
+ * Copyright 2001-2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.codehaus.plexus.security.policy.PasswordRuleViolations;
+import org.codehaus.plexus.security.policy.UserSecurityPolicy;
+import org.codehaus.plexus.security.user.User;
+
+/**
+ * Basic Password Rule. Checks that password only contains alpha-numeric characters. 
+ * 
+ * @plexus.component role="org.codehaus.plexus.security.policy.PasswordRule" role-hint="alpha-numeric"
+ * 
+ * $Id$
+ */
+public class AlphaNumericPasswordRule
+    extends AbstractPasswordRule
+{
+    public void setUserSecurityPolicy( UserSecurityPolicy policy )
+    {
+        // Ignore, policy not needed in this rule.
+    }
+
+    public void testPassword( PasswordRuleViolations violations, User user )
+    {
+        char[] password = user.getPassword().toCharArray();
+
+        for ( int i = 0; i < password.length; i++ )
+        {
+            if ( !Character.isLetterOrDigit( password[i] ) )
+            {
+                violations.addViolation( "user.password.violation.alphanum.only" );
+                return;
+            }
+        }
+    }
+
+}
---
+++ plexus-security/trunk/policy/src/main/java/org/codehaus/plexus/security/policy/rules/WhitespacePasswordRule.java
@@ -0,0 +1,52 @@
+package org.codehaus.plexus.security.policy.rules;
+
+/*
+ * Copyright 2001-2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.codehaus.plexus.security.policy.PasswordRuleViolations;
+import org.codehaus.plexus.security.policy.UserSecurityPolicy;
+import org.codehaus.plexus.security.user.User;
+
+/**
+ * Basic Password Rule. Checks that password does not have whitespaces in it. 
+ * 
+ * @plexus.component role="org.codehaus.plexus.security.policy.PasswordRule" role-hint="no-whitespaces"
+ * 
+ * $Id$
+ */
+public class WhitespacePasswordRule 
+    extends AbstractPasswordRule
+{
+    public void setUserSecurityPolicy( UserSecurityPolicy policy )
+    {
+        // Ignore, policy not needed in this rule.
+    }
+
+    public void testPassword( PasswordRuleViolations violations, User user )
+    {
+        char[] password = user.getPassword().toCharArray();
+        
+        for ( int i = 0; i < password.length; i++ )
+        {
+            if ( Character.isWhitespace( password[i] ) )
+            {
+                violations.addViolation( "user.password.violation.whitespace.detected" );
+                return;
+            }
+        }
+    }
+
+}
