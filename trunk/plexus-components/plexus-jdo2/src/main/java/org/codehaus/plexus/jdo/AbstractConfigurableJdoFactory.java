3098d7944586d80560e42e2de4d78595f36d20c7
joakime <joakime@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-11-02 20:49:59 +0000

    * Cleaning up source code license headers. * Ensuring that setProperty() cannot result in NPE.

--- trunk/plexus-components/plexus-jdo2/src/main/java/org/codehaus/plexus/jdo/AbstractConfigurableJdoFactory.java
+++ trunk/plexus-components/plexus-jdo2/src/main/java/org/codehaus/plexus/jdo/AbstractConfigurableJdoFactory.java
@@ -1,5 +1,21 @@
 package org.codehaus.plexus.jdo;
 
+/*
+ * Copyright 2001-2006 The Codehaus.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import org.codehaus.plexus.logging.AbstractLogEnabled;
 import org.codehaus.plexus.personality.plexus.lifecycle.phase.Initializable;
 import org.codehaus.plexus.personality.plexus.lifecycle.phase.InitializationException;
@@ -10,6 +26,11 @@ import java.util.Properties;
 import java.util.Iterator;
 import java.util.Map;
 
+/**
+ * AbstractConfigurableJdoFactory 
+ *
+ * @version $Id$
+ */
 public abstract class AbstractConfigurableJdoFactory
     extends AbstractLogEnabled
     implements ConfigurableJdoFactory, Initializable
@@ -58,6 +79,11 @@ public abstract class AbstractConfigurableJdoFactory
 
     public void setProperty( String key, String value )
     {
+        if ( otherProperties == null )
+        {
+            otherProperties = new Properties();
+        }
+        
         setPropertyInner( otherProperties, key, value );
     }
 
