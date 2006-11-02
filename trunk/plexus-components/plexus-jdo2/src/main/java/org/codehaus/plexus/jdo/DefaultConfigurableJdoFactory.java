a12515d6034f66fa981e5ccecbbedab98618755f
joakime <joakime@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-11-02 20:49:59 +0000

    * Cleaning up source code license headers. * Ensuring that setProperty() cannot result in NPE.

--- trunk/plexus-components/plexus-jdo2/src/main/java/org/codehaus/plexus/jdo/DefaultConfigurableJdoFactory.java
+++ trunk/plexus-components/plexus-jdo2/src/main/java/org/codehaus/plexus/jdo/DefaultConfigurableJdoFactory.java
@@ -1,33 +1,28 @@
 package org.codehaus.plexus.jdo;
 
 /*
-* Copyright 2005-2006 The Apache Software Foundation.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2001-2006 The Codehaus.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+import org.codehaus.plexus.personality.plexus.lifecycle.phase.Initializable;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Properties;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
-import javax.jdo.JDOHelper;
-import javax.jdo.PersistenceManagerFactory;
-
-import org.codehaus.plexus.personality.plexus.lifecycle.phase.Initializable;
-import org.codehaus.plexus.personality.plexus.lifecycle.phase.InitializationException;
-import org.codehaus.plexus.logging.AbstractLogEnabled;
 
 /**
  * @author <a href="mailto:trygvis@inamo.no">Trygve Laugst&oslash;l</a>
