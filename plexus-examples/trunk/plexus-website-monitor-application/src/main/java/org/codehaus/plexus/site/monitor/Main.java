c8ee72fcbc8f15e544141e67c7b2bb195dd8cb27
rahul <rahul@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-11-14 06:33:12 +0000

    o  furhter down the plexus application tutorial track.

---
+++ plexus-examples/trunk/plexus-website-monitor-application/src/main/java/org/codehaus/plexus/site/monitor/Main.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.codehaus.plexus.site.monitor;
+
+/**
+ * @author <a href='mailto:rahul.thakur.xdev@gmail.com'>Rahul Thakur</a>
+ */
+public class Main
+{
+
+    /**
+     * @param args
+     */
+    public static void main( String[] args )
+    {
+        System.out.println ("Hello Plexus application!");
+
+    }
+
+}
