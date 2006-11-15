c76ae732511b49f390a2df6a43e9b51fa1ef47a0
rahul <rahul@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-11-15 07:06:30 +0000

    o  updates to example plexus app

--- plexus-examples/trunk/plexus-website-monitor-application/src/main/java/org/codehaus/plexus/site/monitor/Main.java
+++ plexus-examples/trunk/plexus-website-monitor-application/src/main/java/org/codehaus/plexus/site/monitor/Main.java
@@ -3,6 +3,12 @@
  */
 package org.codehaus.plexus.site.monitor;
 
+import org.codehaus.plexus.PlexusContainer;
+import org.codehaus.plexus.PlexusContainerException;
+import org.codehaus.plexus.component.repository.exception.ComponentLookupException;
+import org.codehaus.plexus.embed.Embedder;
+import org.codehaus.plexus.tutorial.WebsiteMonitor;
+
 /**
  * @author <a href='mailto:rahul.thakur.xdev@gmail.com'>Rahul Thakur</a>
  */
@@ -14,7 +20,30 @@ public class Main
      */
     public static void main( String[] args )
     {
-        System.out.println ("Hello Plexus application!");
+        Embedder embedder = new Embedder();
+        try
+        {
+            embedder.start();
+            PlexusContainer container = embedder.getContainer();
+            WebsiteMonitor monitor = (WebsiteMonitor) container.lookup( WebsiteMonitor.ROLE );
+            try
+            {
+                monitor.monitor();
+            }
+            catch ( Exception e )
+            {             
+                e.printStackTrace();
+            }
+
+        }
+        catch ( PlexusContainerException e )
+        {
+            e.printStackTrace();
+        }
+        catch ( ComponentLookupException e )
+        {
+            e.printStackTrace();
+        }
 
     }
 
