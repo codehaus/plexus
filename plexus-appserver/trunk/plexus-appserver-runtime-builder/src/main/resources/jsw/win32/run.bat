6659894dc7d55e89087d1ca06ea7da21ba6ca756
evenisse <evenisse@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-11-21 10:37:44 +0000

    Ability to define appserver.base

--- plexus-appserver/trunk/plexus-appserver-runtime-builder/src/main/resources/jsw/win32/run.bat
+++ plexus-appserver/trunk/plexus-appserver-runtime-builder/src/main/resources/jsw/win32/run.bat
@@ -23,6 +23,55 @@ rem
 set _WRAPPER_CONF=wrapper.conf
 SET TOOLS_JAR=%JAVA_HOME%\lib\tools.jar
 
+:chkMHome
+if not "%PLEXUS_HOME%"=="" goto valMHome
+
+if "%OS%"=="Windows_NT" SET PLEXUS_HOME=%~dp0..\..
+if not "%PLEXUS_HOME%"=="" goto valMHome
+
+echo.
+echo ERROR: PLEXUS_HOME not found in your environment.
+echo Please set the PLEXUS_HOME variable in your environment to match the
+echo location of the PLEXUS installation
+echo.
+goto end
+
+:valMHome
+if exist "%PLEXUS_HOME%\bin\plexus.bat" goto chkMBase
+
+echo.
+echo ERROR: PLEXUS_HOME is set to an invalid directory.
+echo PLEXUS_HOME = %PLEXUS_HOME%
+echo Please set the PLEXUS_HOME variable in your environment to match the
+echo location of the PLEXUS installation
+echo.
+goto end
+
+:chkMBase
+if not "%PLEXUS_BASE%"=="" goto valMBase
+
+if "%OS%"=="Windows_NT" SET PLEXUS_BASE=%PLEXUS_HOME%
+if not "%PLEXUS_BASE%"=="" goto valMBase
+
+echo.
+echo ERROR: PLEXUS_BASE not found in your environment.
+echo Please set the PLEXUS_BASE variable in your environment to match the
+echo location of the PLEXUS installation
+echo.
+goto end
+
+:valMBase
+if exist "%PLEXUS_BASE%\bin\plexus.bat" goto startup
+
+echo.
+echo ERROR: PLEXUS_BASE is set to an invalid directory.
+echo PLEXUS_BASE = %PLEXUS_BASE%
+echo Please set the PLEXUS_BASE variable in your environment to match the
+echo location of the PLEXUS installation
+echo.
+goto end
+@REM ==== END VALIDATION ====
+
 rem
 rem Run the application.
 rem At runtime, the current directory will be that of Wrapper.exe
@@ -35,4 +84,6 @@ pause
 :end
 set _APP_HOME=
 set _WRAPPER_CONF=
-
+set PLEXUS_HOME=
+set PLEXUS_BASE=
+set TOOLS_JAR=
