a0f8047399d1eb2842e9d062463aeec3acacf775
handyande <handyande@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-11-26 13:58:39 +0000

    [maven-scm] copy for tag plexus-component-factories-1.0-alpha-9

---
+++ plexus-component-factories/tags/plexus-component-factories-1.0-alpha-9/plexus-jruby-factory/src/main/java/org/codehaus/plexus/component/factory/jruby/JRubyComponentFactory.java
@@ -0,0 +1,23 @@
+package org.codehaus.plexus.component.factory.jruby;
+
+import org.codehaus.plexus.classworlds.realm.ClassRealm;
+import org.codehaus.plexus.PlexusContainer;
+import org.codehaus.plexus.component.factory.AbstractComponentFactory;
+import org.codehaus.plexus.component.factory.ComponentInstantiationException;
+import org.codehaus.plexus.component.jruby.JRubyInvoker;
+import org.codehaus.plexus.component.repository.ComponentDescriptor;
+
+/**
+ * 
+ * @author eredmond
+ */
+public class JRubyComponentFactory
+    extends AbstractComponentFactory
+{
+    public Object newInstance( ComponentDescriptor componentDescriptor, ClassRealm containerRealm,
+                              PlexusContainer container )
+        throws ComponentInstantiationException
+    {
+        return new JRubyInvoker( componentDescriptor, containerRealm );
+    }
+}
---
+++ plexus-component-factories/tags/plexus-component-factories-1.0-alpha-9/plexus-jruby-factory/src/test/java/org/codehaus/plexus/component/factory/jruby/Executor.java
@@ -0,0 +1,6 @@
+package org.codehaus.plexus.component.factory.jruby;
+
+public interface Executor
+{
+    public void execute(); 
+}
