57aceec84ce52b259971bc0e3c06cabbb4955573
jvanzyl <jvanzyl@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-12-03 03:11:20 +0000

    [maven-scm] copy for tag plexus-resources-1.0-alpha-4

---
+++ tags/plexus-resources-1.0-alpha-4/src/main/java/org/codehaus/plexus/resource/loader/FileResourceLoader.java
@@ -0,0 +1,129 @@
+package org.codehaus.plexus.resource.loader;
+
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2004, The Codehaus
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
+import org.codehaus.plexus.resource.loader.AbstractResourceLoader;
+import org.codehaus.plexus.resource.loader.ResourceNotFoundException;
+import org.codehaus.plexus.util.FileUtils;
+
+/**
+ * @author <a href="mailto:trygvis@inamo.no">Trygve Laugst&oslash;l</a>
+ * @author Jason van Zyl
+ * @version $Id$
+ * @plexus.component role-hint="file"
+ */
+public class FileResourceLoader
+    extends AbstractResourceLoader
+{
+    public static final String ID = "file";
+
+    // ----------------------------------------------------------------------
+    // ResourceLoader Implementation
+    // ----------------------------------------------------------------------
+
+    public InputStream getResourceAsInputStream( String name )
+        throws ResourceNotFoundException
+    {
+        for ( Iterator it = paths.iterator(); it.hasNext(); )
+        {
+            String path = (String) it.next();
+
+            File file = new File( path, name );
+
+            if ( file.canRead() )
+            {
+                try
+                {
+                    return new FileInputStream( file );
+                }
+                catch ( FileNotFoundException e )
+                {
+                    // this really shouldn't happen as the file already is
+                    // verified as readable
+
+                    throw new ResourceNotFoundException( name );
+                }
+            }
+        }
+
+        throw new ResourceNotFoundException( name );
+    }
+
+    public static File getResourceAsFile( String name,
+                                          String outputPath,
+                                          File outputDirectory )
+        throws FileResourceCreationException
+
+    {
+        File f = new File( name );
+
+        if ( f.exists() )
+        {
+            if ( outputPath == null )
+            {
+                return f;
+            }
+            else
+            {
+                try
+                {
+                    File outputFile;
+
+                    if ( outputDirectory != null )
+                    {
+                        outputFile = new File( outputDirectory, outputPath );
+                    }
+                    else
+                    {
+                        outputFile = new File( outputPath );
+                    }
+
+                    if ( !outputFile.getParentFile().exists() )
+                    {
+                        outputFile.getParentFile().mkdirs();
+                    }
+
+                    FileUtils.copyFile( f, outputFile );
+
+                    return outputFile;
+                }
+                catch ( IOException e )
+                {
+                    throw new FileResourceCreationException( "Cannot create file-based resource.", e );
+                }
+            }
+        }
+
+        return null;
+    }
+}
---
+++ tags/plexus-resources-1.0-alpha-4/src/main/java/org/codehaus/plexus/resource/loader/JarResourceLoader.java
@@ -0,0 +1,158 @@
+package org.codehaus.plexus.resource.loader;
+
+import org.codehaus.plexus.personality.plexus.lifecycle.phase.InitializationException;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Jason van Zyl
+ * @plexus.component role-hint="jar"
+ */
+public class JarResourceLoader
+    extends AbstractResourceLoader
+{
+    public static final String ID = "jar";
+
+    /**
+     * Maps entries to the parent JAR File
+     * Key = the entry *excluding* plain directories
+     * Value = the JAR URL
+     */
+    private Map entryDirectory = new HashMap( 559 );
+
+    /**
+     * Maps JAR URLs to the actual JAR
+     * Key = the JAR URL
+     * Value = the JAR
+     */
+    private Map jarfiles = new HashMap( 89 );
+
+    public void initialize()
+        throws InitializationException
+    {
+        if ( paths != null )
+        {
+            for ( int i = 0; i < paths.size(); i++ )
+            {
+                loadJar( (String) paths.get( i ) );
+            }
+        }
+    }
+
+    private void loadJar( String path )
+    {
+        if ( getLogger().isDebugEnabled() )
+        {
+            getLogger().debug( "JarResourceLoader : trying to load \"" + path + "\"" );
+        }
+
+        // Check path information
+        if ( path == null )
+        {
+            getLogger().error( "JarResourceLoader : can not load JAR - JAR path is null" );
+        }
+        if ( !path.startsWith( "jar:" ) )
+        {
+            getLogger().error( "JarResourceLoader : JAR path must start with jar: -> " +
+                "see java.net.JarURLConnection for information" );
+        }
+        if ( !path.endsWith( "!/" ) )
+        {
+            path += "!/";
+        }
+
+        // Close the jar if it's already open
+        // this is useful for a reload
+        closeJar( path );
+
+        // Create a new JarHolder
+        JarHolder temp = new JarHolder( path );
+
+        // Add it's entries to the entryCollection
+        addEntries( temp.getEntries() );
+
+        // Add it to the Jar table
+        jarfiles.put( temp.getUrlPath(), temp );
+    }
+
+    /**
+     * Closes a Jar file and set its URLConnection
+     * to null.
+     */
+    private void closeJar( String path )
+    {
+        if ( jarfiles.containsKey( path ) )
+        {
+            JarHolder theJar = (JarHolder) jarfiles.get( path );
+
+            theJar.close();
+        }
+    }
+
+    /**
+     * Copy all the entries into the entryDirectory
+     * It will overwrite any duplicate keys.
+     */
+    private void addEntries( Hashtable entries )
+    {
+        entryDirectory.putAll( entries );
+    }
+
+    /**
+     * Get an InputStream so that the Runtime can build a
+     * template with it.
+     *
+     * @param source name of template to get
+     * @return InputStream containing the template
+     * @throws ResourceNotFoundException if template not found
+     *                                   in the file template path.
+     */
+    public InputStream getResourceAsInputStream( String source)
+        throws ResourceNotFoundException
+    {
+        InputStream results = null;
+
+        if ( source == null || source.length() == 0 )
+        {
+            throw new ResourceNotFoundException( "Need to have a resource!" );
+        }
+
+        if ( source == null || source.length() == 0 )
+        {
+            String msg = "JAR resource error : argument " + source +
+                " contains .. and may be trying to access " + "content outside of template root.  Rejected.";
+
+            getLogger().error( "JarResourceLoader : " + msg );
+
+            throw new ResourceNotFoundException( msg );
+        }
+
+        /*
+        *  if a / leads off, then just nip that :)
+        */
+        if ( source.startsWith( "/" ) )
+        {
+            source = source.substring( 1 );
+        }
+
+        if ( entryDirectory.containsKey( source ) )
+        {
+            String jarurl = (String) entryDirectory.get( source );
+
+            if ( jarfiles.containsKey( jarurl ) )
+            {
+                JarHolder holder = (JarHolder) jarfiles.get( jarurl );
+
+                results = holder.getResource( source );
+                
+                return results;
+            }
+        }
+
+        throw new ResourceNotFoundException( "JarResourceLoader Error: cannot find resource " + source );
+    }
+}
