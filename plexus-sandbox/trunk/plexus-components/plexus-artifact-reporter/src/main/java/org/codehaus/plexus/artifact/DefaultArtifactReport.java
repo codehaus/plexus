9757aeeb949c3a35f105375960b3928e739d7990
jtolentino <jtolentino@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-12-15 16:20:47 +0000

    New plexus component for artifact reporting.

---
+++ plexus-sandbox/trunk/plexus-components/plexus-artifact-reporter/src/main/java/org/codehaus/plexus/artifact/DefaultArtifactReport.java
@@ -0,0 +1,48 @@
+/**
+ *
+ * Copyright 2006
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.codehaus.plexus.artifact;
+
+import org.codehaus.plexus.logging.AbstractLogEnabled;
+import org.codehaus.plexus.personality.plexus.lifecycle.phase.Startable;
+
+/**
+ * @author John Tolentino
+ */
+public class DefaultArtifactReport
+    extends AbstractLogEnabled
+    implements ArtifactReport, Startable
+{
+    // ----------------------------------------------------------------------
+    // Component Lifecycle
+    // ----------------------------------------------------------------------
+
+    public void start()
+    {
+        getLogger().info( "Starting DefaultArtifactReport component." );
+    }
+
+    public void stop()
+    {
+        getLogger().info( "Stopping DefaultArtifactReport component." );
+    }
+
+    // ----------------------------------------------------------------------
+    // ArtifactReport Implementation
+    // ----------------------------------------------------------------------
+
+}
