5cc1be5e82bcf50eabafd78ef3150ae4ad64947a
rahul <rahul@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-12-16 08:58:46 +0000

    o  added Create virtual host command. (not tested)

---
+++ plexus-sandbox/trunk/plexus-xsiter/xsiter-deployer-component/src/main/java/org/codehaus/plexus/xsiter/command/CreateApacheVirtualHostCommand.java
@@ -0,0 +1,133 @@
+/**
+ * 
+ */
+package org.codehaus.plexus.xsiter.command;
+
+import java.io.File;
+import java.io.StringReader;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Properties;
+
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.scm.manager.ScmManager;
+import org.apache.velocity.app.Velocity;
+import org.codehaus.plexus.util.StringUtils;
+import org.codehaus.plexus.xsiter.deployer.model.DeploymentWorkspace;
+import org.codehaus.plexus.xsiter.utils.DeployerUtils;
+import org.codehaus.plexus.xsiter.vhost.VirtualHostConfiguration;
+import org.codehaus.plexus.xsiter.vhost.VirtualHostManager;
+
+/**
+ * Creates an Apache Virtual Host configuration from a template provided for 
+ * the specified {@link DeploymentWorkspace}
+ * @author <a href='mailto:rahul.thakur.xdev@gmail.com'>Rahul Thakur</a>
+ * @plexus.component role="org.codehaus.plexus.xsiter.command.Command" role-hint="create-vhost"
+ */
+public class CreateApacheVirtualHostCommand
+    extends AbstractCommand
+    implements Command
+{
+
+    /**
+     * Source Control Manager.
+     * 
+     * @plexus.requirement
+     */
+    protected ScmManager scmManager;
+
+    /**
+     * Component to Manage Virtual Hosts for a Project.
+     * 
+     * @plexus.requirement
+     */
+    protected VirtualHostManager virtualHostManager;
+
+    public CreateApacheVirtualHostCommand( String label )
+    {
+        super( label );
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see org.codehaus.plexus.xsiter.command.Command#execute(org.codehaus.plexus.xsiter.command.CommandContext)
+     */
+    public CommandResult execute( CommandContext context )
+        throws CommandException
+    {
+        CommandResult result = new CommandResult();
+        try
+        {
+            // Updated Implementation, use configuration from pom.xml
+            DeploymentWorkspace workspace = context.getWorkspace();
+            MavenProject mavenProject = DeployerUtils.getMavenProjectForCheckedoutProject( context
+                .getDeployerWorkingDirectory(), workspace );
+            Properties props = mavenProject.getProperties();
+
+            String vhostsConfig = (String) props.get( VirtualHostManager.ELT_POM_VHOSTS_CONFIG );
+            StringReader sr = new StringReader( vhostsConfig );
+            List list = virtualHostManager.loadVirtualHostConfigurations( sr );
+            boolean appendVhost = false;
+            for ( Iterator it = list.iterator(); it.hasNext(); )
+            {
+                VirtualHostConfiguration config = (VirtualHostConfiguration) it.next();
+                File workspaceWorkingDir = new File( workspace.getRootDirectory(), workspace.getWorkingDirectory() );
+                String scmTag = null != workspace.getScmTag() ? workspace.getScmTag() : "HEAD";
+                File checkoutDir = new File( workspaceWorkingDir, scmTag );
+                // Adjust the Velocity 'file.resource.loader.path' property to the
+                // checkout directory, so the Vhosts template could be found.
+                Velocity.setProperty( Velocity.FILE_RESOURCE_LOADER_PATH, checkoutDir.getAbsolutePath() );
+                // Adjust other directory paths on the VirtualHostConfiguration
+                // instance
+                resetVirtualHostDirectory( config, workspace );
+                virtualHostManager.addVirtualHost( config, appendVhost );
+                // subsequent vhost configs to be appended
+                if ( !appendVhost )
+                    appendVhost = true;
+            }
+            result.setState( ResultState.SUCCESS );
+            return result;
+        }
+        catch ( Exception e )
+        {
+            result.setState( ResultState.ERROR );
+            result.addMessage( "Unable to create Apache Virtual Host for Workspace: '" + context.getWorkspace().getId()
+                + "'" );
+            return result;
+        }
+    }
+
+    /**
+     * Service method to translate and adjust the directory paths for the Virtual Host configuration
+     * to be relative from the Deployment workspaces ROOT dir.
+     * <p>
+     * Also deletes any existing vhost resources.
+     * 
+     * @param config
+     * @param workspace
+     */
+    protected void resetVirtualHostDirectory( VirtualHostConfiguration config, DeploymentWorkspace workspace )
+    {
+        if ( config.getVhostDirectory().startsWith( "/" ) )
+        {
+            config.setVhostDirectory( StringUtils.replace( workspace.getRootDirectory() + config.getVhostDirectory(),
+                                                           "\\", "/" ) );
+        }
+        else
+        {
+            config.setVhostDirectory( StringUtils.replace( workspace.getRootDirectory() + "/"
+                + config.getVhostDirectory(), "\\", "/" ) );
+        }
+        if ( config.getVhostLogDirectory().startsWith( "/" ) )
+        {
+            config.setVhostLogDirectory( StringUtils.replace( workspace.getRootDirectory()
+                + config.getVhostLogDirectory(), "\\", "/" ) );
+        }
+        else
+        {
+            config.setVhostLogDirectory( StringUtils.replace( workspace.getRootDirectory() + "/"
+                + config.getVhostLogDirectory(), "\\", "/" ) );
+        }
+    }
+
+}
