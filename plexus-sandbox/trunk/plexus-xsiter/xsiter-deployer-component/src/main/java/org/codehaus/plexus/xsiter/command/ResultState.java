a358c531e17afc17bb626613e8bcf9566add514f
rahul <rahul@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-12-16 04:07:49 +0000

    o  Added CommandResult and ResultState.

---
+++ plexus-sandbox/trunk/plexus-xsiter/xsiter-deployer-component/src/main/java/org/codehaus/plexus/xsiter/command/ResultState.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package org.codehaus.plexus.xsiter.command;
+
+/**
+ * A {@link CommandResult}'s possible states are setup here. 
+ * @author <a href='mailto:rahul.thakur.xdev@gmail.com'>Rahul Thakur</a>
+ *
+ */
+public enum ResultState {
+
+    SUCCESS("Success"), FAILURE("Failure"), ERROR("Error");
+
+    /**
+     * State label, for display.
+     */
+    private String label;
+
+    ResultState( String s )
+    {
+        this.label = s;
+    }
+
+    /**
+     * Returns the String representation for the state.
+     * @return state label.
+     */
+    public String toString()
+    {
+        return this.label;
+    }
+}
