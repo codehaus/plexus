647b95d4039430f936c705059a0538e8e836c9c1
trygvis <trygvis@30dfa635-7bf8-0310-9608-e41f2d776689>
2007-01-11 09:56:06 +0000

    Fixing PLX-317: "provide Environment support in plexus-naming" Patch by Olivier Lamy.

--- plexus-sandbox/trunk/plexus-components/plexus-naming/src/test/java/org/codehaus/plexus/naming/NamingTest.java
+++ plexus-sandbox/trunk/plexus-components/plexus-naming/src/test/java/org/codehaus/plexus/naming/NamingTest.java
@@ -23,4 +23,23 @@ public class NamingTest
         Connection conn = dataSource.getConnection();
         conn.close();
     }
+
+    public void testEnvironment()
+        throws Exception
+    {
+        Naming naming = (Naming) lookup( Naming.ROLE, "environments" );
+
+        assertNotNull( naming );
+
+        Context ctx = naming.createInitialContext();
+        ctx = (Context) ctx.lookup( "java:comp/env" );
+
+        Integer ten = (Integer) ctx.lookup( "ten" );
+        assertNotNull( "ten is null", ten );
+        assertEquals( "ten not 10", 10, ten.intValue() );
+
+        String wine = (String) ctx.lookup( "wine" );
+        assertNotNull( wine );
+        assertEquals( "wine not bordeaux", "bordeaux", wine );
+    }
 }
