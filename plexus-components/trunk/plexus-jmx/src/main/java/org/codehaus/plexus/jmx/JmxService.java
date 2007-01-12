2fc6a71b7caf843c4de183d9248a687f2fda6b98
trygvis <trygvis@30dfa635-7bf8-0310-9608-e41f2d776689>
2007-01-12 15:22:38 +0000

    o Adding internal structures to help writing management services.

--- plexus-components/trunk/plexus-jmx/src/main/java/org/codehaus/plexus/jmx/JmxService.java
+++ plexus-components/trunk/plexus-jmx/src/main/java/org/codehaus/plexus/jmx/JmxService.java
@@ -1,13 +1,55 @@
 package org.codehaus.plexus.jmx;
 
+import javax.management.InstanceAlreadyExistsException;
+import javax.management.MBeanRegistrationException;
 import javax.management.MBeanServer;
+import javax.management.MalformedObjectNameException;
+import javax.management.NotCompliantMBeanException;
+import javax.management.ObjectName;
 
 /**
  * @author <a href="mailto:trygvis@inamo.no">Trygve Laugst&oslash;l</a>
  * @version $Id$
  */
-public interface JmxService {
+public interface JmxService
+{
     String ROLE = JmxService.class.getName();
 
+    /**
+     * Returns the default {@link MBeanServer}.
+     *
+     * @return
+     */
     MBeanServer getMBeanServer();
+
+    /**
+     * Registers the object in the MBeanServer returned by {@link #getMBeanServer()} by using
+     * {@link MBeanServer#registerMBean(Object, javax.management.ObjectName)}.
+     *
+     * It will also record all the MBeans in an internal list. By calling {@link #releaseAllMBeansQuiet()} all the
+     * MBeans in the list will be released. This is a useful feature for management components as they can register
+     * any number of beans runtime and release all registered beans with one call in their <code>stop()</code> method.
+     *
+     * <it>NOTE:</it> instances registered in the MBeanServer returned by {@link #getMBeanServer()} will not be
+     * registered.
+     *
+     * @param object
+     * @param objectName
+     * @throws MBeanRegistrationException
+     * @throws InstanceAlreadyExistsException
+     * @throws NotCompliantMBeanException
+     * @throws MalformedObjectNameException
+     */
+    ObjectName registerMBean( Object object, String objectName )
+        throws MBeanRegistrationException, InstanceAlreadyExistsException, NotCompliantMBeanException,
+        MalformedObjectNameException;
+
+    /**
+     * Releases all MBeans registered through {@link #registerMBean(Object, String)}.
+     *
+     * Note that it will swallow all exceptions and only log them to the logger.
+     */
+    public void releaseAllMBeansQuiet();
+
+    // TODO: A method to release a specific MBean
 }
--- plexus-components/trunk/plexus-jmx/src/test/java/org/codehaus/plexus/jmx/JmxServiceTest.java
+++ plexus-components/trunk/plexus-jmx/src/test/java/org/codehaus/plexus/jmx/JmxServiceTest.java
@@ -4,12 +4,13 @@ import org.codehaus.plexus.PlexusTestCase;
 import org.codehaus.plexus.jmx.mgmt.PartyMgmt;
 import org.codehaus.plexus.jmx.mgmt.PartyMgmtMBean;
 
-import javax.management.ObjectName;
-import javax.management.MBeanServerFactory;
-import javax.management.MBeanServer;
-import javax.management.MBeanInfo;
-import javax.management.MBeanAttributeInfo;
 import javax.management.Attribute;
+import javax.management.MBeanServer;
+import javax.management.MBeanServerFactory;
+import javax.management.ObjectName;
+import javax.management.InstanceNotFoundException;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectInstance;
 
 /**
  * @author <a href="mailto:trygve.laugstol@objectware.no">Trygve Laugst&oslash;l</a>
@@ -50,5 +51,40 @@ public class JmxServiceTest
         attribute = mBeanServer.getAttribute( objectName, "Party" );
 
         assertEquals( true, ( (Boolean) attribute ).booleanValue() );
+    }
+
+    public void testInternalMBeanList()
+        throws Exception
+    {
+        JmxService jmxService = (JmxService) lookup( JmxService.ROLE );
+
+        // -----------------------------------------------------------------------
+        // Register an mbean and remove it again.
+        // -----------------------------------------------------------------------
+
+        ObjectName objectName = jmxService.registerMBean( new PartyMgmt(), "org.codehaus.plexus:Type=Party" );
+
+        jmxService.releaseAllMBeansQuiet();
+
+        // -----------------------------------------------------------------------
+        // Find the bean again
+        // -----------------------------------------------------------------------
+
+        MBeanServer mBeanServer = jmxService.getMBeanServer();
+
+        try
+        {
+            mBeanServer.getObjectInstance( objectName );
+
+            fail( "Expected InstanceNotFoundException" );
+        }
+        catch ( InstanceNotFoundException e )
+        {
+            // expected
+        }
+        catch ( NullPointerException e )
+        {
+            fail( "Expected InstanceNotFoundException" );
+        }
     }
 }
