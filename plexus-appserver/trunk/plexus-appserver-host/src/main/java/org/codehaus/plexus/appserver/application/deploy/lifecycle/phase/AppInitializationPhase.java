e8015dcce961e72f11b4a4f9cec1afa446eae306
handyande <handyande@30dfa635-7bf8-0310-9608-e41f2d776689>
2007-01-31 11:52:57 +0000

    revert r5531 which removed application containers. Lookups in applications and webapps are now working as expected again

--- plexus-appserver/trunk/plexus-appserver-host/src/main/java/org/codehaus/plexus/appserver/application/deploy/lifecycle/phase/AppInitializationPhase.java
+++ plexus-appserver/trunk/plexus-appserver-host/src/main/java/org/codehaus/plexus/appserver/application/deploy/lifecycle/phase/AppInitializationPhase.java
@@ -2,15 +2,9 @@ package org.codehaus.plexus.appserver.application.deploy.lifecycle.phase;
 
 import org.codehaus.plexus.appserver.application.deploy.lifecycle.AppDeploymentContext;
 import org.codehaus.plexus.appserver.application.deploy.lifecycle.AppDeploymentException;
-import org.codehaus.plexus.appserver.service.PlexusServiceException;
+import org.codehaus.plexus.DefaultPlexusContainer;
+import org.codehaus.plexus.PlexusContainerException;
 import org.codehaus.plexus.classworlds.realm.ClassRealm;
-import org.codehaus.plexus.util.FileUtils;
-
-import java.util.List;
-import java.util.Iterator;
-import java.io.File;
-import java.io.IOException;
-import java.net.MalformedURLException;
 
 /**
  * @author Jason van Zyl
@@ -21,27 +15,32 @@ public class AppInitializationPhase
     public void execute( AppDeploymentContext context )
         throws AppDeploymentException
     {
+        String name = "plexus.application." + context.getApplicationId();
+
+        DefaultPlexusContainer serverContainer = context.getAppServerContainer();
+
+        // ----------------------------------------------------------------------------
+        // Create the container and start
+        // ----------------------------------------------------------------------------
+
+        DefaultPlexusContainer applicationContainer;
+
         try
         {
-            ClassRealm realm = context.getAppRuntimeProfile().getApplicationRealm();
+            // This call will initialise and start the container
 
-            List jars = FileUtils.getFiles( context.getAppLibDirectory(), "*.jar", null );
-
-            for ( Iterator i = jars.iterator(); i.hasNext(); )
-            {
-                File file = (File) i.next();
-
-                realm.addURL( file.toURL() );
-            }
+            applicationContainer = new DefaultPlexusContainer( name, context.getContextValues(),
+                                                               context.getAppConfigurationFile().getAbsoluteFile(),
+                                                               context.getAppRuntimeProfile().getApplicationWorld()
+                                                               /*, serverContainer*/ );
         }
-        catch ( MalformedURLException e )
+        catch ( PlexusContainerException e )
         {
-            throw new AppDeploymentException( e.getMessage(), e );
-        }
-        catch ( IOException e )
-        {
-            throw new AppDeploymentException( e.getMessage(), e );
+            throw new AppDeploymentException( "Error starting container.", e );
         }
 
+        context.setApplicationContainer( applicationContainer );
+
+        context.getAppRuntimeProfile().setApplicationContainer( applicationContainer );
     }
 }
