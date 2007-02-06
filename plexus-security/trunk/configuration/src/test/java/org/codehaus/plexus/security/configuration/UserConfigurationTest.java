f2e1984f2d8e19abb67300c8ef3fedd1beea4785
jesse <jesse@30dfa635-7bf8-0310-9608-e41f2d776689>
2007-02-06 15:45:25 +0000

    switch the tls config property in the test to something more bogus since I think this test broke when brett added tls support and the property actually started existing

--- plexus-security/trunk/configuration/src/test/java/org/codehaus/plexus/security/configuration/UserConfigurationTest.java
+++ plexus-security/trunk/configuration/src/test/java/org/codehaus/plexus/security/configuration/UserConfigurationTest.java
@@ -62,7 +62,7 @@ public class UserConfigurationTest
     {
         UserConfiguration config = (UserConfiguration) lookup( UserConfiguration.ROLE );
         assertFalse( config.getBoolean( "email.smtp.ssl.enabled" ) );
-        assertTrue( config.getBoolean( "email.smtp.tls.enabled", true ) );
+        assertTrue( config.getBoolean( "email.smtp.tls.enabled.fake", true ) );
     }
 
     public void testGetInt()
