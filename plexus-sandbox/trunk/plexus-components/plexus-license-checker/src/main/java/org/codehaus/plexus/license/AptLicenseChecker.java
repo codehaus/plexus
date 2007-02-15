93e670b616e673d1dfe9f0874c89f2d2f1883ada
jtolentino <jtolentino@30dfa635-7bf8-0310-9608-e41f2d776689>
2007-02-15 21:51:27 +0000

    new plexus component for release license reporting

---
+++ plexus-sandbox/trunk/plexus-components/plexus-license-checker/src/main/java/org/codehaus/plexus/license/AptLicenseChecker.java
@@ -0,0 +1,43 @@
+/**
+ *
+ * Copyright 2007
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.codehaus.plexus.license;
+
+import java.io.IOException;
+import java.io.File;
+
+/**
+ * @author John Tolentino
+ */
+public class AptLicenseChecker
+    extends AbstractLicenseChecker
+{
+    public AptLicenseChecker( File startLicenseFile, File endLicenseFile )
+    {
+        super( startLicenseFile, endLicenseFile );
+    }
+
+    public String removeCommentCharacters( File sourceFile )
+        throws IOException
+    {
+        String processed = Utils.fileToString( sourceFile );
+
+        processed = processed.replaceAll( "~~", "" );
+        processed = processed.replaceAll( "\\s", "" );
+        return processed;
+    }
+}
---
+++ plexus-sandbox/trunk/plexus-components/plexus-license-checker/src/test/resources/NoLicense.java
@@ -0,0 +1,7 @@
+public class NoLicense
+{
+    public static void main(String args[])
+    {
+       System.out.println("just print something...");
+    }
+}
