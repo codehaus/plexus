fccceb58fcafb122be13e37e0cc0127d61346d6d
brett <brett@30dfa635-7bf8-0310-9608-e41f2d776689>
2007-02-15 14:32:50 +0000

    updated license headers

--- plexus-sandbox/trunk/plexus-components/plexus-registry/plexus-registry-providers/plexus-registry-naming/src/main/java/org/codehaus/plexus/registry/naming/EmptyNamingRegistry.java
+++ plexus-sandbox/trunk/plexus-components/plexus-registry/plexus-registry-providers/plexus-registry-naming/src/main/java/org/codehaus/plexus/registry/naming/EmptyNamingRegistry.java
@@ -1,35 +1,33 @@
 package org.codehaus.plexus.registry.naming;
 
 /*
- * Licensed to The Codehaus ( www.codehaus.org ) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The Codehaus licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Copyright 2007 The Codehaus Foundation.
  *
- *  http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
+import org.codehaus.plexus.registry.Registry;
+import org.codehaus.plexus.registry.RegistryException;
+
 import java.util.Collections;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Properties;
 
-import org.codehaus.plexus.registry.Registry;
-import org.codehaus.plexus.registry.RegistryException;
-
 /**
  * @author <a href="mailto:Olivier.LAMY@accor.com">olamy</a>
- * @since 9 feb. 07
  * @version $Id$
+ * @since 9 feb. 07
  */
 public class EmptyNamingRegistry
     extends NamingRegistry
@@ -45,7 +43,7 @@ public class EmptyNamingRegistry
         return defaultValue;
     }
 
-    /** 
+    /**
      * @see org.codehaus.plexus.registry.naming.NamingRegistry#getBoolean(java.lang.String)
      */
     public boolean getBoolean( String key )
@@ -83,7 +81,7 @@ public class EmptyNamingRegistry
         return defaultValue;
     }
 
-    /** 
+    /**
      * @see org.codehaus.plexus.registry.naming.NamingRegistry#getString(java.lang.String)
      */
     public String getString( String key )
@@ -91,7 +89,7 @@ public class EmptyNamingRegistry
         return null;
     }
 
-    /** 
+    /**
      * @see org.codehaus.plexus.registry.naming.NamingRegistry#getSubset(java.lang.String)
      */
     public Registry getSubset( String key )
@@ -99,7 +97,7 @@ public class EmptyNamingRegistry
         return new EmptyNamingRegistry();
     }
 
-    /** 
+    /**
      * @see org.codehaus.plexus.registry.naming.NamingRegistry#getSubsetList(java.lang.String)
      */
     public List getSubsetList( String key )
