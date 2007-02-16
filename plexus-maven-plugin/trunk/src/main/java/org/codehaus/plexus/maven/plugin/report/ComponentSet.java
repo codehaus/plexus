eb5baec08b533953841212a32039678a11585e57
olamy <olamy@30dfa635-7bf8-0310-9608-e41f2d776689>
2007-02-16 21:50:42 +0000

    put default value of javadoc plugin apidocs in javadoc link

--- plexus-maven-plugin/trunk/src/main/java/org/codehaus/plexus/maven/plugin/report/ComponentSet.java
+++ plexus-maven-plugin/trunk/src/main/java/org/codehaus/plexus/maven/plugin/report/ComponentSet.java
@@ -1,35 +1,24 @@
-/*
- * The MIT License
- *
- * Copyright (c) 2006, The Codehaus
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
- * of the Software, and to permit persons to whom the Software is furnished to do
- * so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 package org.codehaus.plexus.maven.plugin.report;
 
-import org.jdom.Element;
-import org.codehaus.doxia.sink.Sink;
-
-import java.util.Iterator;
+/*
+ * Copyright 2007 The Codehaus Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import java.util.List;
-import java.util.ArrayList;
+
+import org.codehaus.doxia.sink.Sink;
+import org.jdom.Element;
 
 /**
  * @author <a href="mailto:trygvis@inamo.no">Trygve Laugst&oslash;l</a>
@@ -51,8 +40,8 @@ public class ComponentSet
         components = new Components( (Element) list.get( 0 ) );
     }
 
-    public void print( Sink sink )
+    public void print( Sink sink, String javaDocDestDir, String jxrDestDir )
     {
-        components.print( sink );
+        components.print( sink, javaDocDestDir, jxrDestDir );
     }
 }
