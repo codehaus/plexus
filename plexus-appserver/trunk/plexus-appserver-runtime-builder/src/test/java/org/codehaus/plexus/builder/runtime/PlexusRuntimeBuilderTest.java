86c215403f5a07ba7172c1fd0ee5b29b74a3dcbb
olamy <olamy@30dfa635-7bf8-0310-9608-e41f2d776689>
2007-03-07 19:14:49 +0000

    move test from mojo to runtime-builder remove old container version from maven-plugin pom

--- plexus-appserver/trunk/plexus-appserver-runtime-builder/src/test/java/org/codehaus/plexus/builder/runtime/PlexusRuntimeBuilderTest.java
+++ plexus-appserver/trunk/plexus-appserver-runtime-builder/src/test/java/org/codehaus/plexus/builder/runtime/PlexusRuntimeBuilderTest.java
@@ -24,76 +24,25 @@ package org.codehaus.plexus.builder.runtime;
  * SOFTWARE.
  */
 
-import org.apache.maven.artifact.Artifact;
-import org.apache.maven.artifact.factory.ArtifactFactory;
-import org.apache.maven.artifact.repository.ArtifactRepository;
-import org.apache.maven.artifact.repository.ArtifactRepositoryFactory;
-import org.apache.maven.artifact.repository.layout.ArtifactRepositoryLayout;
-import org.codehaus.plexus.PlexusTestCase;
-import org.codehaus.plexus.util.FileUtils;
-import org.codehaus.plexus.util.PropertyUtils;
-
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Properties;
-import java.util.Set;
+
+import org.codehaus.plexus.builder.AbstractBuilderTest;
+import org.codehaus.plexus.util.PropertyUtils;
 
 /**
  * @author <a href="mailto:trygvis@inamo.no">Trygve Laugst&oslash;l</a>
  * @version $Id$
  */
 public class PlexusRuntimeBuilderTest
-    extends PlexusTestCase
+    extends AbstractBuilderTest
 {
-    private ArtifactFactory artifactFactory;
-
-    protected void setUp()
-        throws Exception
-    {
-        super.setUp();
-
-        artifactFactory = (ArtifactFactory) lookup( ArtifactFactory.ROLE );
-    }
 
     public void testRuntimeBuilder()
         throws Exception
     {
         PlexusRuntimeBuilder runtimeBuilder = (PlexusRuntimeBuilder) lookup( PlexusRuntimeBuilder.ROLE );
-
-        ArtifactRepositoryFactory artifactRepositoryFactory =
-            (ArtifactRepositoryFactory) lookup( ArtifactRepositoryFactory.ROLE );
-
-        // ----------------------------------------------------------------------
-        // Clean the output directory
-        // ----------------------------------------------------------------------
-
-        File workingDirectory = getTestFile( "target/test-runtime" );
-
-        FileUtils.deleteDirectory( workingDirectory );
-
-        // ----------------------------------------------------------------------
-        //
-        // ----------------------------------------------------------------------
-
-        List remoteRepositories = new ArrayList();
-
-        ArtifactRepositoryLayout repositoryLayout =
-            (ArtifactRepositoryLayout) lookup( ArtifactRepositoryLayout.ROLE, "legacy" );
-
-        ArtifactRepository localRepository = artifactRepositoryFactory.createArtifactRepository( "local", "file://" +
-            getTestFile( "src/test/repository" ).getAbsolutePath(), repositoryLayout, null, null );
-
-        Set projectArtifacts = new HashSet();
-
-        projectArtifacts.add( makeArtifact( "org.codehaus.plexus", "plexus-classworlds", "1.2-alpha-5" ) );
-        projectArtifacts.add( makeArtifact( "org.codehaus.plexus", "plexus-component-api", "1.0-alpha-15-SNAPSHOT" ) );
-        projectArtifacts.add( makeArtifact( "org.codehaus.plexus", "plexus-container-default", "1.0-alpha-15-SNAPSHOT" ) );
-        projectArtifacts.add( makeArtifact( "plexus", "plexus-container-artifact", "1.0-alpha-2" ) );
-        projectArtifacts.add( makeArtifact( "org.codehaus.plexus", "plexus-appserver-host", "2.0-alpha-6-SNAPSHOT" ) );
-        projectArtifacts.add( makeArtifact( "org.codehaus.plexus", "plexus-utils", "1.4-alpha-1" ) );
 
         // ----------------------------------------------------------------------
         //
@@ -105,21 +54,81 @@ public class PlexusRuntimeBuilderTest
 
         Properties configurationProperties = PropertyUtils.loadProperties( configurationPropertiesFile );
 
-        runtimeBuilder.build( workingDirectory, remoteRepositories, localRepository, projectArtifacts,
-                              Collections.EMPTY_SET, plexusConfiguration, configurationProperties, false );
+        runtimeBuilder.build( getWorkingDirectory(), new ArrayList(), getLocalArtifactRepository(),
+                              getProjectArtifacts(), getAdditionalCoreArtifacts(), plexusConfiguration,
+                              configurationProperties, false );
+
+        // assert on script in bin
+        File shLauncher = new File( getWorkingDirectory(), "bin/plexus.sh" );
+        assertTrue( "plexus.sh not in bin", shLauncher.exists() );
+
+        File batLauncher = new File( getWorkingDirectory(), "/bin/plexus.bat" );
+        assertTrue( "plexus.bat not in bin", batLauncher.exists() );
+
+        // assert on conf directory
+        File classworldConf = new File( getWorkingDirectory(), "conf/classworlds.conf" );
+        assertTrue( "classworlds.conf not in conf directory", classworldConf.exists() );
+
+        File plexusConf = new File( getWorkingDirectory(), "conf/plexus.xml" );
+        assertTrue( "plexus.xml not in conf directory", plexusConf.exists() );
+
+        // assert classworlds in core/boot
+        File classWorldsJar = new File( getWorkingDirectory(), "core/boot/plexus-classworlds-1.2-alpha-7.jar" );
+        assertTrue( "classworlds not in core/boot", classWorldsJar.exists() );
+
+        // assert on core artifacts 
+        // container(s), appserver-host and plexus-utils
+        File componentApiJar = new File( getWorkingDirectory(), "core/plexus-component-api-1.0-alpha-18.jar" );
+        assertTrue( "plexus-component-api not in core dir", componentApiJar.exists() );
+
+        File containerJar = new File( getWorkingDirectory(), "core/plexus-container-default-1.0-alpha-18.jar" );
+        assertTrue( "plexus-container-default not in core dir", containerJar.exists() );
+
+        File appHost = new File( getWorkingDirectory(), "core/plexus-appserver-host-2.0-alpha-8-SNAPSHOT.jar" );
+        assertTrue( "plexus-appserver-host not in core dir", appHost.exists() );
+
+        File plexusUtils = new File( getWorkingDirectory(), "core/plexus-utils-1.4.jar" );
+        assertTrue( "plexus-utils not in core dir", plexusUtils.exists() );
+
+        // assert on additionalCoreArtifacts
+        File commonsLogging = new File( getWorkingDirectory(), "core/commons-logging-1.0.4.jar" );
+        assertTrue( "commons-logging not in core dir", commonsLogging.exists() );
+
+        File log4j = new File( getWorkingDirectory(), "core/log4j-1.2.8.jar" );
+        assertTrue( "log4j not in core dir", log4j.exists() );
+
+        File mail = new File( getWorkingDirectory(), "core/mail-1.4.jar" );
+        assertTrue( "mail not in core dir", mail.exists() );
+
+        //assert on logs directory
+        File logsDir = new File( getWorkingDirectory(), "logs" );
+        assertTrue( "no logs directory", logsDir.exists() );
+
+        //assert on services directory
+        File servicesDir = new File( getWorkingDirectory(), "services" );
+        assertTrue( "no services directory", servicesDir.exists() );
+
+        //assert on temp directory
+        File tempDir = new File( getWorkingDirectory(), "temp" );
+        assertTrue( "no temp directory", tempDir.exists() );
+
+        // assert on add plexus-application
+        runtimeBuilder.addPlexusApplication( getPlexusApplication(), getWorkingDirectory() );
+        File appsDirectory = new File( getWorkingDirectory(), "apps" );
+        assertTrue( "no apps directory", appsDirectory.exists() );
+        assertEquals( "not only one file in apps dir", 1, appsDirectory.list().length );
+        File appArtifact = new File( appsDirectory, "bar-1.0.jar" );
+        assertTrue( "bar-1.0.jar not in apps directory", appArtifact.exists() );
+
+        // assert on add plexus services
+        runtimeBuilder.addPlexusService( getPlexusService(), getWorkingDirectory() );
+        File servicesDirectory = new File( getWorkingDirectory(), "services" );
+        assertTrue( "no services directory", servicesDirectory.exists() );
+        assertEquals( "not only one file in services dir", 1, servicesDirectory.list().length );
+        File serviceArtifact = new File( servicesDirectory, "plexus-appserver-service-jetty-2.0-alpha-8-SNAPSHOT.sar" );
+        assertTrue( "plexus-appserver-service-jetty-2.0-alpha-8-SNAPSHOT.sar not in services directory",
+                    serviceArtifact.exists() );
+
     }
 
-    // ----------------------------------------------------------------------
-    //
-    // ----------------------------------------------------------------------
-
-    private Artifact makeArtifact( String groupId, String artifactId, String version )
-    {
-        Artifact artifact = artifactFactory.createBuildArtifact( groupId, artifactId, version, "jar" );
-
-        artifact.setFile(
-            getTestFile( "src/test/repository/" + groupId + "/jars/" + artifactId + "-" + version + ".jar" ) );
-
-        return artifact;
-    }
 }
