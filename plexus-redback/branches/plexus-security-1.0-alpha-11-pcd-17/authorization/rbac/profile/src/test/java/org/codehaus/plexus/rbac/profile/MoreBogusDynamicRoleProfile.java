fa5620d1626a6a22d70465ccef1e9550bca7f26c
jesse <jesse@30dfa635-7bf8-0310-9608-e41f2d776689>
2007-03-14 15:44:09 +0000

    branch working off of the -17 container

---
+++ plexus-redback/branches/plexus-security-1.0-alpha-11-pcd-17/authorization/rbac/profile/src/test/java/org/codehaus/plexus/rbac/profile/MoreBogusDynamicRoleProfile.java
@@ -0,0 +1,63 @@
+package org.codehaus.plexus.rbac.profile;
+
+import java.util.List;
+import java.util.Collections;
+/*
+ * Copyright 2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * RoleProfileTest:
+ *
+ * @author: Jesse McConnell <jesse@codehaus.org>
+ * @version: $ID:$
+ *
+ * @plexus.component
+ *   role="org.codehaus.plexus.rbac.profile.DynamicRoleProfile"
+ *   role-hint="more-bogus"
+ */
+public class MoreBogusDynamicRoleProfile
+    extends AbstractDynamicRoleProfile
+{
+    public String getRoleName( String resource )
+    {
+        return "MORE BOGUS ROLE" + RoleProfileConstants.DELIMITER + resource;
+    }
+
+    public List getOperations()
+    {
+        return Collections.singletonList( "BOGUS-OPERATION" );
+    }
+
+
+    // add a child for the statis role bogus
+    public List getChildRoles()
+    {
+        return Collections.singletonList( "bogus" );
+    }
+
+
+    // add a child for the dynamic role bogus
+    public List getDynamicChildRoles( String resource )
+    {
+        return Collections.singletonList( "BOGUS ROLE" + RoleProfileConstants.DELIMITER + resource );
+    }
+
+
+    public boolean isAssignable()
+    {
+        return true;
+    }
+}
