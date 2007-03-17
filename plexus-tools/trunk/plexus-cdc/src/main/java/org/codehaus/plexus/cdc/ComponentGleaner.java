a9e1598dd685cd255b68a4976d4b60ef2a31ed20
handyande <handyande@30dfa635-7bf8-0310-9608-e41f2d776689>
2007-03-17 21:07:16 +0000

    Add a copy of the component requirement etc code from before the role-hint changes. This means we can generate component.xml without the default role hints irrespective of container version.

--- plexus-tools/trunk/plexus-cdc/src/main/java/org/codehaus/plexus/cdc/ComponentGleaner.java
+++ plexus-tools/trunk/plexus-cdc/src/main/java/org/codehaus/plexus/cdc/ComponentGleaner.java
@@ -26,7 +26,7 @@ package org.codehaus.plexus.cdc;
 
 import com.thoughtworks.qdox.model.JavaClass;
 import com.thoughtworks.qdox.model.JavaClassCache;
-import org.codehaus.plexus.component.repository.ComponentDescriptor;
+import org.codehaus.plexus.component.repository.legacy.ComponentDescriptor;
 
 /**
  * @author <a href="mailto:jason@maven.org">Jason van Zyl</a>
