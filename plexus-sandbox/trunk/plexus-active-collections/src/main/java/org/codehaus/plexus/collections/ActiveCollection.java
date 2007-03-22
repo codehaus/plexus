9a6a748fbaa980421cd0269075d80a38bc5f2fe5
jdcasey <jdcasey@30dfa635-7bf8-0310-9608-e41f2d776689>
2007-03-22 19:47:28 +0000

    Adding javadocs

--- plexus-sandbox/trunk/plexus-active-collections/src/main/java/org/codehaus/plexus/collections/ActiveCollection.java
+++ plexus-sandbox/trunk/plexus-active-collections/src/main/java/org/codehaus/plexus/collections/ActiveCollection.java
@@ -1,19 +1,57 @@
 package org.codehaus.plexus.collections;
 
+import org.codehaus.plexus.PlexusContainer;
 import org.codehaus.plexus.component.repository.exception.ComponentLookupException;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Represents a collection-style view of a {@link PlexusContainer} instance, which retrieves a fresh
+ * batch of component elements for each method call. This interface DOES NOT implement {@link Collection},
+ * since it is used as a common parent for active {@link List}, {@link Set}, and {@link Map} 
+ * implementations.
+ * <br/>
+ * <b>NOTE: All active collections are immutable, and will throw an 
+ * {@link UnsupportedOperationException} if a mutator method is called.</b>
+ * 
+ * @author jdcasey
+ *
+ */
 public interface ActiveCollection
 {
     
+    /**
+     * Retrive the role, or type of component, which is collected in the current instance. The current
+     * collection will only "contain" elements that specify this role in their component definitions.
+     */
     String getRole();
 
+    /**
+     * Same semantics as {@link Collection#isEmpty()} or {@link Map#isEmpty()}.
+     */
     boolean isEmpty();
 
+    /**
+     * Same semantics as {@link Collection#isEmpty()} or {@link Map#isEmpty()}, except this method
+     * will throw a {@link ComponentLookupException} if one or more of the elements collected here
+     * fails during lookup.
+     */
     boolean checkedIsEmpty()
         throws ComponentLookupException;
 
+    /**
+     * Same semantics as {@link Collection#size()} or {@link Map#size()}.
+     */
     int size();
     
+    /**
+     * Same semantics as {@link Collection#size()} or {@link Map#size()}, except this method
+     * will throw a {@link ComponentLookupException} if one or more of the elements collected here
+     * fails during lookup.
+     */
     int checkedSize()
         throws ComponentLookupException;
 
--- plexus-sandbox/trunk/plexus-active-collections/src/main/java/org/codehaus/plexus/collections/ActiveList.java
+++ plexus-sandbox/trunk/plexus-active-collections/src/main/java/org/codehaus/plexus/collections/ActiveList.java
@@ -1,5 +1,6 @@
 package org.codehaus.plexus.collections;
 
+import org.codehaus.plexus.PlexusContainer;
 import org.codehaus.plexus.component.repository.exception.ComponentLookupException;
 
 import java.util.Collection;
@@ -7,65 +8,119 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 
+/**
+ * Collection with {@link List} semantics, which is backed by a {@link PlexusContainer} instance, 
+ * and retrieves a fresh batch of elements for each method call.
+ * <br/>
+ * <b>NOTE: All active collections are immutable, and will throw an 
+ * {@link UnsupportedOperationException} if a mutator method is called.</b>
+ *  
+ * @author jdcasey
+ *
+ */
 public interface ActiveList
     extends ActiveCollection, List
 {
 
     String ROLE = ActiveList.class.getName();
 
+    /**
+     * Same as {@link List#contains(Object)}, except this method will throw a 
+     * {@link ComponentLookupException} if one or more elements in the set fail during lookup.
+     * 
+     * @throws ComponentLookupException if one or more elements of the set fail during lookup.
+     */
     boolean checkedContains( Object value )
         throws ComponentLookupException;
 
+    /**
+     * Same as {@link List#containsAll(Collection)}, except this method will throw a 
+     * {@link ComponentLookupException} if one or more elements in the set fail during lookup.
+     * 
+     * @throws ComponentLookupException if one or more elements of the set fail during lookup.
+     */
     boolean checkedContainsAll( Collection collection )
         throws ComponentLookupException;
 
+    /**
+     * Same as {@link List#get(int)}, except this method will throw a 
+     * {@link ComponentLookupException} if one or more elements in the set fail during lookup.
+     * 
+     * @throws ComponentLookupException if one or more elements of the set fail during lookup.
+     */
     Object checkedGet( int index )
         throws ComponentLookupException;
 
+    /**
+     * Same as {@link List#indexOf(Object)}, except this method will throw a 
+     * {@link ComponentLookupException} if one or more elements in the set fail during lookup.
+     * 
+     * @throws ComponentLookupException if one or more elements of the set fail during lookup.
+     */
     int checkedIndexOf( Object value )
         throws ComponentLookupException;
 
+    /**
+     * Same as {@link List#iterator()}, except this method will throw a 
+     * {@link ComponentLookupException} if one or more elements in the set fail during lookup.
+     * 
+     * @throws ComponentLookupException if one or more elements of the set fail during lookup.
+     */
     Iterator checkedIterator()
         throws ComponentLookupException;
 
+    /**
+     * Same as {@link List#lastIndexOf(Object)}, except this method will throw a 
+     * {@link ComponentLookupException} if one or more elements in the set fail during lookup.
+     * 
+     * @throws ComponentLookupException if one or more elements of the set fail during lookup.
+     */
     int checkedLastIndexOf( Object value )
         throws ComponentLookupException;
 
+    /**
+     * Same as {@link List#listIterator()}, except this method will throw a 
+     * {@link ComponentLookupException} if one or more elements in the set fail during lookup.
+     * 
+     * @throws ComponentLookupException if one or more elements of the set fail during lookup.
+     */
     ListIterator checkedListIterator()
         throws ComponentLookupException;
 
+    /**
+     * Same as {@link List#listIterator(int)}, except this method will throw a 
+     * {@link ComponentLookupException} if one or more elements in the set fail during lookup.
+     * 
+     * @throws ComponentLookupException if one or more elements of the set fail during lookup.
+     */
     ListIterator checkedListIterator( int index )
         throws ComponentLookupException;
 
+    /**
+     * Same as {@link List#subList(int, int)}, except this method will throw a 
+     * {@link ComponentLookupException} if one or more elements in the set fail during lookup.
+     * 
+     * @throws ComponentLookupException if one or more elements of the set fail during lookup.
+     */
     List checkedSubList( int fromIndex, int toIndex )
         throws ComponentLookupException;
 
+    /**
+     * Same as {@link List#toArray()}, except this method will throw a 
+     * {@link ComponentLookupException} if one or more elements in the set fail during lookup.
+     * 
+     * @throws ComponentLookupException if one or more elements of the set fail during lookup.
+     */
     Object[] checkedToArray()
         throws ComponentLookupException;
 
+    /**
+     * Same as {@link List#toArray(Object[])}, except this method will throw a 
+     * {@link ComponentLookupException} if one or more elements in the set fail during lookup.
+     * 
+     * @throws ComponentLookupException if one or more elements of the set fail during lookup.
+     */
     Object[] checkedToArray( Object[] array )
         throws ComponentLookupException;
-
-    boolean contains( Object value );
-
-    boolean containsAll( Collection collection );
-
-    Object get( int index );
-
-    int indexOf( Object value );
-
-    Iterator iterator();
-
-    int lastIndexOf( Object value );
-
-    ListIterator listIterator();
-
-    ListIterator listIterator( int index );
-
-    List subList( int fromIndex, int toIndex );
-
-    Object[] toArray();
-
-    Object[] toArray( Object[] array );
 
 }
