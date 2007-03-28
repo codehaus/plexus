aabf1a4c824a393dcebabd1bbe0da262d59e86f9
jesse <jesse@30dfa635-7bf8-0310-9608-e41f2d776689>
2007-03-28 21:45:21 +0000

    [maven-scm] copy for tag plexus-appserver-2.0-alpha-8

---
+++ plexus-appserver/tags/plexus-appserver-2.0-alpha-8/plexus-appserver-host/src/main/java/org/codehaus/plexus/appserver/application/deploy/lifecycle/phase/CreateAppConfigurationPhase.java
@@ -0,0 +1,147 @@
+package org.codehaus.plexus.appserver.application.deploy.lifecycle.phase;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Properties;
+
+import org.codehaus.plexus.DefaultPlexusContainer;
+import org.codehaus.plexus.appserver.ApplicationServerConstants;
+import org.codehaus.plexus.appserver.application.deploy.lifecycle.AppDeploymentContext;
+import org.codehaus.plexus.appserver.application.deploy.lifecycle.AppDeploymentException;
+import org.codehaus.plexus.configuration.PlexusConfiguration;
+import org.codehaus.plexus.configuration.xml.XmlPlexusConfiguration;
+import org.codehaus.plexus.context.Context;
+import org.codehaus.plexus.context.ContextException;
+import org.codehaus.plexus.util.InterpolationFilterReader;
+import org.codehaus.plexus.util.xml.Xpp3Dom;
+import org.codehaus.plexus.util.xml.Xpp3DomBuilder;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+
+/**
+ * @author Jason van Zyl
+ */
+public class CreateAppConfigurationPhase
+    extends AbstractAppDeploymentPhase
+{
+    public void execute( AppDeploymentContext context )
+        throws AppDeploymentException
+    {
+
+        DefaultPlexusContainer serverContainer = context.getAppServerContainer();
+
+        getLogger().info( "Using appDir = " + context.getAppDir() );
+
+        // ----------------------------------------------------------------------------
+        // Create the containers context value mappings
+        // ----------------------------------------------------------------------------
+
+        Map contextMap = new HashMap();
+
+        // need of values from container context to be interpolated 
+        Context containerContext = serverContainer.getContext();
+        contextMap.putAll( containerContext.getContextData() );
+
+        // some will be override with the following code
+
+        Properties contextValues = context.getContext();
+
+        if ( contextValues != null )
+        {
+            for ( Iterator i = contextValues.keySet().iterator(); i.hasNext(); )
+            {
+                String contextName = (String) i.next();
+
+                contextMap.put( contextName, contextValues.getProperty( contextName ) );
+            }
+        }
+
+        // ----------------------------------------------------------------------
+        // We want to set ${app.home} and we want to create a new realm for the
+        // appserver. Need to think about how to really separate the apps
+        // from the parent container.
+        // ----------------------------------------------------------------------
+
+        if ( !contextMap.containsKey( ApplicationServerConstants.APP_SERVER_HOME_KEY ) )
+        {
+            contextMap.put( ApplicationServerConstants.APP_SERVER_HOME_KEY, context.getAppServer().getAppServerHome()
+                .getAbsolutePath() );
+        }
+
+        if ( !contextMap.containsKey( ApplicationServerConstants.APP_SERVER_BASE_KEY ) )
+        {
+            contextMap.put( ApplicationServerConstants.APP_SERVER_BASE_KEY, context.getAppServer().getAppServerBase()
+                .getAbsolutePath() );
+        }
+
+        getLogger().debug( "appserver.home = " + contextMap.get( ApplicationServerConstants.APP_SERVER_HOME_KEY ) );
+        getLogger().debug( "appserver.base = " + contextMap.get( ApplicationServerConstants.APP_SERVER_BASE_KEY ) );
+
+        // ----------------------------------------------------------------------------
+        // Make the application's home directory available in the context
+        // ----------------------------------------------------------------------------
+        contextMap.put( "plexus.home", context.getAppDir().getAbsolutePath() );
+
+        contextMap.put( "app.home", context.getAppDir().getAbsolutePath() );
+
+        // ----------------------------------------------------------------------------
+        // Make the user's home directory available in the context
+        // ----------------------------------------------------------------------------
+        //noinspection AccessOfSystemProperties
+        contextMap.put( "user.home", System.getProperty( "user.home" ) );
+
+        Object appserver = null;
+
+        try
+        {
+            appserver = serverContainer.getContext().get( ApplicationServerConstants.APP_SERVER_CONTEXT_KEY );
+        }
+        catch ( ContextException e )
+        {
+            // won't happen.
+        }
+
+        contextMap.put( ApplicationServerConstants.APP_SERVER_CONTEXT_KEY, appserver );
+
+        context.setContextValues( contextMap );
+
+        // ----------------------------------------------------------------------
+        //
+        // ----------------------------------------------------------------------
+
+        Xpp3Dom dom = null;
+
+        try
+        {
+            //noinspection IOResourceOpenedButNotSafelyClosed
+            Reader configurationReader = new InterpolationFilterReader( new FileReader( context
+                .getAppConfigurationFile() ), contextMap );
+
+            dom = Xpp3DomBuilder.build( configurationReader );
+        }
+        catch ( FileNotFoundException e )
+        {
+            // we skipped this once already, ignore it this time.
+            // Would be better to preload it instead.
+        }
+        catch ( IOException e )
+        {
+            throw new AppDeploymentException( "Error processing application configurator.", e );
+        }
+        catch ( XmlPullParserException e )
+        {
+            throw new AppDeploymentException( "Error processing application configurator.", e );
+        }
+
+        if ( dom != null )
+        {
+            PlexusConfiguration applicationConfiguration = new XmlPlexusConfiguration( dom );
+
+            context.setAppConfiguration( applicationConfiguration );
+        }
+    }
+}
---
+++ plexus-appserver/tags/plexus-appserver-2.0-alpha-8/plexus-appserver-services/plexus-appserver-service-jetty/src/main/java/org/codehaus/plexus/jetty/JettyPlusServletContainer.java
@@ -0,0 +1,56 @@
+package org.codehaus.plexus.jetty;
+
+import org.codehaus.plexus.personality.plexus.lifecycle.phase.StartingException;
+import org.mortbay.jetty.plus.Server;
+import org.mortbay.xml.XmlConfiguration;
+
+import java.io.File;
+
+public class JettyPlusServletContainer
+    extends AbstractJettyServletContainer
+{
+    private File jettyXmlFile;
+
+    public void setJettyXmlFile( File jettyXmlFile )
+    {
+        this.jettyXmlFile = jettyXmlFile;
+    }
+
+    public File getJettyXmlFile()
+    {
+        return jettyXmlFile;
+    }
+
+    public void applyJettyXml()
+        throws Exception
+    {
+        if ( jettyXmlFile == null )
+        {
+            return;
+        }
+
+        if ( jettyXmlFile.exists() )
+        {
+            getLogger().info( "Configuring Jetty from xml configuration file = " + jettyXmlFile.getCanonicalPath() );
+            XmlConfiguration xmlConfiguration = new XmlConfiguration( jettyXmlFile.getCanonicalFile().toURL() );
+            xmlConfiguration.configure( server );
+        }
+    }
+
+    public void start()
+        throws StartingException
+    {
+        server = new Server();
+
+        try
+        {
+            applyJettyXml();
+
+            server.start();
+        }
+        catch ( Exception e )
+        {
+            throw new StartingException( "Error while starting Jetty", e );
+        }
+    }
+}
