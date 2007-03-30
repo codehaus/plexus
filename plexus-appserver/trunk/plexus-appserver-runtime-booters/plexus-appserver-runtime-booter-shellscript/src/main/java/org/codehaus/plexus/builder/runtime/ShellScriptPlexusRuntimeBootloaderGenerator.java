2023164fafa0b301891634e54ed3b9319cfdfdeb
handyande <handyande@30dfa635-7bf8-0310-9608-e41f2d776689>
2007-03-30 00:17:10 +0000

    Shell scripts are now split in to seperate platform generators, nice test - on to JSW tomorrow. Will convert this to CDC after the refactoring is complete

--- plexus-appserver/trunk/plexus-appserver-runtime-booters/plexus-appserver-runtime-booter-shellscript/src/main/java/org/codehaus/plexus/builder/runtime/ShellScriptPlexusRuntimeBootloaderGenerator.java
+++ plexus-appserver/trunk/plexus-appserver-runtime-booters/plexus-appserver-runtime-booter-shellscript/src/main/java/org/codehaus/plexus/builder/runtime/ShellScriptPlexusRuntimeBootloaderGenerator.java
@@ -1,28 +1,66 @@
 package org.codehaus.plexus.builder.runtime;
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.codehaus.plexus.builder.runtime.platform.ShellScriptPlatformGenerator;
+
 import java.io.File;
 import java.util.Properties;
+import java.util.Map;
+import java.util.Iterator;
 
 /**
  * @author <a href="jason@maven.org">Jason van Zyl</a>
  * @author <a href="mailto:trygvis@inamo.no">Trygve Laugst&oslash;l</a>
  */
 public class ShellScriptPlexusRuntimeBootloaderGenerator
-    extends AbstractPlexusRuntimeBootloaderGenerator
+    implements PlexusRuntimeBootloaderGenerator
 {
-    private final static String UNIX_LAUNCHER_TEMPLATE = "shellscript/plexus.vm";
+    private Map platformGenerators;
 
-    private final static String WINDOWS_LAUNCHER_TEMPLATE = "shellscript/plexus-bat.vm";
+    private GeneratorTools tools;
 
     public void generate( File outputDirectory, Properties configurationProperties )
         throws PlexusRuntimeBootloaderGeneratorException
     {
-        mkdirs( outputDirectory );
+        tools.mkdirs( outputDirectory );
 
-        mergeTemplate( ShellScriptPlexusRuntimeBootloaderGenerator.UNIX_LAUNCHER_TEMPLATE, new File( outputDirectory, "plexus.sh" ), false );
+        // ----------------------------------------------------------------------------
+        // Look up the appropriate generators
+        // ----------------------------------------------------------------------------
 
-        mergeTemplate( ShellScriptPlexusRuntimeBootloaderGenerator.WINDOWS_LAUNCHER_TEMPLATE, new File( outputDirectory, "plexus.bat" ), true );
+        // for now just run them all, this needs to be configurable
+        // TODO make the list we load configurable
+        Iterator platforms = platformGenerators.keySet().iterator();
+        while ( platforms.hasNext() )
+        {
+            ShellScriptPlatformGenerator platform =
+                (ShellScriptPlatformGenerator) platformGenerators.get(platforms.next() );
 
-        executable( new File( outputDirectory, "plexus.sh" ) );
+            // TODO figure if we can fix / remove this second param
+            platform.generate( outputDirectory, "", configurationProperties );
+        }
+    }
+
+    public Map getPlatformGenerators()
+    {
+        return platformGenerators;
     }
 }
