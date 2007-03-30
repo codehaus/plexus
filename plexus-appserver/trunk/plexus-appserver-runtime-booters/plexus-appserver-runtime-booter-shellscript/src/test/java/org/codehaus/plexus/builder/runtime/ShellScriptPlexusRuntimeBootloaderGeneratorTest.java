60ad00fd00c2662b5819ead1ea4e40dadc30a6f6
handyande <handyande@30dfa635-7bf8-0310-9608-e41f2d776689>
2007-03-30 00:17:10 +0000

    Shell scripts are now split in to seperate platform generators, nice test - on to JSW tomorrow. Will convert this to CDC after the refactoring is complete

---
+++ plexus-appserver/trunk/plexus-appserver-runtime-booters/plexus-appserver-runtime-booter-shellscript/src/test/java/org/codehaus/plexus/builder/runtime/ShellScriptPlexusRuntimeBootloaderGeneratorTest.java
@@ -0,0 +1,73 @@
+package org.codehaus.plexus.builder.runtime;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.codehaus.plexus.PlexusTestCase;
+
+import java.io.File;
+import java.util.Properties;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author Andrew Williams
+ * @version $Id$
+ * @since 1.0
+ */
+public class ShellScriptPlexusRuntimeBootloaderGeneratorTest
+    extends PlexusTestCase
+{
+    private File outDir;
+
+    public void setUp()
+        throws Exception
+    {
+        super.setUp();
+
+        outDir = getTestFile( "target/tmp" );
+        outDir.mkdirs();
+    }
+
+    public void testLoadComponents()
+        throws Exception
+    {
+        PlexusRuntimeBootloaderGenerator shellGenerator = getShellGenerator();
+
+        assertTrue( shellGenerator.getPlatformGenerators().size() > 0 );
+    }
+
+    public void testMakesPlexusScripts()
+        throws Exception
+    {
+        PlexusRuntimeBootloaderGenerator shellGenerator = getShellGenerator();
+
+        shellGenerator.generate( outDir, new Properties() );
+
+        assertTrue( ( new File( outDir, "plexus.sh" ) ).exists() );
+        assertTrue( ( new File( outDir, "plexus.bat" ) ).exists() );
+    }
+
+    private PlexusRuntimeBootloaderGenerator getShellGenerator()
+        throws Exception
+    {
+        return (PlexusRuntimeBootloaderGenerator)
+            lookup( "org.codehaus.plexus.builder.runtime.PlexusRuntimeBootloaderGenerator", "shellscript" );
+    }
+}
