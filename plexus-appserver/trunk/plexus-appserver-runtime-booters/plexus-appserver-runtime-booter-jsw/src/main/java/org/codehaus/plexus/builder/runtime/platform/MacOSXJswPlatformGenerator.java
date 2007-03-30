c87a8c6849e48029315333c4c90227e9f5654f6a
handyande <handyande@30dfa635-7bf8-0310-9608-e41f2d776689>
2007-03-30 11:46:14 +0000

    The bulk of the generator code for jsw. The resources now need sorted out, as some are being picked up from the builder Duplicate code needs to be refactored to common parents or utils

---
+++ plexus-appserver/trunk/plexus-appserver-runtime-booters/plexus-appserver-runtime-booter-jsw/src/main/java/org/codehaus/plexus/builder/runtime/platform/MacOSXJswPlatformGenerator.java
@@ -0,0 +1,62 @@
+package org.codehaus.plexus.builder.runtime.platform;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.codehaus.plexus.builder.runtime.PlexusRuntimeBootloaderGeneratorException;
+
+import java.io.File;
+import java.util.Properties;
+
+/**
+ * Generator to create the MacOSX specific portions of the JSW booter.
+ *
+ * @author Andrew Williams
+ * @version $Id$
+ * @since 2.0-alpha-9
+ */
+public class MacOSXJswPlatformGenerator
+    extends AbstractJswPlatformGenerator
+{
+    public static final String MACOSX = "macosx";
+
+    public static final String MACOSX_SOURCE = JSW + "/wrapper-macosx-universal-32-" + JSW_VERSION;
+
+    public void generate( File binDirectory,
+                          String resourceDir,
+                          Properties configurationProperties )
+        throws PlexusRuntimeBootloaderGeneratorException
+    {
+        // TODO: make it configurable - we don't always want a subdir
+        File osxBinDir = new File( binDirectory, MACOSX );
+        tools.mkdirs( osxBinDir );
+
+        File runSh = new File( osxBinDir, "run.sh" );
+        tools.filterCopy( tools.getResourceAsStream( JSW + "/run.sh" ), runSh, configurationProperties );
+        tools.executable( runSh );
+
+        tools.copyResource( MACOSX + "/wrapper", MACOSX_SOURCE + "/bin/wrapper", true, binDirectory  );
+        tools.copyResource( MACOSX + "/libwrapper.jnilib", MACOSX_SOURCE + "/lib/libwrapper.jnilib", false, binDirectory );
+
+        Properties osxProps = new Properties();
+        osxProps.setProperty( "library.path", "../../bin/" + MACOSX );
+        osxProps.setProperty( "extra.path", "" );
+        copyWrapperConf( osxBinDir, configurationProperties, osxProps );
+    }
+}
---
+++ plexus-appserver/trunk/plexus-appserver-runtime-booters/plexus-appserver-runtime-booter-jsw/src/test/java/org/codehaus/plexus/builder/runtime/JswPlexusRuntimeBootloaderGeneratorTest.java
@@ -0,0 +1,71 @@
+package org.codehaus.plexus.builder.runtime;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.codehaus.plexus.PlexusTestCase;
+
+import java.io.File;
+import java.util.Properties;
+
+/**
+ * Simple test for the jsw wrappers, making sure they are found.
+ * See the individual Jsw PlatformGenerator tests for more testing.
+ *
+ * @author Andrew Williams
+ * @version $Id$
+ * @since 2.0-alpha-9
+ */
+public class JswPlexusRuntimeBootloaderGeneratorTest
+    extends PlexusTestCase
+{
+    private File outDir;
+
+    public void setUp()
+        throws Exception
+    {
+        super.setUp();
+
+        outDir = getTestFile( "target/tmp" );
+        outDir.mkdirs();
+    }
+
+    public void testLoadComponents()
+        throws Exception
+    {
+        PlexusRuntimeBootloaderGenerator jswGenerator = getJswGenerator();
+
+        assertTrue( jswGenerator.getPlatformGenerators().size() > 0 );
+    }
+
+    public void testMakesPlexusScripts()
+        throws Exception
+    {
+        PlexusRuntimeBootloaderGenerator jswGenerator = getJswGenerator();
+
+        jswGenerator.generate( outDir, new Properties() );
+    }
+
+    private PlexusRuntimeBootloaderGenerator getJswGenerator()
+        throws Exception
+    {
+        return (PlexusRuntimeBootloaderGenerator)
+            lookup( "org.codehaus.plexus.builder.runtime.PlexusRuntimeBootloaderGenerator", "jsw" );
+    }
+}
