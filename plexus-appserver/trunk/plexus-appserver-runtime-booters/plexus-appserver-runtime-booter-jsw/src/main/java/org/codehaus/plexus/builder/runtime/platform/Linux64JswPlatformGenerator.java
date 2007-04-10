ab4fdc0ad18b93af4a5096e530ac9a80df9c664c
handyande <handyande@30dfa635-7bf8-0310-9608-e41f2d776689>
2007-04-10 20:59:42 +0000

    duplicate linux, splitting to 32 and 64 variants

--- plexus-appserver/trunk/plexus-appserver-runtime-booters/plexus-appserver-runtime-booter-jsw/src/main/java/org/codehaus/plexus/builder/runtime/platform/Linux64JswPlatformGenerator.java
+++ plexus-appserver/trunk/plexus-appserver-runtime-booters/plexus-appserver-runtime-booter-jsw/src/main/java/org/codehaus/plexus/builder/runtime/platform/Linux64JswPlatformGenerator.java
@@ -1,42 +1,23 @@
 package org.codehaus.plexus.builder.runtime.platform;
 
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
 import org.codehaus.plexus.builder.runtime.PlexusRuntimeBootloaderGeneratorException;
 
 import java.io.File;
 import java.util.Properties;
 
 /**
- * Generator to create the Linux specific portions of the JSW booter.
+ * Generator to create the Linux 64 bit specific portions of the JSW booter.
  *
  * @author Andrew Williams
  * @version $Id$
  * @since 2.0-alpha-9
  */
-public class LinuxJswPlatformGenerator
+public class Linux64JswPlatformGenerator
     extends AbstractJswPlatformGenerator
 {
-    public static final String LINUX = "linux";
+    public static final String LINUX = "linux-x86-64";
 
-    public static final String LINUX_SOURCE = JSW + "/wrapper-linux-x86-32-" + JSW_VERSION;
+    public static final String LINUX_SOURCE = JSW + "/wrapper-linux-x86-64-" + JSW_VERSION;
 
     public void generate( File binDirectory,
                           String resourceDir,
