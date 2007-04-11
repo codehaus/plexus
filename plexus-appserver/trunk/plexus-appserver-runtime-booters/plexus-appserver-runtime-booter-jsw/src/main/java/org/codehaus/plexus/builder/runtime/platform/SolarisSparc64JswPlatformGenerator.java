9226bfac1f234294d7e69ed7342ea52726ee2b40
handyande <handyande@30dfa635-7bf8-0310-9608-e41f2d776689>
2007-04-11 12:39:57 +0000

    Split solaris into the 3 systems jsw supports - sparc-32, sparc-64 and xa6-32

--- plexus-appserver/trunk/plexus-appserver-runtime-booters/plexus-appserver-runtime-booter-jsw/src/main/java/org/codehaus/plexus/builder/runtime/platform/SolarisSparc64JswPlatformGenerator.java
+++ plexus-appserver/trunk/plexus-appserver-runtime-booters/plexus-appserver-runtime-booter-jsw/src/main/java/org/codehaus/plexus/builder/runtime/platform/SolarisSparc64JswPlatformGenerator.java
@@ -1,24 +1,5 @@
 package org.codehaus.plexus.builder.runtime.platform;
 
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
 import org.codehaus.plexus.builder.runtime.PlexusRuntimeBootloaderGeneratorException;
 
 import java.io.File;
@@ -31,12 +12,12 @@ import java.util.Properties;
  * @version $Id$
  * @since 2.0-alpha-9
  */
-public class SolarisJswPlatformGenerator
+public class SolarisSparc64JswPlatformGenerator
     extends AbstractJswPlatformGenerator
 {
-    public static final String SOLARIS = "solaris";
+    public static final String SOLARIS = "solaris-sparc-64";
 
-    public static final String SOLARIS_SOURCE = JSW + "/wrapper-solaris-sparc-32-" + JSW_VERSION;
+    public static final String SOLARIS_SOURCE = JSW + "/wrapper-solaris-sparc-64-" + JSW_VERSION;
 
     public void generate( File binDirectory,
                           String resourceDir,
---
+++ plexus-appserver/trunk/plexus-appserver-runtime-booters/plexus-appserver-runtime-booter-jsw/src/main/java/org/codehaus/plexus/builder/runtime/platform/SolarisX8632JswPlatformGenerator.java
@@ -0,0 +1,43 @@
+package org.codehaus.plexus.builder.runtime.platform;
+
+import org.codehaus.plexus.builder.runtime.PlexusRuntimeBootloaderGeneratorException;
+
+import java.io.File;
+import java.util.Properties;
+
+/**
+ * Generator to create the Solaris specific portions of the JSW booter.
+ *
+ * @author Andrew Williams
+ * @version $Id$
+ * @since 2.0-alpha-9
+ */
+public class SolarisX8632JswPlatformGenerator
+    extends AbstractJswPlatformGenerator
+{
+    public static final String SOLARIS = "solaris-x86-32";
+
+    public static final String SOLARIS_SOURCE = JSW + "/wrapper-solaris-x86-32-" + JSW_VERSION;
+
+    public void generate( File binDirectory,
+                          String resourceDir,
+                          Properties configurationProperties )
+        throws PlexusRuntimeBootloaderGeneratorException
+    {
+        // TODO: make it configurable - we don't always want a subdir
+        File solarisBinDir = new File( binDirectory, SOLARIS );
+        tools.mkdirs( solarisBinDir );
+
+        File runSh = new File( solarisBinDir, "run.sh" );
+        tools.filterCopy( tools.getResourceAsStream( JSW + "/wrapper-common-" + JSW_VERSION + "/src/bin/sh.script.in" ), runSh, configurationProperties );
+        tools.executable( runSh );
+
+        tools.copyResource( SOLARIS + "/wrapper", SOLARIS_SOURCE + "/bin/wrapper", true, binDirectory  );
+        tools.copyResource( SOLARIS + "/libwrapper.so", SOLARIS_SOURCE + "/lib/libwrapper.so", false, binDirectory );
+
+        Properties solarisProps = new Properties();
+        solarisProps.setProperty( "library.path", "../../bin/" + SOLARIS );
+        solarisProps.setProperty( "extra.path", "" );
+        copyWrapperConf( solarisBinDir, configurationProperties, solarisProps );
+    }
+}
