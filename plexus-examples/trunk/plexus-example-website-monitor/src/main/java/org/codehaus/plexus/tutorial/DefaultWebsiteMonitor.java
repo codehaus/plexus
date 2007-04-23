13310c255a84dcc955238911dc8c5b1d8879b034
rahul <rahul@30dfa635-7bf8-0310-9608-e41f2d776689>
2007-04-23 09:19:52 +0000

    o  simply throw ex, don't log.

--- plexus-examples/trunk/plexus-example-website-monitor/src/main/java/org/codehaus/plexus/tutorial/DefaultWebsiteMonitor.java
+++ plexus-examples/trunk/plexus-example-website-monitor/src/main/java/org/codehaus/plexus/tutorial/DefaultWebsiteMonitor.java
@@ -2,18 +2,16 @@
  * ========================================================================
  * 
  * Copyright 2006 Rahul Thakur.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
  * 
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  * 
  * ========================================================================
  */
@@ -65,14 +63,9 @@ public class DefaultWebsiteMonitor
                 int statusCode = client.executeMethod( getMethod );
 
                 if ( statusCode < HttpStatus.SC_OK || statusCode >= HttpStatus.SC_MULTIPLE_CHOICES )
-                {
-                    if ( getLogger().isErrorEnabled() )
-                        getLogger().error(
-                                           "HTTP request returned HTTP status code: " + statusCode + " for website: "
-                                               + website );
                     throw new Exception( "HTTP request returned HTTP status code: " + statusCode + " for website: "
                         + website );
-                }
+
                 if ( getLogger().isInfoEnabled() )
                     getLogger().info(
                                       "HTTP request returned HTTP status code: " + statusCode + " for website: "
