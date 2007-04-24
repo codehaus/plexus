b6f93d1f7ffb908da25efa9bc149f3e150de3c7e
jvanzyl <jvanzyl@30dfa635-7bf8-0310-9608-e41f2d776689>
2007-04-24 19:47:39 +0000

    [maven-scm] copy for tag plexus-containers-1.0-alpha-24

---
+++ plexus-containers/tags/plexus-containers-1.0-alpha-24/plexus-container-default/src/main/java/org/codehaus/plexus/personality/plexus/lifecycle/phase/LogEnablePhase.java
@@ -0,0 +1,58 @@
+package org.codehaus.plexus.personality.plexus.lifecycle.phase;
+
+/*
+ * Copyright 2001-2006 Codehaus Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.codehaus.plexus.classworlds.realm.ClassRealm;
+import org.codehaus.plexus.component.manager.ComponentManager;
+import org.codehaus.plexus.component.repository.ComponentDescriptor;
+import org.codehaus.plexus.component.repository.exception.ComponentLookupException;
+import org.codehaus.plexus.lifecycle.phase.AbstractPhase;
+import org.codehaus.plexus.logging.LogEnabled;
+import org.codehaus.plexus.logging.Logger;
+import org.codehaus.plexus.logging.LoggerManager;
+
+import java.util.Map;
+
+public class LogEnablePhase
+    extends AbstractPhase
+{
+    public void execute( Object object, ComponentManager componentManager, ClassRealm lookupRealm )
+        throws PhaseExecutionException
+    {
+        if ( object instanceof LogEnabled )
+        {
+            LoggerManager loggerManager;
+            try
+            {
+                /* as we do not know what logger role hint has been configured pull the first logger we find. Andy
+                 * TODO - figure how to make this more determanistic? */
+                Map loggers = componentManager.getContainer().lookupMap( LoggerManager.ROLE, lookupRealm );
+                loggerManager = (LoggerManager) loggers.get( loggers.keySet().iterator().next() );
+            }
+            catch ( ComponentLookupException e )
+            {
+                throw new PhaseExecutionException( "Unable to locate logger manager", e );
+            }
+
+            ComponentDescriptor descriptor = componentManager.getComponentDescriptor();
+
+            Logger logger = loggerManager.getLoggerForComponent( descriptor.getRole(), descriptor.getRoleHint() );
+
+            ( (LogEnabled) object ).enableLogging( logger );
+        }
+    }
+}
