44963b2e72c36e6b68d3f2af93902865854abac5
handyande <handyande@30dfa635-7bf8-0310-9608-e41f2d776689>
2007-04-25 12:48:53 +0000

    Wrong licensee, thanks Brett

--- plexus-appserver/trunk/plexus-appserver-runtime-booters/plexus-appserver-runtime-booter-jsw/src/main/java/org/codehaus/plexus/builder/runtime/platform/SolarisX8632JswPlatformGenerator.java
+++ plexus-appserver/trunk/plexus-appserver-runtime-booters/plexus-appserver-runtime-booter-jsw/src/main/java/org/codehaus/plexus/builder/runtime/platform/SolarisX8632JswPlatformGenerator.java
@@ -1,10 +1,10 @@
 package org.codehaus.plexus.builder.runtime.platform;
 
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
+ * Licensed to The Codehaus ( www.codehaus.org ) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
+ * regarding copyright ownership.  The Codehaus licenses this file
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
