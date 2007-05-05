0b750b18ffc9fdfb80543604bfb00e52ea3bb491
kenney <kenney@30dfa635-7bf8-0310-9608-e41f2d776689>
2007-05-05 20:28:09 +0000

    Update AppInitializationPhase: put back app.home/lib/*.jar adding to application realm

--- plexus-appserver/trunk/plexus-appserver-host/src/main/java/org/codehaus/plexus/appserver/application/deploy/lifecycle/phase/AppInitializationPhase.java
+++ plexus-appserver/trunk/plexus-appserver-host/src/main/java/org/codehaus/plexus/appserver/application/deploy/lifecycle/phase/AppInitializationPhase.java
@@ -1,9 +1,16 @@
 package org.codehaus.plexus.appserver.application.deploy.lifecycle.phase;
 
+import java.io.File;
+import java.io.FileFilter;
+import java.net.MalformedURLException;
+
 import org.codehaus.plexus.DefaultPlexusContainer;
 import org.codehaus.plexus.PlexusContainerException;
 import org.codehaus.plexus.appserver.application.deploy.lifecycle.AppDeploymentContext;
 import org.codehaus.plexus.appserver.application.deploy.lifecycle.AppDeploymentException;
+import org.codehaus.plexus.classworlds.ClassWorld;
+import org.codehaus.plexus.classworlds.realm.ClassRealm;
+import org.codehaus.plexus.classworlds.realm.NoSuchRealmException;
 
 /**
  * @author Jason van Zyl
@@ -24,11 +31,12 @@ public class AppInitializationPhase
 
         try
         {
+            addLibJars( context, name );
+
             // This call will initialise and start the container
 
-            applicationContainer = new DefaultPlexusContainer( name, context.getContextValues(),
-                                                               context.getAppConfigurationFile().getAbsoluteFile(),
-                                                               context.getAppRuntimeProfile().getApplicationWorld() );
+            applicationContainer = new DefaultPlexusContainer( name, context.getContextValues(), context
+                .getAppConfigurationFile().getAbsoluteFile(), context.getAppRuntimeProfile().getApplicationWorld() );
         }
         catch ( PlexusContainerException e )
         {
@@ -36,5 +44,50 @@ public class AppInitializationPhase
         }
 
         context.getAppRuntimeProfile().setApplicationContainer( applicationContainer );
+    }
+
+    private void addLibJars( AppDeploymentContext context, String realmName )
+        throws AppDeploymentException
+    {
+        // not using fileutils here since that can throw an IOException
+        File[] jars = context.getAppLibDirectory().listFiles( new FileFilter()
+        {
+            public boolean accept( File pathname )
+            {
+                return pathname.isFile() && pathname.toString().endsWith( ".jar" );
+            }
+        } );
+
+        // should not happen
+        if ( jars == null )
+        {
+            getLogger().warn( "Not a directory: " + context.getAppLibDirectory() );
+        }
+        else
+        {
+            ClassWorld appWorld = context.getAppRuntimeProfile().getApplicationWorld();
+            ClassRealm realm;
+
+            try
+            {
+                realm = appWorld.getRealm( realmName );
+            }
+            catch ( NoSuchRealmException e1 )
+            {
+                throw new AppDeploymentException( "Realm not found: " + realmName, e1 );
+            }
+
+            for ( int i = 0; i < jars.length; i++ )
+            {
+                try
+                {
+                    realm.addURL( jars[i].toURI().toURL() );
+                }
+                catch ( MalformedURLException e )
+                {
+                    getLogger().warn( "Error converting file " + jars[i] + " to URL", e );
+                }
+            }
+        }
     }
 }
