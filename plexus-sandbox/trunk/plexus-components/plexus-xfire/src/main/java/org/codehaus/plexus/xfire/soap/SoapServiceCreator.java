1fe5cfca34d05b9013bc6a33462dbef4f73a53e2
jesse <jesse@30dfa635-7bf8-0310-9608-e41f2d776689>
2007-05-11 22:39:34 +0000

    bit of a stab at making plexus-xfire a bit more functional...I have some work to do on it before I use it much further, key bit right would be to wack out the parts of the DefaultSoapServiceCreator that I added that read the xml files and force the creation of the service.  I should make that process be something that a ConfigurationSource can pull from in that service creator, but anyway, the basic jist of that is in there now.  I would like to make the configuration bits extensible so that in redback I could maybe wrap things up so you just specify a security operation that might be required for authz and then have that checked before passing through to the actual service, maybe even be able to flag it as authn required or not.  some more thought on that needed, I know squat about webservices and xfire, just thought I would look at plexus-xfire integration and make the tests work.  and before anyone says hey, why didn't you use Foo plexus service whatever...I didn't know about it...

--- plexus-sandbox/trunk/plexus-components/plexus-xfire/src/main/java/org/codehaus/plexus/xfire/soap/SoapServiceCreator.java
+++ plexus-sandbox/trunk/plexus-components/plexus-xfire/src/main/java/org/codehaus/plexus/xfire/soap/SoapServiceCreator.java
@@ -1,5 +1,21 @@
 package org.codehaus.plexus.xfire.soap;
 
+/*
+ * Copyright 2001-2006 The Codehaus.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import org.codehaus.xfire.service.Service;
 
 /**
@@ -7,7 +23,7 @@ import org.codehaus.xfire.service.Service;
  */
 public interface SoapServiceCreator
 {
-    String ROLE = SoapServiceCreator.class.getName();
+    public static final String ROLE = SoapServiceCreator.class.getName();
 
     Service createService( String role )
         throws SoapServiceCreationException;
