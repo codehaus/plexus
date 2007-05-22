e1cf44fb419174a98bc857b9f8464b188717d60d
jesse <jesse@30dfa635-7bf8-0310-9608-e41f2d776689>
2007-05-22 17:00:34 +0000

    revert the change where I clean the session on the login action and make sure all redirect instances are smoking the session

--- plexus-redback/trunk/redback-integrations/redback-xwork/redback-xwork-integration/src/main/java/org/codehaus/plexus/redback/xwork/action/LoginAction.java
+++ plexus-redback/trunk/redback-integrations/redback-xwork/redback-xwork-integration/src/main/java/org/codehaus/plexus/redback/xwork/action/LoginAction.java
@@ -89,7 +89,7 @@ public class LoginAction
     // ------------------------------------------------------------------
 
     public String show()
-    {       
+    {             
         return INPUT;
     }
 
