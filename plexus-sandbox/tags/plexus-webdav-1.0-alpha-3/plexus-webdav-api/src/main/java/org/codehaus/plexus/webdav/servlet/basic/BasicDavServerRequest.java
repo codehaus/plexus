501e574a1f79d8b406625df7482d69ad8d6dabcf
joakime <joakime@30dfa635-7bf8-0310-9608-e41f2d776689>
2007-06-21 14:11:07 +0000

    [maven-scm] copy for tag plexus-webdav-1.0-alpha-3

---
+++ plexus-sandbox/tags/plexus-webdav-1.0-alpha-3/plexus-webdav-api/src/main/java/org/codehaus/plexus/webdav/servlet/basic/BasicDavServerRequest.java
@@ -0,0 +1,59 @@
+package org.codehaus.plexus.webdav.servlet.basic;
+
+/*
+ * Copyright 2001-2007 The Codehaus.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+import org.codehaus.plexus.webdav.servlet.DavServerRequest;
+import org.codehaus.plexus.webdav.util.WrappedRepositoryRequest;
+
+/**
+ * BasicDavServerRequest - for requests that have a prefix based off of the servlet path id.
+ *
+ * @author <a href="mailto:joakim@erdfelt.com">Joakim Erdfelt</a>
+ * @version $Id$
+ */
+public class BasicDavServerRequest
+    implements DavServerRequest
+{
+    private WrappedRepositoryRequest request;
+
+    private String prefix;
+
+    private String logicalResource;
+
+    public BasicDavServerRequest( WrappedRepositoryRequest request )
+    {
+        this.request = request;
+        this.prefix = request.getServletPath();
+        this.logicalResource = request.getPathInfo();
+    }
+
+    public String getLogicalResource()
+    {
+        return this.logicalResource;
+    }
+
+    public String getPrefix()
+    {
+        return this.prefix;
+    }
+
+    public WrappedRepositoryRequest getRequest()
+    {
+        return request;
+    }
+}
