cff2de51fca574f185c241ba64f68e304f3c4167
kenney <kenney@30dfa635-7bf8-0310-9608-e41f2d776689>
2007-07-06 12:26:39 +0000

    Add /classes to the classpath; component overrides possible now.

--- plexus-appserver/trunk/plexus-appserver-host/src/main/java/org/codehaus/plexus/appserver/service/deploy/lifecycle/phase/AddServiceLibrariesPhase.java
+++ plexus-appserver/trunk/plexus-appserver-host/src/main/java/org/codehaus/plexus/appserver/service/deploy/lifecycle/phase/AddServiceLibrariesPhase.java
@@ -5,12 +5,18 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
 
+import org.codehaus.plexus.appserver.PlexusServiceConstants;
 import org.codehaus.plexus.appserver.service.deploy.lifecycle.ServiceDeploymentContext;
 import org.codehaus.plexus.appserver.service.deploy.lifecycle.ServiceDeploymentException;
 import org.codehaus.plexus.util.FileUtils;
 
 /**
+ * Adds service /classes and /lib/*.jar to the service classpath. The order is important - entries earlier in the
+ * classpath will override later entries, so /classes/META-INF/plexus/components.xml can be used to override/reconfigure
+ * components declared in lib/*.jar.
+ *
  * @author Jason van Zyl
+ * @author Kenney Westerhof
  */
 public class AddServiceLibrariesPhase
     extends AbstractServiceDeploymentPhase
@@ -18,6 +24,20 @@ public class AddServiceLibrariesPhase
     public void execute( ServiceDeploymentContext context )
         throws ServiceDeploymentException
     {
+        File classesDir = new File( context.getServiceDirectory(), PlexusServiceConstants.CLASSES_DIRECTORY );
+
+        try
+        {
+            if ( classesDir.isDirectory() )
+            {
+                context.getRealm().addURL( classesDir.toURL() );
+            }
+        }
+        catch ( IOException e )
+        {
+            throw new ServiceDeploymentException( "Cannot scan the service's /classes directory: " + classesDir, e );
+        }
+
         File libDir = new File( context.getServiceDirectory(), "lib" );
 
         if ( !libDir.exists() )
