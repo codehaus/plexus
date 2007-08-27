6756c5e0ffd68132de0cdc0648784fd9c454104b
jesse <jesse@30dfa635-7bf8-0310-9608-e41f2d776689>
2007-08-27 18:12:12 +0000

    common ldap classes

---
+++ plexus-redback/trunk/redback-common/redback-common-ldap/src/main/java/org/codehaus/plexus/redback/common/ldap/UserMapper.java
@@ -0,0 +1,56 @@
+package org.codehaus.plexus.redback.common.ldap;
+
+/*
+ * Copyright 2001-2007 The Codehaus.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.codehaus.plexus.redback.users.User;
+
+import javax.naming.directory.Attributes;
+
+public interface UserMapper
+{
+	public static final String ROLE = UserMapper.class.getName();
+
+    BasicUser getUser( Attributes attributes )
+        throws MappingException;
+
+    Attributes getCreationAttributes( User user, boolean encodePasswordIfChanged )
+        throws MappingException;
+
+    UserUpdate getUpdate( BasicUser user )
+        throws MappingException;
+
+    String[] getUserAttributeNames();
+
+    String getEmailAddressAttribute();
+
+    String getUserFullNameAttribute();
+
+    String getPasswordAttribute();
+
+    String getUserIdAttribute();
+
+    String getEmailAttribute();
+
+    String getUserBaseDn();
+
+    String getUserObjectClass();
+
+    BasicUser newUserInstance( String username, String fullName, String email );
+
+    BasicUser newTemplateUserInstance();
+
+}
