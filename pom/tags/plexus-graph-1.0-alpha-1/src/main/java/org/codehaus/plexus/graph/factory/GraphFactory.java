2d9892aebcf185a80824c73859bec71032d2dcf2
jvanzyl <jvanzyl@30dfa635-7bf8-0310-9608-e41f2d776689>
2007-09-17 05:17:50 +0000

    [maven-release-plugin]  copy for tag plexus-graph-1.0-alpha-1

---
+++ pom/tags/plexus-graph-1.0-alpha-1/src/main/java/org/codehaus/plexus/graph/factory/GraphFactory.java
@@ -0,0 +1,148 @@
+package org.codehaus.plexus.graph.factory;
+
+/*
+ * Licensed to the Codehaus Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.codehaus.plexus.graph.DirectedGraph;
+import org.codehaus.plexus.graph.MutableDirectedGraph;
+import org.codehaus.plexus.graph.contract.Contract;
+import org.codehaus.plexus.graph.domain.basic.DefaultDirectedGraph;
+import org.codehaus.plexus.graph.exception.GraphException;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+
+/** Description of the Class */
+public class GraphFactory
+{
+
+    /** Constructor for the GraphFactory object */
+    public GraphFactory()
+    {
+    }
+
+    /**
+     * makeGraph
+     *
+     * @param contracts     Which contracts to enforce.
+     * @param baseGraph     Is the actual *GraphImpl which will be at the core of
+     *                      the Proxy.
+     * @param baseGraphType Interface which is returned.
+     * @param isWeighted    Does the graph handle Weights?
+     */
+    private Object makeGraph( Contract contracts[],
+                              InvocationHandler baseGraph,
+                              Class baseGraphType,
+                              boolean isWeighted )
+        throws GraphException
+    {
+        int interfaceCount = contracts.length;
+        interfaceCount++;// BaseGraph Type
+        if ( isWeighted )
+        {
+            interfaceCount++;
+        }// WeightedGraph Type
+
+        Class inter[] = new Class[interfaceCount];
+
+        int pos = 0;
+        for ( pos = 0; pos < contracts.length; pos++ )
+        {
+            inter[pos] = contracts[pos].getInterface();
+        }
+
+        if ( isWeighted )
+        {
+            inter[pos] = org.codehaus.plexus.graph.WeightedGraph.class;
+            pos++;
+        }
+
+        inter[pos] = baseGraphType;
+
+        return Proxy.newProxyInstance( baseGraph.getClass().getClassLoader(), inter, baseGraph );
+    }
+
+    /**
+     * makeDirectedGraph
+     *
+     * @param contracts  - Array of Contracts this Graph should meet.
+     * @param isWeighted - If true, the Graph will implement the WeightedGraph
+     *                   interface.
+     * @param graph      - If it is provided, the graph will initially be equal to
+     *                   the graph.
+     */
+    public DirectedGraph makeDirectedGraph( Contract contracts[],
+                                            boolean isWeighted,
+                                            DirectedGraph graph )
+        throws GraphException
+    {
+        DefaultDirectedGraph dgi = null;
+
+        if ( graph != null )
+        {
+            dgi = new DefaultDirectedGraph( graph );
+        }
+        else
+        {
+            dgi = new DefaultDirectedGraph();
+        }
+
+        for ( int i = 0; i < contracts.length; i++ )
+        {
+            dgi.addContract( contracts[i] );
+        }
+
+        return (DirectedGraph) makeGraph( contracts, dgi, org.codehaus.plexus.graph.DirectedGraph.class, isWeighted );
+    }
+
+    /**
+     * makeMutableDirectedGraph
+     *
+     * @param contracts  - Array of Contracts this Graph should meet.
+     * @param isWeighted - If true, the Graph will implement the WeightedGraph
+     *                   interface.
+     * @param graph      - If it is provided, the graph will initially be equal to
+     *                   the graph.
+     */
+    public MutableDirectedGraph makeMutableDirectedGraph( Contract contracts[],
+                                                          boolean isWeighted,
+                                                          DirectedGraph graph )
+        throws GraphException
+    {
+
+        DefaultDirectedGraph dgi = null;
+
+        if ( graph != null )
+        {
+            dgi = new DefaultDirectedGraph( graph );
+        }
+        else
+        {
+            dgi = new DefaultDirectedGraph();
+        }
+
+        for ( int i = 0; i < contracts.length; i++ )
+        {
+            dgi.addContract( contracts[i] );
+        }
+
+        return (MutableDirectedGraph) makeGraph( contracts, dgi, org.codehaus.plexus.graph.MutableDirectedGraph.class,
+                                                 isWeighted );
+    }
+}
