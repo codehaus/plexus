8d7230088421f0e43d91537a05c3b34d5b67e4ae
eirbjo <eirbjo@30dfa635-7bf8-0310-9608-e41f2d776689>
2007-09-18 17:22:06 +0000

    Adding Plexus Mainclass Finder, a component that will find classes having a main method given a list of jars and/or directories containing classes. The default implementation uses ASM to analyze the byte code of each class

---
+++ plexus-sandbox/trunk/plexus-components/plexus-mainclass-finder/src/test/resources/classpath/org/codehaus/plexus/mainclass/MainClassInClassPath.java
@@ -0,0 +1,9 @@
+package org.codehaus.plexus.mainclass;
+
+public class MainClassInClassPath
+{
+    public static void main( String[] args )
+    {
+        
+    }
+}
---
+++ plexus-sandbox/trunk/plexus-components/plexus-mainclass-finder/src/test/resources/jarfiles/jar/org/codehaus/plexus/mainclass/MainClassInJar.java
@@ -0,0 +1,9 @@
+package org.codehaus.plexus.mainclass;
+
+public class MainClassInJar
+{
+    public static void main( String[] args )
+    {
+        
+    }
+}
