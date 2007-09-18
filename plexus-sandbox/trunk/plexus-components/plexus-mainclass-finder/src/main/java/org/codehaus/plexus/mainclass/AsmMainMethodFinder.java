56e5df4730d81dbcc5d552c83d9501e75293badf
eirbjo <eirbjo@30dfa635-7bf8-0310-9608-e41f2d776689>
2007-09-18 17:22:06 +0000

    Adding Plexus Mainclass Finder, a component that will find classes having a main method given a list of jars and/or directories containing classes. The default implementation uses ASM to analyze the byte code of each class

---
+++ plexus-sandbox/trunk/plexus-components/plexus-mainclass-finder/src/main/java/org/codehaus/plexus/mainclass/AsmMainMethodFinder.java
@@ -0,0 +1,80 @@
+package org.codehaus.plexus.mainclass;
+
+/*
+ * Copyright 2007 The Codehaus Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.commons.EmptyVisitor;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.File;
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * Implementation of {@link MainClassFinder) using
+ * <a href="http://asm.objectweb.org/">ASM</a> to inspect the class files.
+ */
+public class AsmMainMethodFinder
+    extends AbstractMainMethodFinder
+{
+    private static final String ARGS_SIGNATURE = "([Ljava/lang/String;)V";
+
+    protected boolean containsMainClass( InputStream inputStream )
+    {
+        try
+        {
+            ClassReader reader = new ClassReader( inputStream );
+            HasMainMethodClassVisitor v = new HasMainMethodClassVisitor();
+            reader.accept( v, 0 );
+            return v.isMainMethod();
+        }
+        catch ( IOException e )
+        {
+            throw new RuntimeException( e );
+        }
+    }
+
+    class HasMainMethodClassVisitor
+        extends EmptyVisitor
+    {
+
+        private boolean mainMethod;
+
+        public MethodVisitor visitMethod( int access, String name, String desc, String signature, String[] exceptions )
+        {
+            if ( access == Opcodes.ACC_PUBLIC + Opcodes.ACC_STATIC && "main".equals( name ) &&
+                ARGS_SIGNATURE.equals( desc ) )
+            {
+                this.mainMethod = true;
+            }
+            return null;
+        }
+
+        public boolean isMainMethod()
+        {
+            return mainMethod;
+        }
+
+        public void setMainMethod( boolean mainMethod )
+        {
+            this.mainMethod = mainMethod;
+        }
+    }
+}
---
+++ plexus-sandbox/trunk/plexus-components/plexus-mainclass-finder/src/main/java/org/codehaus/plexus/mainclass/MainClassFinder.java
@@ -0,0 +1,37 @@
+package org.codehaus.plexus.mainclass;
+
+/*
+ * Copyright 2007 The Codehaus Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.util.List;
+
+/**
+ * Component that will scan a list of paths {@link java.io.File}s for
+ * classes containing a <code>public static void main(String[] args)</code> method.
+ * The files can represent jar files or directories containing class files.
+ */
+public interface MainClassFinder
+{
+    public final static String ROLE = MainClassFinder.class.getName();
+
+    /**
+     * Find names of all classes containing a main method.
+     *
+     * @param classPath a {@link List} of jar files or directories where class files can be found
+     * @return a {@link List} of class names (Strings)
+     */
+    List findMainMethods( List classPath );
+}
