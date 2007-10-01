65edad647e756e5256b271595ebb42a3dda659b9
user57 <user57@30dfa635-7bf8-0310-9608-e41f2d776689>
2007-10-01 04:21:46 +0000

    Drop the old annotations here, use the bits from plexus-component-annotations instead. Adding a shitty testsuite and an class-based annotation gleaner And dropping some of the other cruft

--- plexus-tools/trunk/plexus-cdc-anno/src/test/projects/test1/src/main/java/org/codehaus/plexus/cdc/component/Component.java
+++
@@ -1,6 +0,0 @@
-package org.codehaus.plexus.cdc.component;
-
-public interface Component
-{
-    public static final String ROLE = "org.codehaus.plexus.cdc.component.Component";
-}
--- plexus-tools/trunk/plexus-cdc-anno/src/test/projects/test2/src/main/java/org/codehaus/plexus/cdc/test/Parent.java
+++
@@ -1,9 +0,0 @@
-package org.codehaus.plexus.cdc.test;
-
-import org.codehaus.plexus.cdc.annotation.Requirement;
-
-public class Parent
-{
-    @Requirement(roleHint="myHint")
-    private Object object;
-}
