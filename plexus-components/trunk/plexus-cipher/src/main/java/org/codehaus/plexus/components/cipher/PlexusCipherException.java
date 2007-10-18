b3c88db9bf732fca3623461186da802f4256985d
oleg <oleg@30dfa635-7bf8-0310-9608-e41f2d776689>
2007-10-18 00:08:01 +0000

    Initial import.

---
+++ plexus-components/trunk/plexus-cipher/src/main/java/org/codehaus/plexus/components/cipher/PlexusCipherException.java
@@ -0,0 +1,25 @@
+package org.codehaus.plexus.components.cipher;
+
+public class PlexusCipherException extends Exception
+{
+
+	public PlexusCipherException()
+	{
+	}
+
+	public PlexusCipherException(String message)
+	{
+		super(message);
+	}
+
+	public PlexusCipherException(Throwable cause)
+	{
+		super(cause);
+	}
+
+	public PlexusCipherException(String message, Throwable cause)
+	{
+		super(message, cause);
+	}
+
+}
