d7e56b3073f3fbd42c3ce7be1544a57fc9bb0656
jdcasey <jdcasey@30dfa635-7bf8-0310-9608-e41f2d776689>
2007-10-27 01:28:48 +0000

    [maven-release-plugin]  copy for tag plexus-containers-1.0-alpha-35

---
+++ plexus-containers/tags/plexus-containers-1.0-alpha-35/plexus-container-default/src/main/java/org/codehaus/plexus/component/factory/ComponentInstantiationException.java
@@ -0,0 +1,43 @@
+package org.codehaus.plexus.component.factory;
+
+/*
+ * Copyright 2001-2006 Codehaus Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ *
+ * 
+ * @author <a href="mailto:michal@codehaus.org">Michal Maczka</a>
+ *
+ * @version $Id$
+ */
+public class ComponentInstantiationException
+    extends Exception
+{
+    public ComponentInstantiationException( String message )
+    {
+        super( message );
+    }
+
+    public ComponentInstantiationException( String message, Throwable cause )
+    {
+        super( message, cause );
+    }
+
+    public ComponentInstantiationException( Throwable cause )
+    {
+        super( cause );
+    }
+}
