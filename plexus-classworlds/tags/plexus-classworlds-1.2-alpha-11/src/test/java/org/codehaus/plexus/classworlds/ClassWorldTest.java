b23d86e96316eacea3d6eeb2da0a21bb033d2532
mkleint <mkleint@30dfa635-7bf8-0310-9608-e41f2d776689>
2007-12-05 20:55:27 +0000

    [maven-release-plugin]  copy for tag plexus-classworlds-1.2-alpha-11

---
+++ plexus-classworlds/tags/plexus-classworlds-1.2-alpha-11/src/test/java/org/codehaus/plexus/classworlds/ClassWorldTest.java
@@ -0,0 +1,153 @@
+package org.codehaus.plexus.classworlds;
+
+/*
+ * Copyright 2001-2006 Codehaus Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Enumeration;
+import org.codehaus.plexus.classworlds.realm.DuplicateRealmException;
+import org.codehaus.plexus.classworlds.realm.NoSuchRealmException;
+import org.codehaus.plexus.classworlds.realm.ClassRealm;
+
+public class ClassWorldTest
+    extends AbstractClassWorldsTestCase
+{
+    private ClassWorld world;
+
+    public ClassWorldTest( String name )
+    {
+        super( name );
+    }
+
+    public void setUp()
+    {
+        this.world = new ClassWorld();
+    }
+
+    public void tearDown()
+    {
+        this.world = null;
+    }
+
+    public void testEmpty()
+    {
+        assertTrue( this.world.getRealms().isEmpty() );
+    }
+
+    public void testNewRealm()
+        throws Exception
+    {
+        ClassRealm realm = this.world.newRealm( "foo" );
+
+        assertNotNull( realm );
+    }
+
+    public void testGetRealm()
+        throws Exception
+    {
+        ClassRealm realm = this.world.newRealm( "foo" );
+
+        assertSame( realm, this.world.getRealm( "foo" ) );
+    }
+
+    public void testNewRealm_Duplicate()
+        throws Exception
+    {
+        try
+        {
+            this.world.newRealm( "foo" );
+            this.world.newRealm( "foo" );
+
+            fail( "throw DuplicateRealmException" );
+        }
+        catch ( DuplicateRealmException e )
+        {
+            // expected and correct
+
+            assertSame( this.world, e.getWorld() );
+
+            assertEquals( "foo", e.getId() );
+        }
+    }
+
+    public void testGetRealm_NoSuch()
+        throws Exception
+    {
+        try
+        {
+            this.world.getRealm( "foo" );
+            fail( "throw NoSuchRealmException" );
+        }
+        catch ( NoSuchRealmException e )
+        {
+            // expected and correct
+
+            assertSame( this.world, e.getWorld() );
+
+            assertEquals( "foo", e.getId() );
+        }
+    }
+
+    public void testGetRealms()
+        throws Exception
+    {
+        assertTrue( this.world.getRealms().isEmpty() );
+
+        ClassRealm foo = this.world.newRealm( "foo" );
+
+        assertEquals( 1, this.world.getRealms().size() );
+
+        assertTrue( this.world.getRealms().contains( foo ) );
+
+        ClassRealm bar = this.world.newRealm( "bar" );
+
+        assertEquals( 2, this.world.getRealms().size() );
+
+        assertTrue( this.world.getRealms().contains( bar ) );
+    }
+    
+    public void testPLX334() 
+        throws Exception 
+    {
+        ClassLoader loader = new URLClassLoader( new URL[] { getJarUrl("component1-1.0.jar") } );
+        ClassRealm nb = world.newRealm( "netbeans", loader );
+        ClassRealm plexus = world.newRealm( "plexus" );
+        plexus.importFrom( "netbeans", "META-INF/plexus" );
+        plexus.importFrom( "netbeans", "org.codehaus.plexus" );
+        Enumeration e = plexus.findResources( "META-INF/plexus/components.xml" );
+        assertNotNull( e );
+        int resourceCount = 0;
+        for ( Enumeration resources = e; resources.hasMoreElements(); )
+        {
+            resources.nextElement();
+            resourceCount++;
+        }
+        assertEquals( 2, resourceCount );
+        Class c = plexus.loadClass( "org.codehaus.plexus.Component1" );
+        assertNotNull( c );
+        
+    }
+    
+    protected URL getJarUrl( String jarName )
+        throws Exception
+    {
+        File jarFile = new File( TestUtil.getBasedir(), "src/test-jars/" + jarName );
+        return jarFile.toURI().toURL();
+    }
+    
+}
