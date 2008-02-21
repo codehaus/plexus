82fa8ebc64a4a7ba21a6bdd44e29d564670ee28e
jdcasey <jdcasey@30dfa635-7bf8-0310-9608-e41f2d776689>
2008-02-21 17:17:19 +0000

    [maven-release-plugin]  copy for tag plexus-archiver-1.0-alpha-10

---
+++ plexus-components/tags/plexus-archiver-1.0-alpha-10/src/main/java/org/codehaus/plexus/archiver/FilterEnabled.java
@@ -0,0 +1,14 @@
+package org.codehaus.plexus.archiver;
+
+import java.util.List;
+
+
+/**
+ * @deprecated Use {@link FileSelector file selectors}.
+ */
+public interface FilterEnabled
+{
+    
+    void setArchiveFilters( List filters );
+
+}
