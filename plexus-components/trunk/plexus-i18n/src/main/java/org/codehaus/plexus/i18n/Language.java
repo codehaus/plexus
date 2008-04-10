204b64eacb5fe83abc73d1055c94fc7947ba11d3
oleg <oleg@30dfa635-7bf8-0310-9608-e41f2d776689>
2008-04-10 17:28:07 +0000

    Added wrapper for Messages.properties bundle as a component

--- plexus-components/trunk/plexus-i18n/src/main/java/org/codehaus/plexus/i18n/Language.java
+++ plexus-components/trunk/plexus-i18n/src/main/java/org/codehaus/plexus/i18n/Language.java
@@ -1,37 +1,22 @@
 package org.codehaus.plexus.i18n;
 
 import java.util.Locale;
-import java.util.ResourceBundle;
 
-public class Language
+public interface Language
 {
-	public static String DEFAULT_NAME = "Messages";
+    public static String ROLE = Language.class.getName();
+
+    /** look for Messages.properties in the clazz package by default */
+    public static final String DEFAULT_NAME = "Messages";
+
+    public Language init();
 	
-	private Class clazz;
-	ResourceBundle rb;
+	public Language init( Class clazz );
 	
-	public Language()
-	{
-	}
-	
-	public Language( Class clazz )
-	{
-		this( clazz, Locale.getDefault() );
-	}
-	
-	public Language( Class clazz, Locale locale )
-	{
-		this.clazz = clazz;
-		rb = ResourceBundle.getBundle( clazz.getPackage().getName()+"."+DEFAULT_NAME, locale, clazz.getClassLoader() );
-	}
+	public Language init( Class clazz, Locale locale );
 	
 	public String getMessage( String key )
 	throws LanguageException
-	{
-		if( rb == null )
-			throw new LanguageException("resourceBundle not initialized for "+(clazz==null?"null":clazz.getName() ) );
-		
-		return rb.getString(key);
-	}
+	;
 	
 }
