21288fd8f34b68e8cee8f7cf716fca39b7b028df
jdcasey <jdcasey@30dfa635-7bf8-0310-9608-e41f2d776689>
2008-06-13 22:20:31 +0000

    [maven-release-plugin]  copy for tag plexus-utils-1.5.2

---
+++ plexus-utils/tags/plexus-utils-1.5.2/src/main/java/org/codehaus/plexus/util/interpolation/ValueSource.java
@@ -0,0 +1,12 @@
+package org.codehaus.plexus.util.interpolation;
+
+
+/**
+ *
+ * @author jdcasey
+ * @deprecated Use plexus-interpolation APIs instead.
+ */
+public interface ValueSource
+    extends org.codehaus.plexus.interpolation.ValueSource
+{
+}
