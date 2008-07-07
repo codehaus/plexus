4f23c750825444e61cff15871bbb6fbb73a340e5
olamy <olamy@30dfa635-7bf8-0310-9608-e41f2d776689>
2008-07-07 23:30:08 +0000

    fix use of escapeJavaScript to avoid depending on commons-lang version 2.4 which introduce change http://issues.apache.org/jira/browse/LANG-363 and break use of values like /foo/bar/foo.properties with plexus-spring.

--- plexus-sandbox/trunk/plexus-components/plexus-spring/src/main/java/org/codehaus/plexus/spring/DOM2Utils.java
+++ plexus-sandbox/trunk/plexus-components/plexus-spring/src/main/java/org/codehaus/plexus/spring/DOM2Utils.java
@@ -19,7 +19,12 @@ package org.codehaus.plexus.spring;
  * under the License.
  */
 
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+
 import org.apache.commons.lang.StringEscapeUtils;
+import org.codehaus.plexus.util.StringUtils;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -68,11 +73,124 @@ public class DOM2Utils
     
     public static String escapeAttributeValue( String value )
     {
-        return StringEscapeUtils.escapeJavaScript( value );
+        if ( StringUtils.isEmpty( value ) )
+        {
+            return null;
+        }
+        StringWriter writer = new StringWriter( value.length() * 2 );
+        try
+        {
+            escapeJavaStyleString( writer, value, true );
+            return writer.toString();
+        }
+        catch ( IOException e )
+        {
+            // this should never ever happen while writing to a StringWriter
+            throw new RuntimeException( "error during escapeJavaStyleString " + e.getMessage(), e );
+        }
     }
 
     public static String escapeText( String text )
     {
         return StringEscapeUtils.escapeXml( text );
     }
+    
+    private static void escapeJavaStyleString( Writer out, String str, boolean escapeSingleQuote )
+        throws IOException
+    {
+        if ( out == null )
+        {
+            throw new IllegalArgumentException( "The Writer must not be null" );
+        }
+        if ( str == null )
+        {
+            return;
+        }
+        int sz;
+        sz = str.length();
+        for ( int i = 0; i < sz; i++ )
+        {
+            char ch = str.charAt( i );
+
+            // handle unicode
+            if ( ch > 0xfff )
+            {
+                out.write( "\\u" + hex( ch ) );
+            }
+            else if ( ch > 0xff )
+            {
+                out.write( "\\u0" + hex( ch ) );
+            }
+            else if ( ch > 0x7f )
+            {
+                out.write( "\\u00" + hex( ch ) );
+            }
+            else if ( ch < 32 )
+            {
+                switch ( ch )
+                {
+                    case '\b':
+                        out.write( '\\' );
+                        out.write( 'b' );
+                        break;
+                    case '\n':
+                        out.write( '\\' );
+                        out.write( 'n' );
+                        break;
+                    case '\t':
+                        out.write( '\\' );
+                        out.write( 't' );
+                        break;
+                    case '\f':
+                        out.write( '\\' );
+                        out.write( 'f' );
+                        break;
+                    case '\r':
+                        out.write( '\\' );
+                        out.write( 'r' );
+                        break;
+                    default:
+                        if ( ch > 0xf )
+                        {
+                            out.write( "\\u00" + hex( ch ) );
+                        }
+                        else
+                        {
+                            out.write( "\\u000" + hex( ch ) );
+                        }
+                        break;
+                }
+            }
+            else
+            {
+                switch ( ch )
+                {
+                    case '\'':
+                        if ( escapeSingleQuote )
+                        {
+                            out.write( '\\' );
+                        }
+                        out.write( '\'' );
+                        break;
+                    case '"':
+                        out.write( '\\' );
+                        out.write( '"' );
+                        break;
+                    case '\\':
+                        out.write( '\\' );
+                        out.write( '\\' );
+                        break;
+                    default:
+                        out.write( ch );
+                        break;
+                }
+            }
+        }
+    }
+
+    private static String hex( char ch )
+    {
+        return Integer.toHexString( ch ).toUpperCase();
+    }
+    
 }
