e91094be8b0064d42330aab4c446a5d377aba930
jvanzyl <jvanzyl@30dfa635-7bf8-0310-9608-e41f2d776689>
2008-07-23 17:07:50 +0000

    o redback is its own project now

--- archived/plexus-redback/tags/redback-1.0-alpha-2/redback-system/src/test/java/org/codehaus/plexus/redback/system/DefaultSecureApplication.java
+++
@@ -1,9 +0,0 @@
-package org.codehaus.plexus.redback.system;
-
-/**
- * @author Jason van Zyl
- */
-public class DefaultSecureApplication
-    implements SecureApplication
-{
-}
--- archived/plexus-redback/trunk/redback-system/src/test/java/org/codehaus/plexus/redback/system/SecureApplication.java
+++
@@ -1,9 +0,0 @@
-package org.codehaus.plexus.redback.system;
-
-/**
- * @author Jason van Zyl
- */
-public interface SecureApplication
-{
-    String ROLE = SecureApplication.class.getName();
-}
