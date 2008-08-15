04da73c0bca0b656ce609a52e5f31618e8917c70
oleg <oleg@30dfa635-7bf8-0310-9608-e41f2d776689>
2008-08-15 19:11:54 +0000

    [maven-release-plugin]  copy for tag plexus-i18n-1.0-beta-10

---
+++ plexus-components/tags/plexus-i18n-1.0-beta-10/src/main/java/org/codehaus/plexus/i18n/LanguageException.java
@@ -0,0 +1,25 @@
+package org.codehaus.plexus.i18n;
+
+public class LanguageException extends Exception
+{
+
+	public LanguageException()
+	{
+	}
+
+	public LanguageException(String message)
+	{
+		super(message);
+	}
+
+	public LanguageException(Throwable cause)
+	{
+		super(cause);
+	}
+
+	public LanguageException(String message, Throwable cause)
+	{
+		super(message, cause);
+	}
+
+}
