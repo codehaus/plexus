3d2ccf5fd931d97904a95f8c8cc3ee953f3fcf81
jdcasey <jdcasey@30dfa635-7bf8-0310-9608-e41f2d776689>
2008-11-13 16:09:41 +0000

    move to octal values

--- plexus-components/trunk/plexus-io/src/main/java/org/codehaus/plexus/components/io/attributes/AttributeConstants.java
+++ plexus-components/trunk/plexus-io/src/main/java/org/codehaus/plexus/components/io/attributes/AttributeConstants.java
@@ -3,23 +3,23 @@ package org.codehaus.plexus.components.io.attributes;
 public final class AttributeConstants
 {
 
-    public static final int OCTAL_OWNER_READ = 256;
+    public static final int OCTAL_OWNER_READ = 0400;
 
-    public static final int OCTAL_OWNER_WRITE = 128;
+    public static final int OCTAL_OWNER_WRITE = 0200;
 
-    public static final int OCTAL_OWNER_EXECUTE = 64;
+    public static final int OCTAL_OWNER_EXECUTE = 0100;
 
-    public static final int OCTAL_GROUP_READ = 32;
+    public static final int OCTAL_GROUP_READ = 040;
 
-    public static final int OCTAL_GROUP_WRITE = 16;
+    public static final int OCTAL_GROUP_WRITE = 020;
 
-    public static final int OCTAL_GROUP_EXECUTE = 8;
+    public static final int OCTAL_GROUP_EXECUTE = 010;
 
-    public static final int OCTAL_WORLD_READ = 4;
+    public static final int OCTAL_WORLD_READ = 04;
 
-    public static final int OCTAL_WORLD_WRITE = 2;
+    public static final int OCTAL_WORLD_WRITE = 02;
 
-    public static final int OCTAL_WORLD_EXECUTE = 1;
+    public static final int OCTAL_WORLD_EXECUTE = 01;
 
     private AttributeConstants()
     {
