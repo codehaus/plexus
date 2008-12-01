44b3899e68a42481b6fe4f69559cd612ab36288a
jvanzyl <jvanzyl@30dfa635-7bf8-0310-9608-e41f2d776689>
2008-12-01 04:20:33 +0000

    [maven-release-plugin]  copy for tag plexus-containers-1.0-beta-3.0.1

---
+++ plexus-containers/tags/plexus-containers-1.0-beta-3.0.1/plexus-component-metadata/src/main/java/org/codehaus/plexus/maven/plugin/PlexusDescriptorMojo.java
@@ -0,0 +1,43 @@
+package org.codehaus.plexus.maven.plugin;
+
+/*
+ * Copyright (c) 2004-2005, Codehaus.org
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import org.apache.maven.plugin.MojoExecutionException;
+
+/**
+ * Generates a Plexus <tt>components.xml</tt> component descriptor file from source (javadoc) or
+ * class annotations.
+ * 
+ * @goal generate-metadata
+ * @phase process-classes
+ * @requiresDependencyResolution compile
+ * @author Jason van Zyl
+ * @author Trygve Laugst&oslash;l
+ * @version $Id$
+ */
+public class PlexusDescriptorMojo
+    extends AbstractDescriptorMojo
+{
+    public void execute()
+        throws MojoExecutionException
+    {
+        generateDescriptor( COMPILE_SCOPE, generatedMetadata );
+    }
+}
