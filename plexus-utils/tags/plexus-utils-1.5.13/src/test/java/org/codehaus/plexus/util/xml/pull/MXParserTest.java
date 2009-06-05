89f7f149c10d4ad27c8180bc6c7f6c5ff0cb4dfe
jvanzyl <jvanzyl@30dfa635-7bf8-0310-9608-e41f2d776689>
2009-06-05 16:47:31 +0000

    [maven-release-plugin]  copy for tag plexus-utils-1.5.13

---
+++ plexus-utils/tags/plexus-utils-1.5.13/src/test/java/org/codehaus/plexus/util/xml/pull/MXParserTest.java
@@ -0,0 +1,149 @@
+package org.codehaus.plexus.util.xml.pull;
+
+/*
+ * Copyright The Codehaus Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.StringReader;
+
+import junit.framework.TestCase;
+
+/**
+ * @author <a href="mailto:trygvis@inamo.no">Trygve Laugst&oslash;l</a>
+ * @version $Id$
+ */
+public class MXParserTest
+    extends TestCase
+{
+    public void testHexadecimalEntities()
+        throws Exception
+    {
+        MXParser parser = new MXParser();
+
+        parser.defineEntityReplacementText( "test", "replacement" );
+
+        String input = "<root>&#x41;</root>";
+
+        parser.setInput( new StringReader( input ) );
+
+        assertEquals( XmlPullParser.START_TAG, parser.next() );
+
+        assertEquals( XmlPullParser.TEXT, parser.next() );
+
+        assertEquals( "A", parser.getText() );
+
+        assertEquals( XmlPullParser.END_TAG, parser.next() );
+    }
+
+    public void testDecimalEntities()
+        throws Exception
+    {
+        MXParser parser = new MXParser();
+
+        parser.defineEntityReplacementText( "test", "replacement" );
+
+        String input = "<root>&#65;</root>";
+
+        parser.setInput( new StringReader( input ) );
+
+        assertEquals( XmlPullParser.START_TAG, parser.next() );
+
+        assertEquals( XmlPullParser.TEXT, parser.next() );
+
+        assertEquals( "A", parser.getText() );
+
+        assertEquals( XmlPullParser.END_TAG, parser.next() );
+    }
+
+    public void testPredefinedEntities()
+        throws Exception
+    {
+        MXParser parser = new MXParser();
+
+        parser.defineEntityReplacementText( "test", "replacement" );
+
+        String input = "<root>&lt;&gt;&amp;&apos;&quot;</root>";
+
+        parser.setInput( new StringReader( input ) );
+
+        assertEquals( XmlPullParser.START_TAG, parser.next() );
+
+        assertEquals( XmlPullParser.TEXT, parser.next() );
+
+        assertEquals( "<>&'\"", parser.getText() );
+
+        assertEquals( XmlPullParser.END_TAG, parser.next() );
+    }
+
+    public void testCustomEntities()
+        throws Exception
+    {
+        MXParser parser = new MXParser();
+
+        String input = "<root>&myentity;</root>";
+        parser.setInput( new StringReader( input ) );
+        parser.defineEntityReplacementText( "myentity", "replacement" );
+        assertEquals( XmlPullParser.START_TAG, parser.next() );
+        assertEquals( XmlPullParser.TEXT, parser.next() );
+        assertEquals( "replacement", parser.getText() );
+        assertEquals( XmlPullParser.END_TAG, parser.next() );
+
+        parser = new MXParser();
+        input = "<root>&myCustom;</root>";
+        parser.setInput( new StringReader( input ) );
+        parser.defineEntityReplacementText( "fo", "&#65;" );
+        parser.defineEntityReplacementText( "myCustom", "&fo;" );
+        assertEquals( XmlPullParser.START_TAG, parser.next() );
+        assertEquals( XmlPullParser.TEXT, parser.next() );
+        assertEquals( "&#65;", parser.getText() );
+        assertEquals( XmlPullParser.END_TAG, parser.next() );
+    }
+
+    public void testUnicodeEntities()
+        throws Exception
+    {
+        MXParser parser = new MXParser();
+        String input = "<root>&#x1d7ed;</root>";
+        parser.setInput( new StringReader( input ) );
+
+        assertEquals( XmlPullParser.START_TAG, parser.nextToken() );
+        assertEquals( XmlPullParser.ENTITY_REF, parser.nextToken() );
+        assertEquals( "\uD835\uDFED", parser.getText() );
+        assertEquals( XmlPullParser.END_TAG, parser.nextToken() );
+
+        parser = new MXParser();
+        input = "<root>&#x159;</root>";
+        parser.setInput( new StringReader( input ) );
+
+        assertEquals( XmlPullParser.START_TAG, parser.nextToken() );
+        assertEquals( XmlPullParser.ENTITY_REF, parser.nextToken() );
+        assertEquals( "\u0159", parser.getText() );
+        assertEquals( XmlPullParser.END_TAG, parser.nextToken() );
+    }
+
+    public void testProcessingInstruction()
+        throws Exception
+    {
+        String input = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><test>nnn</test>";
+
+        MXParser parser = new MXParser();
+        parser.setInput( new StringReader( input ) );
+
+        assertEquals( XmlPullParser.PROCESSING_INSTRUCTION, parser.nextToken() );
+        assertEquals( XmlPullParser.START_TAG, parser.nextToken() );
+        assertEquals( XmlPullParser.TEXT, parser.nextToken() );
+        assertEquals( XmlPullParser.END_TAG, parser.nextToken() );
+    }
+}
