27071b7f5351dd2a5ef2d6cbfc0ed379532977a5
olamy <olamy@30dfa635-7bf8-0310-9608-e41f2d776689>
2009-09-14 22:13:42 +0000

    [maven-scm] copy for tag plexus-io-1.0-alpha-5

---
+++ plexus-components/tags/plexus-io-1.0-alpha-5/src/main/java/org/codehaus/plexus/components/io/attributes/AttributeConstants.java
@@ -0,0 +1,44 @@
+package org.codehaus.plexus.components.io.attributes;
+
+/*
+ * Copyright 2007 The Codehaus Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public final class AttributeConstants
+{
+
+    public static final int OCTAL_OWNER_READ = 0400;
+
+    public static final int OCTAL_OWNER_WRITE = 0200;
+
+    public static final int OCTAL_OWNER_EXECUTE = 0100;
+
+    public static final int OCTAL_GROUP_READ = 040;
+
+    public static final int OCTAL_GROUP_WRITE = 020;
+
+    public static final int OCTAL_GROUP_EXECUTE = 010;
+
+    public static final int OCTAL_WORLD_READ = 04;
+
+    public static final int OCTAL_WORLD_WRITE = 02;
+
+    public static final int OCTAL_WORLD_EXECUTE = 01;
+
+    private AttributeConstants()
+    {
+    }
+
+}
